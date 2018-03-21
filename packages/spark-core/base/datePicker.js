d5df42498afebce8081fa77e93990c2a7759061d
Rob Copeland <copeland.robert@gmail.com>
2018-03-21 16:58:59 -0400

    datepicker date overrides, a11y bug, search bug, added required tests

--- packages/spark-core/base/datePicker.js
+++ packages/spark-core/base/datePicker.js
@@ -2,11 +2,15 @@ import TinyDatePicker from 'tiny-date-picker';
 import getElements from '../utilities/getElements';
 
 const datePicker = () => {
-  getElements('[data-sprk-input="datepicker"] input', (element) => {
-    TinyDatePicker(element, {
+  getElements('[data-sprk-input="datepicker"]', (element) => {
+    const input = element.querySelector('input');
+    const overrideMinDate = element.getAttribute('data-sprk-min-date');
+    const overrideMaxDate = element.getAttribute('data-sprk-max-date');
+
+    TinyDatePicker(input, {
       mode: 'dp-below',
-      min: '01/1/2008',
-      max: '01/1/2068',
+      min: overrideMinDate || '01/1/2008',
+      max: overrideMaxDate || '01/1/2068',
     });
   });
 };
