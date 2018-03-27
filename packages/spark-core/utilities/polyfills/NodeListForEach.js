97811c50e5197aaeea62083557129bed9c1f28a5
Rob Copeland <copeland.robert@gmail.com>
2018-03-27 17:04:10 -0400

    added NodeList.forEach polyfill

---
+++ packages/spark-core/utilities/polyfills/NodeListForEach.js
@@ -0,0 +1,13 @@
+/* global window NodeList */
+const NodeListForEachPolyfill = () => {
+  if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+      const arg = thisArg || window;
+      for (let i = 0; i < this.length; i++) {
+        callback.call(arg, this[i], i, this);
+      }
+    };
+  }
+};
+
+export { NodeListForEachPolyfill as default };
