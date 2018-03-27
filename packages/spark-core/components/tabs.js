61b4e70c97ef4e01852ba52899277cc1af0d0047
Rob Copeland <copeland.robert@gmail.com>
2018-03-27 10:52:26 -0400

    finished all the a11y stuff for tabs, altered the tabs template content

--- packages/spark-core/components/tabs.js
+++ packages/spark-core/components/tabs.js
@@ -17,19 +17,67 @@ const setActiveTab = (tab, tabpanel) => {
   tab.classList.add('sprk-c-Tabs__button--active');
   tab.setAttribute('aria-selected', 'true');
   tabpanel.classList.remove('sprk-u-Hidden');
+  tab.focus();
+};
+
+const getActiveTabIndex = (tabs) => {
+  let activeIndex = null;
+  tabs.forEach((tab, index) => {
+    if (tab.classList.contains('sprk-c-Tabs__button--active')) {
+      activeIndex = index;
+    }
+  });
+
+  return activeIndex;
+};
+
+const advanceTab = (tabs, tabpanels) => {
+  const activeIndex = getActiveTabIndex(tabs);
+
+  resetTabs(tabs, tabpanels);
+
+  if (activeIndex + 1 <= tabs.length - 1) {
+    setActiveTab(tabs[activeIndex + 1], tabpanels[activeIndex + 1]);
+  } else {
+    setActiveTab(tabs[0], tabpanels[0]);
+  }
+};
+
+const retreatTab = (tabs, tabpanels) => {
+  const activeIndex = getActiveTabIndex(tabs);
+
+  resetTabs(tabs, tabpanels);
+
+  if (activeIndex - 1 === -1) {
+    setActiveTab(tabs[tabs.length - 1], tabpanels[tabs.length - 1]);
+  } else {
+    setActiveTab(tabs[activeIndex - 1], tabpanels[activeIndex - 1]);
+  }
 };
 
 const bindUIEvents = (element) => {
+  const tabContainer = element.querySelector('.sprk-c-Tabs__buttons');
   const tabs = element.querySelectorAll('[role="tab"]');
   const tabpanels = element.querySelectorAll('[role="tabpanel"]');
-
-  // TODO: arrows cycle through tabs, focus in on tablist moves to active
 
   tabs.forEach((tab, index) => {
     tab.addEventListener('click', () => {
       resetTabs(tabs, tabpanels);
       setActiveTab(tab, tabpanels[index]);
     });
+  });
+
+  tabContainer.addEventListener('keydown', (event) => {
+    if (event.keyCode === 37) {
+      retreatTab(tabs, tabpanels);
+    } else if (event.keyCode === 39) {
+      advanceTab(tabs, tabpanels);
+    }
+  });
+
+  tabContainer.addEventListener('focusin', () => {
+    const activeIndex = getActiveTabIndex(tabs);
+    tabs[activeIndex].focus();
   });
 };
 
@@ -49,4 +97,4 @@ const tabs = () => {
   });
 };
 
-export { tabs, ariaOrientation, resetTabs, setActiveTab };
+export { tabs, ariaOrientation, resetTabs, setActiveTab, advanceTab, retreatTab };
