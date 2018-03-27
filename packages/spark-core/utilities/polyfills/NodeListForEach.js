0066f5f171c66b87a0d0ef7ed49ac90f88743ac6
Rob Copeland <copeland.robert@gmail.com>
2018-03-27 17:45:44 -0400

    refactored the NodeListForEach polyfill to match the others

--- packages/spark-core/utilities/polyfills/NodeListForEach.js
+++ packages/spark-core/utilities/polyfills/NodeListForEach.js
@@ -1,13 +1,10 @@
-/* global window NodeList */
-const NodeListForEachPolyfill = () => {
-  if (window.NodeList && !NodeList.prototype.forEach) {
-    NodeList.prototype.forEach = function (callback, thisArg) {
-      const arg = thisArg || window;
-      for (let i = 0; i < this.length; i++) {
-        callback.call(arg, this[i], i, this);
-      }
-    };
-  }
-};
-
-export { NodeListForEachPolyfill as default };
+/* eslint-disable */
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    const arg = thisArg || window;
+    for (let i = 0; i < this.length; i++) {
+      callback.call(arg, this[i], i, this);
+    }
+  };
+}
+/* eslint-enable */
