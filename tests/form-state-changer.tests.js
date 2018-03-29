69a6185a8d40214dc4bd565f4d3ffb924f94bec0
Rob Copeland <copeland.robert@gmail.com>
2018-03-29 09:57:58 -0400

    added form-state-changer tests, added tests files to coverage excludes

--- tests/form-state-changer.tests.js
+++ tests/form-state-changer.tests.js
@@ -1,4 +1,4 @@
-/* global describe it */
+/* global document before describe it */
 import { mapTemplates, toggleTemplate } from '../src/assets/drizzle/scripts/form-state-changer';
 
 const jsdom = require('jsdom');
@@ -9,13 +9,52 @@ const dom = new JSDOM('<!DOCTYPE html><p>Hello world</p>');
 global.document = dom.window.document;
 
 describe('mapTemplates ', () => {
-  it('should', () => {
-    expect(1).eql(2);
+  const templates = [];
+  before(() => {
+    templates[0] = document.createElement('div');
+    templates[0].setAttribute('data-template-id', 'test1');
+    templates[1] = document.createElement('div');
+    templates[1].setAttribute('data-template-id', 'test1-error-state');
+    templates[2] = document.createElement('div');
+    templates[2].setAttribute('data-template-id', 'test1-disabled-state');
+    templates[3] = document.createElement('div');
+    templates[3].setAttribute('data-template-id', 'test2');
+    templates[4] = document.createElement('div');
+    templates[4].setAttribute('data-template-id', 'test2-error-state');
+    templates[5] = document.createElement('div');
+    templates[5].setAttribute('data-template-id', 'test2-disabled-state');
+  });
+
+  it('should only return templates that match the id given', () => {
+    let containsMismatches = false;
+    const mappedTemplates = mapTemplates('test1', templates);
+    mappedTemplates.forEach((template) => {
+      if (template.getAttribute('data-template-id').indexOf('test2') !== -1) {
+        containsMismatches = true;
+      }
+    });
+    expect(containsMismatches).eql(false);
   });
 });
 
 describe('toggleTemplate', () => {
-  it('should', () => {
-    expect(1).eql(2);
+  const templates = [];
+  before(() => {
+    templates[0] = document.createElement('div');
+    templates[0].setAttribute('data-template-id', 'test1');
+    templates[0].classList.add('hidden');
+    templates[1] = document.createElement('div');
+    templates[1].setAttribute('data-template-id', 'test1-error-state');
+    templates[1].classList.add('hidden');
+    templates[2] = document.createElement('div');
+    templates[2].setAttribute('data-template-id', 'test1-disabled-state');
+    templates[2].classList.add('hidden');
+  });
+
+  it('should remove .hidden from the matching template', () => {
+    toggleTemplate('test1', 'normal', templates);
+    expect(templates[0].classList.contains('hidden')).eql(false);
+    expect(templates[1].classList.contains('hidden')).eql(true);
+    expect(templates[1].classList.contains('hidden')).eql(true);
   });
 });
