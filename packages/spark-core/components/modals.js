047faa12f5d0b2e5405a2d50e667b415beb9f435
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-04-09 16:32:51 -0400

    comment clean up

--- packages/spark-core/components/modals.js
+++ packages/spark-core/components/modals.js
@@ -3,7 +3,6 @@ import getElements from '../utilities/getElements';
 
 // Get focusable elements in modal
 const getFocusableEls = (modal) => {
-  // Grab all focusable elements
   let focusEls = modal.querySelectorAll('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex="0"]');
   focusEls = Array.prototype.slice.call(focusEls);
   return focusEls;
@@ -12,7 +11,6 @@ const getFocusableEls = (modal) => {
 // Focus first el in modal
 const focusFirstModalEl = (modal) => {
   const focusEls = getFocusableEls(modal);
-  // If there are some then apply focus to first one
   if (focusEls.length > 0) {
     focusEls[0].focus();
   }
@@ -24,32 +22,30 @@ const isMaskClicked = e => e.target.getAttribute('data-sprk-modal') === 'mask';
 
 const isEscPressed = e => e.key === 'Escape';
 
-// Hide the modal and remove aria attribute on body
+// Hide the modal and mask and also remove aria-hidden on body
 const hideModal = (modal, focusedBodyEl) => {
   const isHidden = modal.classList.contains('sprk-u-Hide');
   const mask = document.querySelector('[data-sprk-modal="mask"]');
 
   if (!isHidden) {
-    // Modal does not have the hide class so we now add it to close it
     modal.classList.add('sprk-u-Hide');
     mask.classList.add('sprk-u-Hide');
-    // We want to remove the hidden aria attr from body
+    // Remove the hidden aria attr from body
     document.body.removeAttribute('aria-hidden');
     // Send focus back to last active element before modal was shown
     focusedBodyEl.focus();
   }
 };
 
-// Show the modal and set aria attribute true on body
+// Show the modal and set aria-hidden true on body
 const showModal = (modal) => {
   const isNotShown = modal.classList.contains('sprk-u-Hide');
   const mask = document.querySelector('[data-sprk-modal="mask"]');
 
   if (isNotShown) {
-    // Modal is closed so now we show it
     modal.classList.remove('sprk-u-Hide');
     mask.classList.remove('sprk-u-Hide');
-    // We want to alert assitive devices that body is hidden
+    // We want to alert assistive devices that body is hidden
     document.body.setAttribute('aria-hidden', 'true');
   }
 };
@@ -58,7 +54,7 @@ const handleModalEvents = (modal, focusedBodyEl) => {
   // Grab body element
   const docBodyHidden = document.querySelector('[aria-hidden="true"]');
 
-  // Attach listener for keydown
+  // Attach listener for keydown events
   docBodyHidden.addEventListener('keydown', (e) => {
     const focusableEls = getFocusableEls(modal);
     const firstFocusableEl = focusableEls[0];
@@ -70,19 +66,19 @@ const handleModalEvents = (modal, focusedBodyEl) => {
       hideModal(modal, focusedBodyEl);
     }
 
-    // If only 1 focusable element prevent default
+    // If only one focusable element prevent default
     if (focusableEls.length === 1) {
       e.preventDefault();
     } else if (isTabPressed(e) && e.shiftKey) {
-      // If user tabbing backward from the first focusable element
+      // If user tabs backward from the first focusable element
       // then move to the last focusable element.
       if (document.activeElement === firstFocusableEl) {
         e.preventDefault();
         lastFocusableEl.focus();
       }
     } else if (isTabPressed(e)) {
-      // If user tabbing forward from last focusable element
-      // then to the first focusable element
+      // If user tabs forward from the last focusable element
+      // then move to the first focusable element
       if (document.activeElement === lastFocusableEl) {
         e.preventDefault();
         firstFocusableEl.focus();
@@ -90,7 +86,7 @@ const handleModalEvents = (modal, focusedBodyEl) => {
     }
   });
 
-  // Attach listener for clicks on document
+  // Attach listener for modal mask clicks on document
   docBodyHidden.addEventListener('click', (e) => {
     // If mask is clicked then hide the modal
     if (isMaskClicked(e)) {
@@ -103,7 +99,7 @@ const handleModalEvents = (modal, focusedBodyEl) => {
 const setupCancelEl = (modal, modalName, focusedBodyEl) => {
   // Look for cancel elements
   getElements(`[data-sprk-modal-cancel="${modalName}"]`, (cancel) => {
-    // Show/Hide the modal and mask pair when cancel is clicked
+    // Hide the modal when cancel is clicked
     cancel.addEventListener('click', (e) => {
       e.preventDefault();
       hideModal(modal, focusedBodyEl);
