eec6d634e04b0c48d53f6855f45e2cb4c4f561b4
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-04-20 10:20:59 -0400

    remove attr selector because of perf and add classlist ie11 file

--- packages/spark-core/spark-core.js
+++ packages/spark-core/spark-core.js
@@ -21,6 +21,7 @@ import './utilities/polyfills/ArrayFrom';
 import './utilities/polyfills/StringIncludes';
 import './utilities/polyfills/ArrayFind';
 import './utilities/polyfills/NodeListForEach';
+import './utilities/polyfills/classListSVG';
 
 // Init
 requiredSelect();
---
+++ packages/spark-core/utilities/polyfills/classListSVG.js
@@ -0,0 +1,3 @@
+/* eslint-disable */
+// add polyfil for ie11 classlist usage on svg elements
+/* eslint-enable */
