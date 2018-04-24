36abf56bb7f3aef29693c74cfe111675626e6e33
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-04-24 09:17:10 -0400

    add polyfill for classlist on els that don

--- packages/spark-core/utilities/polyfills/classListSVG.js
+++ packages/spark-core/utilities/polyfills/classListSVG.js
@@ -1,3 +1,12 @@
 /* eslint-disable */
-// add polyfil for ie11 classlist usage on svg elements
+// Add polyfil for ie11 classlist usage on svg elements
+if (!Object.getOwnPropertyDescriptor(Element.prototype, 'classList')) {
+  if (HTMLElement && Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'classList')) {
+    Object.defineProperty(
+      Element.prototype,
+      'classList',
+      Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'classList'),
+    );
+  }
+}
 /* eslint-enable */
