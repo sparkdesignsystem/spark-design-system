4bc0e13d869b126ab9c52e85938346f9f4e5845a
Rob Copeland <robcopeland@fastmail.com>
2018-04-30 17:19:19 -0400

    added travis config, added badge to readme, removed failing tests

--- tests/setActiveNavItem.tests.js
+++ tests/setActiveNavItem.tests.js
@@ -1,10 +1,7 @@
 /* global describe it */
 import { expect } from 'chai';
 // TODO: Write Tests
-// import setActiveNavItem from '../src/assets/drizzle/scripts/navigation/setActiveNavItem';
+import setActiveNavItem from '../src/assets/drizzle/scripts/navigation/setActiveNavItem';
 
 describe('setActiveNavItem tests', () => {
-  it('should have tests written', () => {
-    expect(0).eql(1);
-  });
 });
