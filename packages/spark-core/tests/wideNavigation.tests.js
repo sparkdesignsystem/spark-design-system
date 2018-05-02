17aa1e64a5ce870ca6d7e0f3e91b32a4b4dca82f
Rob Copeland <robcopeland@fastmail.com>
2018-05-02 11:42:11 -0400

    narrow nav bugs, wide nav a11y

--- packages/spark-core/tests/wideNavigation.tests.js
+++ packages/spark-core/tests/wideNavigation.tests.js
@@ -1,6 +1,6 @@
 /* global before document describe it */
 import { expect } from 'chai';
-import { showDropDown } from '../components/wide-navigation';
+import { showDropDown, hideAllDropDowns } from '../components/wide-navigation';
 
 describe('wide nav tests', () => {
   let nav;
@@ -33,5 +33,51 @@ describe('wide nav tests', () => {
   it('should remove the hidden class from the subNav inside', () => {
     showDropDown(navItem1);
     expect(subNav1.classList.contains('sprk-u-Hide')).eql(false);
+  });
+});
+
+describe('hide all navigation tests', () => {
+  let nav;
+  let navItem1;
+  let navItem2;
+  let navItem3;
+  let subNav1;
+  let subNav2;
+
+  before(() => {
+    nav = document.createElement('ul');
+    navItem1 = document.createElement('li');
+    navItem1.setAttribute('aria-expanded', 'true');
+    navItem2 = document.createElement('li');
+    navItem2.setAttribute('aria-expanded', 'true');
+    navItem3 = document.createElement('li');
+
+    subNav1 = document.createElement('ul');
+    subNav1.classList.add('sprk-c-WideNavigation--sub');
+
+    subNav2 = document.createElement('ul');
+    subNav2.classList.add('sprk-c-WideNavigation--sub');
+
+    navItem1.appendChild(subNav1);
+    navItem3.appendChild(subNav2);
+
+    nav.appendChild(navItem1);
+    nav.appendChild(navItem2);
+    nav.appendChild(navItem3);
+
+    hideAllDropDowns(
+      nav.querySelectorAll('.sprk-c-WideNavigation--sub'),
+      nav.querySelectorAll('[aria-expanded]'),
+    );
+  });
+
+  it('should add the hidden class to the sub navs and set aria-expanded', () => {
+    expect(subNav1.classList.contains('sprk-u-Hide')).eql(true);
+    expect(subNav2.classList.contains('sprk-u-Hide')).eql(true);
+  });
+
+  it('should set aria-expanded on nav items to false', () => {
+    expect(navItem1.getAttribute('aria-expanded') === 'false').eql(true);
+    expect(navItem2.getAttribute('aria-expanded') === 'false').eql(true);
   });
 });
