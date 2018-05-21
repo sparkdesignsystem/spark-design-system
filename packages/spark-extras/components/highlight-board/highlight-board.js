96f0fb07df1c9a1a6b336696a15e200807db8e28
Rob Copeland <robcopeland@fastmail.com>
2018-05-21 11:27:09 -0400

    added polyfill for object-fit

--- packages/spark-extras/components/highlight-board/highlight-board.js
+++ packages/spark-extras/components/highlight-board/highlight-board.js
@@ -1,1 +1,7 @@
-export default () => {};
+import objectFitImages from '../../node_modules/object-fit-images/dist/ofi.es-modules';
+
+const highlightBoard = () => {
+  objectFitImages('.sprk-c-HighlightBoard__image');
+};
+
+export { highlightBoard as default };
