b00aea026fa6ba93ea137ea9158dfc5445d65a35
Rob Copeland <robcopeland@fastmail.com>
2018-06-04 11:27:09 -0400

    adding tests for validation runner

--- packages/spark-core/tests/validation-runner.tests.js
+++ packages/spark-core/tests/validation-runner.tests.js
@@ -1,7 +1,30 @@
-/* global describe it require */
-// import { expect } from 'chai';
-// import runValidation from '../utilities/validation/validation-runner';
+/* global describe it before require document */
+import { expect } from 'chai';
+import sinon from 'sinon';
+import runValidation from '../utilities/validation/validation-runner';
 
 describe('validation runner', () => {
-  it('should write tests for validation runner');
+  let element;
+  let field;
+  const validFunction = sinon.stub();
+  const inValidFunction = sinon.stub();
+
+  before(() => {
+    element = document.createElement('div');
+    field = document.createElement('input');
+    field.setAttribute('pattern', '');
+    element.appendChild(field);
+  });
+
+  it('should call the valid function if the field is valid.', () => {
+    runValidation(element, field, validFunction, inValidFunction);
+    expect(validFunction.calledOnce).eql(true);
+  });
+
+  it('should call the inValid function if the field is valid.', () => {
+    field.setAttribute('pattern', '[abc]');
+    field.value = '123';
+    runValidation(element, field, validFunction, inValidFunction);
+    expect(inValidFunction.calledOnce).eql(true);
+  });
 });
--- packages/spark-core/utilities/validation/validation-runner.js
+++ packages/spark-core/utilities/validation/validation-runner.js
@@ -2,8 +2,10 @@ const runValidation = (element, field, validFunction, invalidFunction) => {
   const validTest = field.validity.valid;
   if (field.validity.valid) {
     validFunction(element);
+    console.log('valid');
   } else {
     invalidFunction(element, 'There is an error on this field.');
+    console.log('invalid');
   }
   return validTest;
 };
