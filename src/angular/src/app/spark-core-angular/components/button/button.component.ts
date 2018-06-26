a87269aeb540eb31eb8275b3a0a69d2078b82695
Rob Copeland <robcopeland@fastmail.com>
2018-06-26 10:50:27 -0400

    fixing spinner click handler

--- src/angular/src/app/spark-core-angular/components/button/button.component.ts
+++ src/angular/src/app/spark-core-angular/components/button/button.component.ts
@@ -13,8 +13,20 @@ export class SparkButtonComponent {
   @Input() isRemoval: boolean;
   @Input() isDisabled: boolean;
   @Input() analyticsString: string;
+  @Input() spinner: boolean;
+  @Input() spinnerSize: string;
+  @Input() spinnerLightness: string;
 
-  addSpinner(): void {
-    setSpinning(event.target, { });
+  isSpinning: boolean = false;
+  options: Object = {
+    size: this.spinnerSize,
+    lightness: this.spinnerLightness
+  };
+
+  checkSpinner(): void {
+    if (this.spinner && !this.isSpinning) {
+      setSpinning(event.target, this.options);
+      this.isSpinning = true;
+    }
   }
 }
