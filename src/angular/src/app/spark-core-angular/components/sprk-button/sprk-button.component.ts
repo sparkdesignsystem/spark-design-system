366576859a5b9341348fdb071afbbaa8dc0c0009
Rob Copeland <robcopeland@fastmail.com>
2018-06-27 09:49:11 -0400

    switching to buttonType over spliting them out

--- src/angular/src/app/spark-core-angular/components/sprk-button/sprk-button.component.ts
+++ src/angular/src/app/spark-core-angular/components/sprk-button/sprk-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { setSpinning } from '@sparkdesignsystem/spark-core/components/spinners';
 
 @Component({
@@ -8,19 +8,62 @@ import { setSpinning } from '@sparkdesignsystem/spark-core/components/spinners';
 })
 
 export class SparkButtonComponent {
-  @Input() isSecondary: boolean;
-  @Input() isTertiary: boolean;
-  @Input() isRemoval: boolean;
+  @Input() buttonType: string;
   @Input() isDisabled: boolean;
   @Input() analyticsString: string;
   @Input() spinner: boolean;
+  @Input() additionalClasses: string;
+  @Input() additionalAttributes: Object;
 
   public isSpinning: boolean = false;
+
+  public classString: string;
+  public attributesString: string;
+
+  getClasses(): string {
+    let classArray: Array<String> = [
+    'sprk-c-Button'
+    ];
+
+    switch (this.buttonType) {
+      case 'secondary':
+        classArray.push('sprk-c-Button--secondary');
+        break;
+      case 'tertiary':
+        classArray.push('sprk-c-Button--tertiary');
+        break;
+      case 'removal':
+        classArray.push('sprk-c-Button--tertiary');
+        break;
+      default:
+        break;
+    }
+
+    if(this.isDisabled) {
+      classArray.push('sprk-c-Button--removal');
+    }
+
+    if(this.additionalClasses) {
+      this.additionalClasses.split(' ').forEach((className) => {
+        classArray.push(className);
+      })
+    }
+
+    return classArray.join(' ');
+  }
+
+  getAttributes(): string {
+    return '';
+  }
 
   checkSpinner(): void {
     if (this.spinner && !this.isSpinning) {
       setSpinning(event.target, {});
       this.isSpinning = true;
     }
+  }
+
+  ngOnInit() {
+    this.classString = this.getClasses();
   }
 }
