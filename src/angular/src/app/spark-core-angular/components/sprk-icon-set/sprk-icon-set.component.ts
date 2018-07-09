843bbada8b5ca789cdc5f25a2bacc1302cce55e8
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-07-09 13:12:22 -0400

    bring sprk icon tests to 100

--- src/angular/src/app/spark-core-angular/components/sprk-icon-set/sprk-icon-set.component.ts
+++ src/angular/src/app/spark-core-angular/components/sprk-icon-set/sprk-icon-set.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'sprk-icon-set',
   templateUrl: 'sprk-icon-set.component.html',
   styles: ['']
 })
+
 export class SparkIconSetComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
