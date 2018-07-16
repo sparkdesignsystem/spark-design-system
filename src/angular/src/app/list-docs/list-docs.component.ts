d038c88c5c009113121498324c33ca6ceffd98b6
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-07-16 10:23:35 -0400

    remove include of onit

--- src/angular/src/app/list-docs/list-docs.component.ts
+++ src/angular/src/app/list-docs/list-docs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'sprk-list-docs',
@@ -65,11 +65,7 @@ import { Component, OnInit } from '@angular/core';
   `,
   styles: ['']
 })
-export class ListDocsComponent implements OnInit {
+export class ListDocsComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
