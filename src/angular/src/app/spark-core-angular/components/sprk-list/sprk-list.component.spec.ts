a465cb45fcddb429bf3d52ddb6edbd7658080367
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-07-17 09:09:17 -0400

    tests for lists

--- src/angular/src/app/spark-core-angular/components/sprk-list/sprk-list.component.spec.ts
+++ src/angular/src/app/spark-core-angular/components/sprk-list/sprk-list.component.spec.ts
@@ -5,6 +5,7 @@ import { SparkListComponent } from './sprk-list.component';
 describe('SparkListComponent', () => {
   let component: SparkListComponent;
   let fixture: ComponentFixture<SparkListComponent>;
+  let element;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +17,29 @@ describe('SparkListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SparkListComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement.querySelector('ul');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('getClasses should match what gets set on the element', () => {
+    fixture.detectChanges();
+    expect(element.classList.toString()).toEqual(component.getClasses());
+  });
+
+  it('should add the correct classes if additionalClasses have values', () => {
+    component.additionalClasses = 'sprk-u-pam sprk-u-man';
+    fixture.detectChanges();
+    expect(component.getClasses()).toEqual('sprk-b-List sprk-u-pam sprk-u-man');
+  });
+
+  it('should set the data-analytics attribute given a value in the analyticsString Input', () => {
+    component.analyticsString = 'List 1';
+    fixture.detectChanges();
+    expect(element.hasAttribute('data-analytics')).toEqual(true);
+    expect(element.getAttribute('data-analytics')).toEqual('List 1');
   });
 });
