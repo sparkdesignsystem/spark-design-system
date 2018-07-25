2b6859bc0317382bddf7f5f5631df02382a5903d
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-07-25 15:48:09 -0400

    add icon option to promo and card

--- src/angular/src/app/card-docs/card-docs.component.ts
+++ src/angular/src/app/card-docs/card-docs.component.ts
@@ -69,6 +69,34 @@ import { Component } from '@angular/core';
       </h2>
 
       <sprk-card
+        media="img"
+        cardType="teaser"
+        title="Spark Card With Button CTA!"
+        body="
+          Content placeholder! Things! Content placeholder!
+          Things! Content placeholder! Things! Content placeholder!
+          Things!
+        "
+        imgSrc="https://sparkdesignsystem.com/assets/toolkit/images/desktop.jpg"
+        imgAlt="Placeholder Image"
+        imgHref="www.sparkdesignsystem.com"
+        ctaType="button"
+        ctaText="Learn!"
+        ctaHref="www.sparkdesignsystem.com"
+        ctaAnalytics="Button: Spark">
+      </sprk-card>
+    </div>
+
+    <div class="drizzle-o-ContentGrouping">
+      <h2 class="drizzle-b-h2">
+        Teaser With Icon
+      </h2>
+
+      <sprk-card
+        media="icon"
+        iconType="calculator"
+        iconHref="https://sparkdesignsystem.com"
+        additionalClassesIcon="sprk-c-Icon--l sprk-b-TypeBodyTwo"
         cardType="teaser"
         title="Spark Card With Button CTA!"
         body="
@@ -93,6 +121,7 @@ import { Component } from '@angular/core';
 
       <sprk-card
         cardType="teaser"
+        media="img"
         title="Spark Card with a link CTA"
         body="
           Content placeholder! Things! Content placeholder!
@@ -111,12 +140,13 @@ import { Component } from '@angular/core';
 
     <div class="drizzle-o-ContentGrouping">
       <h2 class="drizzle-b-h2">
-        Teaser With Heading on Top
+        Teaser With Heading On Top
       </h2>
 
       <sprk-card
         cardType="teaserHeading"
         title="Teaser Heading On Top of Image"
+        media="img"
         body="
           Placeholder text that is here in this card!
         "
@@ -127,6 +157,33 @@ import { Component } from '@angular/core';
         ctaText="Neat!"
         ctaHref="www.sparkdesignsystem.com"
         ctaAnalytics="Link: Spark">
+      </sprk-card>
+    </div>
+
+    <div class="drizzle-o-ContentGrouping">
+      <h2 class="drizzle-b-h2">
+        Teaser With Icon And Heading On Top
+      </h2>
+
+      <sprk-card
+        media="icon"
+        iconType="calculator"
+        iconHref="https://sparkdesignsystem.com"
+        additionalClassesIcon="sprk-c-Icon--l sprk-b-TypeBodyTwo"
+        cardType="teaserHeading"
+        title="Spark Card With Button CTA!"
+        body="
+          Content placeholder! Things! Content placeholder!
+          Things! Content placeholder! Things! Content placeholder!
+          Things!
+        "
+        imgSrc="https://sparkdesignsystem.com/assets/toolkit/images/desktop.jpg"
+        imgAlt="Placeholder Image"
+        imgHref="www.sparkdesignsystem.com"
+        ctaType="button"
+        ctaText="Learn!"
+        ctaHref="www.sparkdesignsystem.com"
+        ctaAnalytics="Button: Spark">
       </sprk-card>
     </div>
 
