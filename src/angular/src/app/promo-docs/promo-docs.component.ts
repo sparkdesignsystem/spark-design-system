d83c125cb340c41a93078052afd0bdb9c261089b
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-07-25 11:28:29 -0400

    start promo angular component

---
+++ src/angular/src/app/promo-docs/promo-docs.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sprk-promo-docs',
+  template: `
+    PROMO
+  `,
+  styles: ['']
+})
+
+export class PromoDocsComponent {
+  constructor() { }
+}
