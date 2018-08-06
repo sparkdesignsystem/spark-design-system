385359b4efa7b5206f2f130793a9db3360e5308c
Rob Copeland <robcopeland@fastmail.com>
2018-08-06 15:36:26 -0400

    adding some tests for directives

--- src/angular/src/app/spark-core-angular/directives/inputs/sprk-input/sprk-input.directive.spec.ts
+++ src/angular/src/app/spark-core-angular/directives/inputs/sprk-input/sprk-input.directive.spec.ts
@@ -1,1 +1,63 @@
-//TODO: write tests!
+import { Component } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { SprkInputDirective } from './sprk-input.directive';
+
+@Component({
+  selector: 'test',
+  template: `
+  <input 
+    additionalClasses="sprk-u-man"
+    sprk-input>`
+})
+class TestComponent {}
+
+@Component({
+  selector: 'test-select',
+  template: `
+  <select
+    additionalClasses="sprk-u-man"
+    sprk-input></select>`
+})
+class TestSelectComponent {}
+
+describe('Spark Input Directive', () => {
+  let component: TestComponent;
+  let selectComponent: TestSelectComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let selectFixture: ComponentFixture<TestSelectComponent>;
+  let inputElement: HTMLElement;
+  let selectElement: HTMLElement;
+
+  beforeEach(async(()=>{
+    TestBed.configureTestingModule({
+      declarations: [ SprkInputDirective, TestComponent, TestSelectComponent ]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+
+    selectFixture = TestBed.createComponent(TestSelectComponent);
+    selectComponent = selectFixture.componentInstance;
+
+    fixture.detectChanges();
+    inputElement = fixture.nativeElement.querySelector('input');
+
+    selectElement = selectFixture.nativeElement.querySelector('select');
+  }));
+
+  it('should create itself', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should add classes if additionalClasses has a value', () => {
+    fixture.detectChanges();
+    expect(inputElement.classList.toString()).toEqual('sprk-b-TextInput sprk-u-Width-100 sprk-u-man');
+  });
+
+  it('should add the correct class if a select element is used', () => {
+    selectFixture.detectChanges();
+    expect(selectElement.classList.toString()).toEqual('sprk-b-Select sprk-u-man');
+
+  });
+});
--- src/angular/src/app/spark-core-angular/directives/inputs/sprk-selection-label/sprk-selection-label.directive.spec.ts
+++ src/angular/src/app/spark-core-angular/directives/inputs/sprk-selection-label/sprk-selection-label.directive.spec.ts
@@ -1,1 +1,39 @@
-//TODO: write tests!
+import { Component } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { SprkSelectionLabelDirective } from './sprk-selection-label.directive';
+
+@Component({
+  selector: 'test',
+  template: `
+  <label
+    additionalClasses="sprk-u-man"
+    sprk-selection-label>Label</label>`
+})
+class TestComponent {}
+
+describe('Spark Helper Text Directive', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let labelElement: HTMLElement;
+
+  beforeEach(async(()=>{
+    TestBed.configureTestingModule({
+      declarations: [ SprkSelectionLabelDirective, TestComponent ]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    labelElement = fixture.nativeElement.querySelector('label');
+  }));
+
+  it('should create itself', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should add classes if additionalClasses has a value', () => {
+    fixture.detectChanges();
+    expect(labelElement.classList.toString()).toEqual('sprk-b-Label sprk-b-Label--inline sprk-u-mls sprk-u-man');
+  });
+});
