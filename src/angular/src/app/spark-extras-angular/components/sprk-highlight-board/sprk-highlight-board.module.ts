7609a58cbd729c78cc289c2b88bf38cfcf3fcf3f
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-08-22 10:49:58 -0400

    build out highlight board

--- src/angular/src/app/spark-extras-angular/components/sprk-highlight-board/sprk-highlight-board.module.ts
+++ src/angular/src/app/spark-extras-angular/components/sprk-highlight-board/sprk-highlight-board.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SparkStackModule } from '../../../spark-core-angular/components/sprk-stack/sprk-stack.module';
+import { SparkStackItemModule } from '../../../spark-core-angular/directives/sprk-stack-item/sprk-stack-item.module';
 import { SparkHighlightBoardComponent } from './sprk-highlight-board.component';
 
 @NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+    SparkStackModule,
+    SparkStackItemModule
+  ],
   declarations: [
     SparkHighlightBoardComponent,
   ],
