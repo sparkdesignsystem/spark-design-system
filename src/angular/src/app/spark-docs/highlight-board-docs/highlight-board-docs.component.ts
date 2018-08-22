fa0754768d53b293e3d27fd9e2f30f6208ce8c45
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-08-22 10:49:58 -0400

    build out highlight board

--- src/angular/src/app/spark-docs/highlight-board-docs/highlight-board-docs.component.ts
+++ src/angular/src/app/spark-docs/highlight-board-docs/highlight-board-docs.component.ts
@@ -3,18 +3,66 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sprk-highlight-board-docs',
   template: `
+  <div class="drizzle-o-ContentGrouping">
+    <div class="sprk-o-CenteredColumn">
+      <h2 class="drizzle-b-h2">
+        Highlight Board Default
+      </h2>
 
-    <div class="drizzle-o-ContentGrouping">
-      <div class="sprk-o-CenteredColumn">
-        <h2 class="drizzle-b-h2">
-          Highlight Board
-        </h2>
-
-        <sprk-highlight-board></sprk-highlight-board>
-      </div>
+      <sprk-highlight-board
+        heading="Highlight Board Heading Example"
+        subHeading="Sub heading example for the Highlight Board component."
+        ctaText="Learn More"
+        imgSrc="https://staging.sparkdesignsystem.com/assets/toolkit/images/desktop.jpg">
+      </sprk-highlight-board>
     </div>
+  </div>
 
+  <div class="drizzle-o-ContentGrouping">
+    <div class="sprk-o-CenteredColumn">
+      <h2 class="drizzle-b-h2">
+        Highlight Board With No Image
+      </h2>
 
+      <sprk-highlight-board
+        heading="Highlight Board Heading Example"
+        subHeading="Sub heading example for the Highlight Board component."
+        ctaText="Learn More"
+        type="noImage">
+      </sprk-highlight-board>
+    </div>
+  </div>
+
+  <div class="drizzle-o-ContentGrouping">
+    <div class="sprk-o-CenteredColumn">
+      <h2 class="drizzle-b-h2">
+        Highlight Board Stacked
+      </h2>
+
+      <sprk-highlight-board
+        heading="Highlight Board Heading Example"
+        subHeading="Sub heading example for the Highlight Board component."
+        ctaText="Learn More"
+        type="stacked"
+        imgSrc="https://staging.sparkdesignsystem.com/assets/toolkit/images/desktop.jpg">
+      </sprk-highlight-board>
+    </div>
+  </div>
+
+  <div class="drizzle-o-ContentGrouping">
+    <h2 class="drizzle-b-h2">
+      Highlight Board Full Width
+    </h2>
+
+    <sprk-highlight-board
+      heading="Highlight Board Heading Example"
+      subHeading="Sub heading example for the Highlight Board component."
+      ctaText="Learn More"
+      ctaTwoText="CTA Two"
+      type="fullWidth"
+      imgSrc="https://staging.sparkdesignsystem.com/assets/toolkit/images/desktop.jpg">
+    </sprk-highlight-board>
+  </div>
   `,
   styles: ['']
 })
--- src/angular/src/app/spark-extras-angular/components/sprk-highlight-board/sprk-highlight-board.component.ts
+++ src/angular/src/app/spark-extras-angular/components/sprk-highlight-board/sprk-highlight-board.component.ts
@@ -3,23 +3,94 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'sprk-highlight-board',
   template: `
-    hello! highlight board!
+  <div [ngClass]="getClasses()">
+    <img
+      *ngIf="type !== 'noImage'"
+      class="sprk-c-HighlightBoard__image"
+      src="{{ imgSrc }}">
+
+    <div
+      [ngClass]="{
+        'sprk-c-HighlightBoard__content': true,
+        'sprk-c-HighlightBoard__content--light': type === 'default' || !type || type === 'fullWidth'
+      }">
+      <div class="sprk-u-mbl">
+        <h1 class="sprk-b-TypeDisplayThree sprk-c-HighlightBoard__primary-heading sprk-u-mbs">
+          {{ heading }}
+        </h1>
+
+        <h4 class="sprk-b-TypeDisplayFour sprk-c-HighlightBoard__sub-heading sprk-u-mbm">
+          {{ subHeading }}
+        </h4>
+      </div>
+
+      <sprk-stack
+        itemSpacing="medium"
+        splitAt="small"
+        additionalClasses="sprk-c-HighlightBoard__cta-container">
+        <div sprk-stack-item>
+          <a
+            [routerLink]="ctaHref"
+            class="sprk-c-HighlightBoard__cta sprk-c-Button sprk-c-Button--primary sprk-c-Button--full@sm"
+            [attr.data-analytics]="analyticsString">
+            {{ ctaText }}
+          </a>
+        </div>
+
+        <div
+          sprk-stack-item
+          *ngIf="ctaTwoText">
+          <a
+            [routerLink]="ctaTwoHref"
+            class="sprk-c-HighlightBoard__cta sprk-c-Button sprk-c-Button--primary sprk-c-Button--full@sm"
+            [attr.data-analytics]="analyticsStringCta2">
+            {{ ctaTwoText }}
+          </a>
+        </div>
+      </sprk-stack>
+    </div>
+  </div>
   `,
   styles: ['']
 })
 
 export class SparkHighlightBoardComponent {
-  @Input() imgOneAlt: string;
-  @Input() imgOneHref: string;
-  @Input() imgOneSrc: string;
-  @Input() analyticsStringImgOne: string;
+  @Input() heading: string;
+  @Input() subHeading: string;
+  @Input() imgSrc: string;
+  @Input() ctaText: string;
+  @Input() ctaHref: string;
+  @Input() ctaTwoText: string;
+  @Input() ctaTwoHref: string;
+  @Input() analyticsString: string;
+  @Input() analyticsStringCta2: string;
   @Input() additionalClasses: string;
-  @Input() title: string;
+  @Input() type: string;
 
   getClasses(): string {
     let classArray: Array<String> = [
-      ''
+      'sprk-c-HighlightBoard'
     ];
+
+    switch (this.type) {
+      case 'noImage':
+        break;
+      case 'stacked':
+        classArray.push('sprk-c-HighlightBoard--has-image sprk-c-HighlightBoard--stacked');
+        break;
+      case 'fullWidth':
+        classArray.push('sprk-c-HighlightBoard--has-image sprk-c-HighlightBoard--has-image');
+        break;
+      default:
+        classArray.push('sprk-c-HighlightBoard--has-image');
+        break;
+    }
+
+    if (this.additionalClasses) {
+      this.additionalClasses.split(' ').forEach((className) => {
+        classArray.push(className);
+      })
+    }
 
     return classArray.join(' ');
   }
