38a4bde30fce43917076ed5bc26764c275c57bd7
Rob Copeland <robcopeland@fastmail.com>
2018-08-27 16:46:51 -0400

    created code / docs dirs

--- src/assets/drizzle/scripts/handlebars-helpers/alternateIdGen.js
+++ src/assets/drizzle/scripts/handlebars-helpers/alternateIdGen.js
@@ -1,9 +1,12 @@
 /* global arguments */
-function alternateIdGen(initialID, alternateDir) {
+function alternateIdGen(initialID, alternateDir, subDir) {
   const splitID = initialID.split('.');
   const dir = splitID.slice(0, -1);
   const pattern = splitID[splitID.length - 1];
   dir.push(alternateDir);
+  if (subDir) {
+    dir.push(subDir);
+  }
   dir.push(pattern);
   return dir.join('.');
 }
