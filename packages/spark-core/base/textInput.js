83b1e36dde851cc61a86a36bf088421e28f3be98
Rob Copeland <robcopeland@fastmail.com>
2018-08-30 17:50:52 -0400

    added code to handle returning to the page with values

--- packages/spark-core/base/textInput.js
+++ packages/spark-core/base/textInput.js
@@ -1,3 +1,4 @@
+/* global window */
 import getElements from '../utilities/getElements';
 
 const bindUIEvents = (element) => {
@@ -22,8 +23,16 @@ const bindUIEvents = (element) => {
 
 
 const textInput = () => {
-  getElements('[data-sprk-input="text"]', (element) => {
-    bindUIEvents(element);
+  window.addEventListener('load', () => {
+    getElements('[data-sprk-input="text"]', (element) => {
+      bindUIEvents(element);
+
+      // for failed form submits / back button
+      if (element.value !== '') {
+        element.classList.add('sprk-b-TextInput--has-value');
+        element.classList.add('sprk-b-TextInput--focusout');
+      }
+    });
   });
 };
 
