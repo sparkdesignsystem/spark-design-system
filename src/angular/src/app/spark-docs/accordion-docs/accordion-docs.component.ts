ed1630cc79c48b4ec53fea56deb882d856dfbb35
Rob Copeland <robcopeland@fastmail.com>
2018-09-10 10:51:58 -0400

    added secondary accordions for angular, fixed alignment bug, switch angular app to import from packages instead of node_modules

--- src/angular/src/app/spark-docs/accordion-docs/accordion-docs.component.ts
+++ src/angular/src/app/spark-docs/accordion-docs/accordion-docs.component.ts
@@ -14,10 +14,20 @@ import { Component } from '@angular/core';
           </sprk-accordion-item>
         </sprk-accordion>
       </div>
+      <div class="drizzle-o-ContentGrouping">
+        <h2 class="drizzle-b-h2">Secondary Accordion</h2>
+        <sprk-accordion accordionType="secondary">
+          <sprk-accordion-item title="This is a title" additionalClasses="sprk-u-mbs">
+            <p>This is some content.</p>
+          </sprk-accordion-item>
+          <sprk-accordion-item title="This is another title">
+            <p>This is some more content.</p>
+          </sprk-accordion-item>
+        </sprk-accordion>
+      </div>
   `,
   styles: ['']
 })
-
 export class AccordionDocsComponent {
-  constructor() { }
+  constructor() {}
 }
