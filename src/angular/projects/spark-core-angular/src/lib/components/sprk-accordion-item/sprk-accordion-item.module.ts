de214612287c66e16bae79d4d17ff4daa3ebc870
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-10-10 15:33:20 -0400

    update ng accordions

--- src/angular/projects/spark-core-angular/src/lib/components/sprk-accordion-item/sprk-accordion-item.module.ts
+++ src/angular/projects/spark-core-angular/src/lib/components/sprk-accordion-item/sprk-accordion-item.module.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SparkIconModule } from '../sprk-icon/sprk-icon.module';
 
 import { SparkAccordionItemComponent } from './sprk-accordion-item.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, SparkIconModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    SparkIconModule
+  ],
   declarations: [SparkAccordionItemComponent],
   exports: [SparkAccordionItemComponent]
 })
