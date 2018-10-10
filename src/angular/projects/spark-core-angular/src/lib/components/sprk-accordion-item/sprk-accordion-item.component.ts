ee3b5286bf90e1bb0a922d4fbf8ff75b367e0bf3
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-10-10 15:33:20 -0400

    update ng accordions

--- src/angular/projects/spark-core-angular/src/lib/components/sprk-accordion-item/sprk-accordion-item.component.ts
+++ src/angular/projects/spark-core-angular/src/lib/components/sprk-accordion-item/sprk-accordion-item.component.ts
@@ -1,29 +1,41 @@
 import { Component, Input } from '@angular/core';
 import * as _ from 'lodash';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'sprk-accordion-item',
   template: `
-      <li [ngClass]="getClasses()">
-        <a [attr.aria-controls]="accordion_controls_id"
-           [ngClass]="{
-             'sprk-c-Accordion__summary': true,
-             'sprk-c-Accordion__summary--open': isOpen
-             }"
-           href="#nogo"
-           [attr.data-analytics]="analyticsString"
-           (click)="toggleAccordion($event)">
-            <h3 class="sprk-b-TypeBodyOne sprk-c-Accordion__heading">
-              {{ title }}
-            </h3>
+    <li [ngClass]="getClasses()">
+      <a [attr.aria-controls]="accordion_controls_id"
+         [ngClass]="{
+           'sprk-c-Accordion__summary': true
+           }"
+         href="#nogo"
+         [attr.data-analytics]="analyticsString"
+         (click)="toggleAccordion($event)">
+          <h3 class="sprk-c-Accordion__heading sprk-b-TypeBodyTwo">
+            {{ title }}
+          </h3>
 
-            <sprk-icon additionalClasses="sprk-c-Accoridion__icon" [iconType]="iconType"></sprk-icon>
-        </a>
+          <sprk-icon additionalClasses="sprk-c-Accordion__icon sprk-c-Icon--toggle sprk-c-Icon--l {{ iconStateClass }}" [iconType]="iconType"></sprk-icon>
+      </a>
 
-        <p *ngIf="isOpen" [id]="accordion_controls_id" class="sprk-c-Accordion__details sprk-b-TypeBodyTwo sprk-u-HideWhenJs">
+      <div *ngIf="isOpen">
+        <p [id]="accordion_controls_id" class="sprk-c-Accordion__content sprk-b-TypeBodyTwo">
           <ng-content></ng-content>
         </p>
-      </li>`
+      </div>
+    </li>`,
+  animations: [
+    // animation triggers go here
+  ]
 })
 export class SparkAccordionItemComponent {
   @Input()
@@ -36,8 +48,9 @@ export class SparkAccordionItemComponent {
   isOpen = false;
 
   componentID = _.uniqueId();
-  accordion_controls_id = `modalHeading__${this.componentID}`;
+  accordion_controls_id = `accordionHeading__${this.componentID}`;
   public iconType = 'chevron-down';
+  public iconStateClass = '';
 
   toggleAccordion(event): void {
     event.preventDefault();
@@ -45,10 +58,18 @@ export class SparkAccordionItemComponent {
     this.iconType === 'chevron-down'
       ? (this.iconType = 'chevron-up')
       : (this.iconType = 'chevron-down');
+
+    this.isOpen === true
+      ? (this.iconStateClass = 'sprk-c-Icon--open')
+      : (this.iconStateClass = '');
   }
 
   getClasses(): string {
     const classArray: string[] = ['sprk-c-Accordion__item'];
+
+    if (this.isOpen) {
+      classArray.push('sprk-c-Accordion__item--open');
+    }
 
     if (this.additionalClasses) {
       this.additionalClasses.split(' ').forEach(className => {
