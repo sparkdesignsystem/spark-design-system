354203f90f0a7496e8154d335332d2341bce1d97
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-10-10 16:00:16 -0400

    setup animation

--- src/angular/src/app/spark-docs/accordion-docs/accordion-docs.component.ts
+++ src/angular/src/app/spark-docs/accordion-docs/accordion-docs.component.ts
@@ -7,7 +7,11 @@ import { Component } from '@angular/core';
         <h2 class="drizzle-b-h2">Primary Accordion</h2>
         <sprk-accordion>
           <sprk-accordion-item title="This is a longer title than all the other accordion titles here. But, it works!">
-            <p>This is some content.</p>
+            <p>
+              This is some content This is some content.
+              This is some content. This is some content
+              This is some content. This is some content.
+            </p>
           </sprk-accordion-item>
           <sprk-accordion-item title="This is another title">
             <p>This is some more content.</p>
