9d2e2f344edba7327b7e072aee239a4ede648403
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-10-10 16:12:10 -0400

    setup onit

--- src/angular/src/app/spark-docs/accordion-docs/accordion-docs.component.ts
+++ src/angular/src/app/spark-docs/accordion-docs/accordion-docs.component.ts
@@ -6,14 +6,14 @@ import { Component } from '@angular/core';
       <div class="drizzle-o-ContentGrouping">
         <h2 class="drizzle-b-h2">Primary Accordion</h2>
         <sprk-accordion>
-          <sprk-accordion-item title="This is a longer title than all the other accordion titles here. But, it works!">
+          <sprk-accordion-item isOpen="true" title="This is a longer title than all the other accordion titles here. But, it works!">
             <p>
               This is some content This is some content.
               This is some content. This is some content
               This is some content. This is some content.
             </p>
           </sprk-accordion-item>
-          <sprk-accordion-item title="This is another title">
+          <sprk-accordion-item isOpen="true" title="This is another title">
             <p>This is some more content.</p>
           </sprk-accordion-item>
           <sprk-accordion-item title="This is another title">
