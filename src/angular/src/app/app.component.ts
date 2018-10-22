48e609a15e462c42429864fcc79328b37cdc056f
Rob Copeland <robcopeland@fastmail.com>
2018-10-22 17:49:47 -0400

    bringing icon set into angular

--- src/angular/src/app/app.component.ts
+++ src/angular/src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'sprk-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Spark-Angular';
+
+  constructor(public ref: ElementRef) {}
+
+  ngOnInit(): void {
+    window
+      .fetch('https://spark-assets.netlify.com/spark-core-icons.svg', {})
+      .then(response => response.text())
+      .then(svg => {
+        this.ref.nativeElement.innerHTML += svg;
+      });
+  }
 }
