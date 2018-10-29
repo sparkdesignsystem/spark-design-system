868e8cd51a2d2c50dee3cd4ea0feb1cd82c5d829
Rob Copeland <robcopeland@fastmail.com>
2018-10-29 12:52:51 -0400

    reskin wide-nav / vanilla

--- packages/spark-core/components/wide-navigation.js
+++ packages/spark-core/components/wide-navigation.js
@@ -2,9 +2,10 @@
 import getElements from '../utilities/getElements';
 
 const showDropDown = (container) => {
-  const subNav = container.querySelector('.sprk-c-WideNavigation--sub');
-  if (subNav) {
-    subNav.classList.remove('sprk-u-Display--none');
+  const subNavContainer = container.querySelector('.sprk-c-WideNavigation__sub-menu-container');
+  console.log(subNavContainer);
+  if (subNavContainer) {
+    subNavContainer.classList.remove('sprk-u-Display--none');
     container.setAttribute('aria-expanded', 'true');
   }
 };
@@ -19,22 +20,30 @@ const hideAllDropDowns = (subNavs, listItems) => {
 };
 
 const bindUIEvents = (element) => {
-  const subNavs = document.querySelectorAll('.sprk-c-WideNavigation--sub');
+  const subNavContainers = document.querySelectorAll('.sprk-c-WideNavigation__sub-menu-container');
   const expandableListItems = document.querySelectorAll('.sprk-c-WideNavigation [aria-expanded]');
 
   element.addEventListener('focusin', (e) => {
     e.stopPropagation();
-    hideAllDropDowns(subNavs, expandableListItems);
+    e.target.classList.add('sprk-c-WideNavigation__item--active');
+    hideAllDropDowns(subNavContainers, expandableListItems);
     showDropDown(element);
+  });
+  element.addEventListener('focusout', (e) => {
+    e.stopPropagation();
+    // e.target.classList.remove('sprk-c-WideNavigation__item--active');
+    // hideAllDropDowns(subNavs, expandableListItems);
   });
   element.addEventListener('mouseenter', (e) => {
     e.stopPropagation();
-    hideAllDropDowns(subNavs, expandableListItems);
+    e.target.classList.add('sprk-c-WideNavigation__item--active');
+    hideAllDropDowns(subNavContainers, expandableListItems);
     showDropDown(element);
   });
   element.addEventListener('mouseleave', (e) => {
     e.stopPropagation();
-    hideAllDropDowns(subNavs, expandableListItems);
+    e.target.classList.remove('sprk-c-WideNavigation__item--active');
+    hideAllDropDowns(subNavContainers, expandableListItems);
   });
 };
 
