89231e695a77c3c481a985a0d08faeba8a8b535b
Rob Copeland <robcopeland@fastmail.com>
2018-11-01 14:36:35 -0400

    updated masthead line color, refactored styling, polyfill for focus-within

--- packages/spark-core/spark-core.js
+++ packages/spark-core/spark-core.js
@@ -25,6 +25,7 @@ import './utilities/polyfills/StringIncludes';
 import './utilities/polyfills/ArrayFind';
 import './utilities/polyfills/NodeListForEach';
 import './utilities/polyfills/classListSVG';
+import './utilities/polyfills/focusWithin';
 
 const SparkCore = ({ datePickerConfig = {} } = {}) => {
   requiredSelect();
