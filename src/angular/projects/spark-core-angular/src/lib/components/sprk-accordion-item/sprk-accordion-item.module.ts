19f394d54a03fd769fb6d73e0610741f3333188c
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-11-08 11:21:34 -0500

    remove import of browanimations in module

--- src/angular/projects/spark-core-angular/src/lib/components/sprk-accordion-item/sprk-accordion-item.module.ts
+++ src/angular/projects/spark-core-angular/src/lib/components/sprk-accordion-item/sprk-accordion-item.module.ts
@@ -1,18 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SparkIconModule } from '../sprk-icon/sprk-icon.module';
-
 import { SparkAccordionItemComponent } from './sprk-accordion-item.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule,
-    BrowserAnimationsModule,
-    SparkIconModule
-  ],
+  imports: [CommonModule, RouterModule, SparkIconModule],
   declarations: [SparkAccordionItemComponent],
   exports: [SparkAccordionItemComponent]
 })
