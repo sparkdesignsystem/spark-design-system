5ec0652a0d322f5d9141b5bc95f06529cb03567e
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-12-20 13:42:33 -0500

    add hightlighted header/responsive shadows

--- src/angular/src/app/spark-docs/card-docs/card-docs.component.ts
+++ src/angular/src/app/spark-docs/card-docs/card-docs.component.ts
@@ -231,6 +231,41 @@ import { Component } from '@angular/core';
         ></div>
       </sprk-card>
     </div>
+
+    <div class="drizzle-o-ContentGrouping">
+      <h2 class="drizzle-b-h2">Standout Card Highlighted Header</h2>
+      <sprk-card
+        cardType="base"
+        idString="card-12"
+        additionalClasses="sprk-c-Card--standout"
+      >
+        <div
+          class="sprk-o-Stack__item sprk-c-Card__header sprk-o-Stack sprk-o-Stack--medium"
+        >
+          <h3
+            class="sprk-b-TypeDisplayEight sprk-o-Stack__item sprk-u-Color--white"
+          >
+            Description
+          </h3>
+
+          <h4
+            class="sprk-b-TypeDisplayFive sprk-o-Stack__item sprk-u-Color--white"
+          >
+            Card Title
+          </h4>
+        </div>
+
+        <div
+          class="sprk-o-Stack__item sprk-o-Stack sprk-o-Stack--medium sprk-c-Card__content"
+        >
+          <p class="sprk-b-TypeBodyTwo sprk-o-Stack__item">
+            Lorem ipsum dolor sit amet, doctus invenire vix te. Facilisi
+            perpetua an pri, errem commune mea at, mei prima tantas
+            signiferumque at. Numquam.
+          </p>
+        </div>
+      </sprk-card>
+    </div>
   `,
   styles: ['']
 })
