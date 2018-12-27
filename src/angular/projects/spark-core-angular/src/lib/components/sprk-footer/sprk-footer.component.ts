5035a03a4d3f7b84e2829b447b13a0e448ef9344
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-12-27 15:55:32 -0500

    update angular component

--- src/angular/projects/spark-core-angular/src/lib/components/sprk-footer/sprk-footer.component.ts
+++ src/angular/projects/spark-core-angular/src/lib/components/sprk-footer/sprk-footer.component.ts
@@ -14,11 +14,9 @@ import { Component, Input } from '@angular/core';
         >
           <div
             *ngIf="globalLinks"
-            class="sprk-o-Stack__item sprk-o-Stack__item--fourth@m sprk-o-Stack sprk-o-Stack--large sprk-o-Box"
+            class="sprk-o-Stack__item sprk-o-Stack__item--three-tenths@m sprk-o-Stack sprk-o-Stack--misc-b sprk-o-Box sprk-u-prh"
           >
-            <h3
-              class="sprk-o-Stack__item sprk-b-TypeBodyOne sprk-u-TextTransform--uppercase"
-            >
+            <h3 class="sprk-o-Stack__item sprk-b-TypeBodyOne">
               {{ globalHeading }}
             </h3>
 
@@ -47,31 +45,29 @@ import { Component, Input } from '@angular/core';
                 />
               </a>
 
-              <p class="sprk-o-Stack__item sprk-b-TypeBodyTwo">
+              <p class="sprk-o-Stack__item sprk-b-TypeBodyFour">
                 {{ item.text }}
               </p>
             </div>
           </div>
 
           <div
-            class="sprk-o-Stack__item sprk-o-Stack__item--three-fourths@m sprk-o-Stack sprk-o-Stack--medium"
+            class="sprk-o-Stack__item sprk-o-Stack__item--seven-tenths@m sprk-o-Stack sprk-o-Stack--medium"
           >
             <div
               *ngIf="localLinks"
-              class="sprk-o-Stack__item sprk-o-Stack sprk-o-Stack--split@m"
+              class="sprk-o-Stack__item sprk-o-Stack sprk-o-Stack--medium sprk-o-Stack--split@m"
             >
               <div
                 *ngFor="let item of localLinks"
-                class="sprk-o-Stack__item sprk-o-Stack__item--third@m sprk-o-Box sprk-o-Stack sprk-o-Stack--large"
+                class="sprk-o-Stack__item sprk-o-Stack__item--third@m sprk-o-Box sprk-u-PaddingRight--a sprk-o-Stack sprk-o-Stack--large"
               >
-                <h3
-                  class="sprk-o-Stack__item sprk-b-TypeBodyOne sprk-u-TextTransform--uppercase"
-                >
+                <h3 class="sprk-o-Stack__item sprk-b-TypeBodyOne">
                   {{ item.heading }}
                 </h3>
 
                 <ul
-                  class="sprk-o-Stack__item sprk-o-Stack sprk-o-Stack--medium sprk-b-List sprk-b-List--bare"
+                  class="sprk-o-Stack__item sprk-o-Stack sprk-o-Stack--misc-a sprk-b-List sprk-b-List--bare"
                 >
                   <li
                     class="sprk-o-Stack__item"
@@ -89,11 +85,11 @@ import { Component, Input } from '@angular/core';
             </div>
 
             <div
-              class="sprk-o-Stack__item sprk-o-Stack sprk-o-Stack--large sprk-o-Box"
+              class="sprk-o-Stack__item sprk-o-Stack sprk-o-Stack--misc-a sprk-o-Box"
             >
               <h3
                 *ngIf="socialLinks"
-                class="sprk-o-Stack__item sprk-b-TypeBodyOne sprk-u-TextTransform--uppercase"
+                class="sprk-o-Stack__item sprk-b-TypeBodyOne"
               >
                 {{ connectHeading }}
               </h3>
@@ -125,51 +121,26 @@ import { Component, Input } from '@angular/core';
                     </a>
                   </li>
                 </ul>
-
-                <ul
-                  *ngIf="badgeLinks"
-                  class="sprk-o-Stack__item sprk-o-Stack__item--flex@m sprk-o-Stack sprk-o-Stack--end-row@m sprk-o-Stack--split@xxs sprk-o-Stack--medium sprk-b-List sprk-b-List--bare"
-                >
-                  <li
-                    *ngFor="let item of badgeLinks"
-                    class="sprk-o-Stack__item"
-                  >
-                    <a
-                      class="sprk-b-Link sprk-b-Link--plain"
-                      href="{{ item.href }}"
-                      attr.data-analytics="{{ item.analytics }}"
-                    >
-                      <sprk-icon
-                        *ngIf="item.icon"
-                        iconType="{{ item.icon }}"
-                        additionalClasses="{{ item.iconCSS }}"
-                      ></sprk-icon>
-                      <span *ngIf="item.icon" class="sprk-u-ScreenReaderText">{{
-                        item.iconScreenReaderText
-                      }}</span>
-                    </a>
-                  </li>
-                </ul>
               </div>
             </div>
           </div>
         </div>
 
+        <span class="sprk-c-Divider sprk-u-mvn" data-id="divider-1"></span>
+
         <div
-          class="sprk-o-Stack__item sprk-o-Stack sprk-o-Stack--large sprk-o-Box"
+          class="sprk-o-Stack__item sprk-o-Stack sprk-o-Stack--misc-b sprk-o-Box sprk-u-PaddingTop--b"
         >
           <div
             *ngIf="awards"
-            class="sprk-o-Stack__item sprk-o-Stack sprk-o-Stack--medium"
+            class="sprk-o-Stack__item sprk-o-Stack sprk-o-Stack--large"
           >
-            <h3
-              class="sprk-o-Stack__item sprk-b-TypeBodyOne sprk-u-TextTransform--uppercase"
-            >
+            <h3 class="sprk-o-Stack__item sprk-b-TypeBodyOne">
               {{ awardsHeading }}
             </h3>
 
             <div
-              class="sprk-o-Stack__item sprk-o-Stack sprk-o-Stack--medium sprk-o-Stack--split@s"
+              class="sprk-o-Stack__item sprk-o-Stack sprk-o-Stack--medium sprk-o-Stack--split@s sprk-u-mbm"
             >
               <a
                 *ngFor="let award of awards"
@@ -185,20 +156,20 @@ import { Component, Input } from '@angular/core';
                 />
               </a>
             </div>
-          </div>
 
-          <div
-            *ngFor="let toggle of disclaimerToggle"
-            class="sprk-o-Stack__item"
-          >
-            <sprk-toggle
-              title="{{ toggle.title }}"
-              analyticsString="{{ toggle.analytics }}"
-              iconClass="sprk-c-Icon--current-color"
-              titleFontClass="sprk-b-TypeBodyFour"
+            <div
+              *ngFor="let toggle of disclaimerToggle"
+              class="sprk-o-Stack__item"
             >
-              <p class="sprk-b-TypeBodyFour">{{ toggle.body }}</p>
-            </sprk-toggle>
+              <sprk-toggle
+                title="{{ toggle.title }}"
+                analyticsString="{{ toggle.analytics }}"
+                iconClass="sprk-c-Icon--current-color"
+                titleFontClass="sprk-b-TypeBodyFour"
+              >
+                <p class="sprk-b-TypeBodyFour">{{ toggle.body }}</p>
+              </sprk-toggle>
+            </div>
           </div>
 
           <div
@@ -212,6 +183,28 @@ import { Component, Input } from '@angular/core';
               {{ disclaimer.text }}
             </p>
           </div>
+
+          <ul
+            *ngIf="badgeLinks"
+            class="sprk-o-Stack__item sprk-o-Stack__item--flex@m sprk-o-Stack sprk-o-Stack--split@xxs sprk-o-Stack--medium sprk-b-List sprk-b-List--bare"
+          >
+            <li *ngFor="let item of badgeLinks" class="sprk-o-Stack__item">
+              <a
+                class="sprk-b-Link sprk-b-Link--plain"
+                href="{{ item.href }}"
+                attr.data-analytics="{{ item.analytics }}"
+              >
+                <sprk-icon
+                  *ngIf="item.icon"
+                  iconType="{{ item.icon }}"
+                  additionalClasses="{{ item.iconCSS }}"
+                ></sprk-icon>
+                <span *ngIf="item.icon" class="sprk-u-ScreenReaderText">{{
+                  item.iconScreenReaderText
+                }}</span>
+              </a>
+            </li>
+          </ul>
         </div>
       </footer>
     </div>
@@ -245,7 +238,7 @@ export class SparkFooterComponent {
 
   getClasses(): string {
     const classArray: string[] = [
-      'sprk-o-CenteredColumn sprk-o-Stack sprk-o-Stack--medium'
+      'sprk-o-CenteredColumn sprk-o-Stack sprk-o-Stack--misc-b'
     ];
 
     if (this.additionalClasses) {
