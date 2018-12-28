e6dbfa9df8bc0370a847410148a377d135b126d5
RVMendoza <RVxMendoza@gmail.com>
2018-12-28 13:02:45 -0500

    [#917] - Update accordion styles

--- packages/spark-core/tests/toggle.tests.js
+++ packages/spark-core/tests/toggle.tests.js
@@ -7,9 +7,7 @@ const proxyquireStrict = require('proxyquire').noCallThru();
 describe('Toggle init', () => {
   const domSliderStub = {};
 
-  const {
-    toggle,
-  } = proxyquireStrict('../components/toggle', { 'dom-slider': domSliderStub });
+  const { toggle } = proxyquireStrict('../components/toggle', { 'dom-slider': domSliderStub });
 
   afterEach(() => {
     document.querySelectorAll.restore();
@@ -70,14 +68,18 @@ describe('Toggle tests', () => {
     content.setAttribute('data-sprk-toggle', 'content');
     content.textContent = 'This is the toggle content..';
     content.classList.add('sprk-b-TypeBodyFour', 'sprk-u-pts');
-    content.slideToggle = () => new Promise((resolve) => { resolve(); });
+    content.slideToggle = () => new Promise((resolve) => {
+      resolve();
+    });
 
     contentAccordion = document.createElement('p');
     contentAccordion.setAttribute('data-sprk-toggle', 'content');
     contentAccordion.textContent = 'This is the toggle accordion content.';
     contentAccordion.classList.add('sprk-b-TypeBodyTwo', 'sprk-c-Accordion__content');
     contentAccordion.style.display = 'none';
-    contentAccordion.slideToggle = () => new Promise((resolve) => { resolve(); });
+    contentAccordion.slideToggle = () => new Promise((resolve) => {
+      resolve();
+    });
 
     icon = document.createElement('svg');
     icon.setAttribute('data-sprk-toggle', 'icon');
@@ -120,12 +122,19 @@ describe('Toggle tests', () => {
   it('should toggle accordion item open', () => {
     handleToggleClick(contentAccordion, iconAccordion, iconAccordionUseElement, triggerAccordion);
     expect(containerAccordion.classList.contains('sprk-c-Accordion__item--open')).eql(true);
-    expect(iconAccordionUseElement.getAttribute('xlink:href')).eql('#chevron-down-circle-filled');
+    expect(iconAccordionUseElement.getAttribute('xlink:href')).eql('#chevron-down-circle-toggle');
   });
 
   it('should toggle accordion icon open', () => {
-    toggleIconType(iconAccordion, iconAccordionUseElement, 'chevron-down-circle', 'chevron-down-circle-filled');
-    expect(iconAccordionUseElement.getAttribute('xlink:href')).eql('#chevron-down-circle-filled');
+    toggleIconType(
+      iconAccordion,
+      iconAccordionUseElement,
+      'chevron-up-circle-toggle',
+      'chevron-down-circle-toggle',
+    );
+    expect(iconAccordionUseElement.getAttribute('xlink:href')).eql(
+      '#chevron-up-circle-toggle-toggle',
+    );
   });
 
   it('should add listener to toggle trigger', () => {
