dfceb4467fd9c24939698d31707df8a05878e388
Amber Febbraro <amberfebbraro@quickenloans.com>
2019-01-17 10:17:58 -0500

    add link

--- src/react/projects/spark-core-react/src/SprkLink/SprkLink.js
+++ src/react/projects/spark-core-react/src/SprkLink/SprkLink.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-class SprkLink extends React.Component {
-  getClassNames () {
-    if (this.props.variant === 'cats') {
-      return 'sprk-c-Link';
-    }
-  }
+const SprkLink = (props) => {
+  const classNames = classnames(
+    'sprk-b-Link',
+    props.additionalClasses, {
+      'sprk-b-Link--simple': props.variant === 'simple',
+      'sprk-b-Link--plain': props.variant === 'plain',
+      'sprk-b-Link--disabled': props.variant === 'disabled',
+      'sprk-b-Link--simple sprk-b-Link--has-icon': props.variant === 'has-icon',
+    },
+  );
 
-  render() {
-    return (
-      <a class={this.getClassNames} href="#nogo">Cats</a>
-    );
-  }
+  return (
+    <a className={classNames} href={props.href} target={props.target}>
+      {props.children}
+    </a>
+  );
 }
 
 SprkLink.propTypes = {
-  variant: PropTypes.string
+  // The children that will be rendered inside the link
+  children: PropTypes.node,
+  // The link variant that determines the class names
+  variant: PropTypes.oneOf(['simple', 'has-icon', 'plain', 'disabled']),
+  // The string to use for the href attribute
+  href: PropTypes.string,
+  // Determines where to display the linked URL ex. _blank
+  target: PropTypes.string,
+  // Any additional classes to add to the link
+  additionalClasses: PropTypes.string
 };
 
 export default SprkLink;
--- src/react/src/routes/SprkLinkDocs/SprkLinkDocs.js
+++ src/react/src/routes/SprkLinkDocs/SprkLinkDocs.js
@@ -6,7 +6,31 @@ import { SprkLink } from '@sparkdesignsystem/spark-core-react';
 const SprkLinkDocs = () => {
   return (
     <CentralColumnLayout>
-      <SprkLink/>
+      <div className="sprk-u-mbm">
+        <SprkLink>
+          Base Link
+        </SprkLink>
+      </div>
+
+      <div className="sprk-u-mbm">
+        <SprkLink variant="simple">
+          Simple Link
+        </SprkLink>
+      </div>
+
+      <div className="sprk-u-mbm">
+        <SprkLink variant="has-icon">
+          {/* TODO: SVG Component*/}
+          <svg className="sprk-o-StrokeIconSymbol sprk-c-Icon sprk-c-Icon--l sprk-u-mrs sprk-c-Icon--stroke-current-color" viewBox="0 0 64 64" id="communication" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke="#c8102e" strokeMiterlimit="10" strokeWidth="2" d="M25.7 41.1v7.01h23.55l7.56 6.3v-6.3H62V23.67H47.44"></path><path fill="none" stroke="#c8102e" strokeMiterlimit="10" strokeWidth="2" d="M47.19 9.59H2V40.7h9.63v8.56L25.9 40.7h21.29V9.59z"></path></svg>
+          Icon With Text Link
+        </SprkLink>
+      </div>
+
+      <div className="sprk-u-mbm">
+        <SprkLink variant="disabled">
+          Disabled Link
+        </SprkLink>
+      </div>
     </CentralColumnLayout>
   );
 };
