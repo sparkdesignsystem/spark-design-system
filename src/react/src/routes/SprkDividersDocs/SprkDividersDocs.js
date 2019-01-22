7cb5fecfeeb707bb4cab1a0f7ad08349d89c22ea
Corcoran <kcorcoran@c02xr4afjgh5.mi.corp.rockfin.com>
2019-01-22 17:24:28 -0500

    add header to divider docs

--- src/react/src/routes/SprkDividersDocs/SprkDividersDocs.js
+++ src/react/src/routes/SprkDividersDocs/SprkDividersDocs.js
@@ -7,6 +7,7 @@ const SprkDividersDocs = () => {
   return (
     <CentralColumnLayout>
       <div className="sprk-u-mbm">
+        <h2 className="drizzle-b-h2">Dividers</h2>
         <SprkDividers idString="divider-1"></SprkDividers>
       </div>
     </CentralColumnLayout>
