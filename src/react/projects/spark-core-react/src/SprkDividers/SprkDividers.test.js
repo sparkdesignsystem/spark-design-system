aacb732343a2fa5d3175f1ef234feb80a8c66c65
Corcoran <kcorcoran@c02xr4afjgh5.mi.corp.rockfin.com>
2019-01-23 11:31:17 -0500

    update unit tests

--- src/react/projects/spark-core-react/src/SprkDividers/SprkDividers.test.js
+++ src/react/projects/spark-core-react/src/SprkDividers/SprkDividers.test.js
@@ -5,7 +5,12 @@ import SprkLink from './SprkLink';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-it('should display a span element with the correct base class', () => {
-  const wrapper = shallow(<SprkLink/>);
+it('should display a span element with the correct base class when element is span', () => {
+  const wrapper = shallow(<SprkLink element="span"/>);
   expect(wrapper.find('span.sprk-c-Dividers').length).toBe(1);
+});
+
+it('should display a div element with the correct base class when element is div', () => {
+  const wrapper = shallow(<SprkLink element="div"/>);
+  expect(wrapper.find('div.sprk-c-Dividers').length).toBe(1);
 });
