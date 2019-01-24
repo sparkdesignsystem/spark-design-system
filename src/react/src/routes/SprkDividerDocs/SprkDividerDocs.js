b38f6995f1d51144bd08c98ee75aee090a3e9ab3
Corcoran <kcorcoran@c02xr4afjgh5.mi.corp.rockfin.com>
2019-01-24 11:55:06 -0500

    Update divs to ExampleContainer component

--- src/react/src/routes/SprkDividerDocs/SprkDividerDocs.js
+++ src/react/src/routes/SprkDividerDocs/SprkDividerDocs.js
@@ -2,19 +2,20 @@ import React from 'react';
 import CentralColumnLayout from '../../containers/CentralColumnLayout/CentralColumnLayout';
 
 import { SprkDivider } from '@sparkdesignsystem/spark-core-react';
+import ExampleContainer from '../../containers/ExampleContainer/ExampleContainer';
 
 const SprkDividerDocs = () => {
   return (
     <CentralColumnLayout>
-      <div className="sprk-u-mbm">
+      <ExampleContainer>
         <h2 className="drizzle-b-h2">Divider as span</h2>
         <SprkDivider idString="divider-1" element="span"></SprkDivider>
-      </div>
+      </ExampleContainer>
 
-      <div className="sprk-u-mbm">
+      <ExampleContainer>
         <h2 className="drizzle-b-h2">Divider as hr</h2>
         <SprkDivider idString="divider-1" element="hr"></SprkDivider>
-      </div>
+      </ExampleContainer>
     </CentralColumnLayout>
   )
 }
