f88d1d096044910588433681516c787d97008ca8
Corcoran <kcorcoran@c02xr4afjgh5.mi.corp.rockfin.com>
2019-01-29 11:58:35 -0500

    add additional unit tests

--- src/react/projects/spark-core-react/src/SprkOrderedList/SprkOrderedList.test.js
+++ src/react/projects/spark-core-react/src/SprkOrderedList/SprkOrderedList.test.js
@@ -15,3 +15,14 @@ it('should display a ol element with correct classes when listType is indented',
   expect(wrapper.find('ol').hasClass('sprk-b-List')).toBe(true);
   expect(wrapper.find('ol').hasClass('sprk-b-List--indented')).toBe(true);
 });
+
+it('should add classes to ol element if additionalClasses has a value', () => {
+  const wrapper = shallow(<SprkOrderedList additionalClasses="additionalClass" />);
+  expect(wrapper.find('ol').hasClass('sprk-b-List')).toBe(true);
+  expect(wrapper.find('ol').hasClass('additionalClass')).toBe(true);
+});
+
+it('should add data-id to ol element if idString has a value', () => {
+  const wrapper = shallow(<SprkOrderedList idString="orderedlist-1" />);
+  expect(wrapper.find('ol[data-id="orderedlist-1"]').length).toBe(1);
+});
