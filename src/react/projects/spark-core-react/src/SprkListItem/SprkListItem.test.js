69ae2bcd13ac251c535d943717479b53c43175d2
Corcoran <kcorcoran@c02xr4afjgh5.mi.corp.rockfin.com>
2019-01-29 11:52:35 -0500

    add additional unit tests

--- src/react/projects/spark-core-react/src/SprkListItem/SprkListItem.test.js
+++ src/react/projects/spark-core-react/src/SprkListItem/SprkListItem.test.js
@@ -9,3 +9,13 @@ it('should display a li element', () => {
   const wrapper = shallow(<SprkListItem/>);
   expect(wrapper.find('li').length).toBe(1);
 });
+
+it('should add classes to li element if additionalClasses has a value', () => {
+  const wrapper = shallow(<SprkListItem additionalClasses="additionalClass" />);
+  expect(wrapper.find('li').hasClass('additionalClass')).toBe(true);
+});
+
+it('should add data-id to li element if idString has a value', () => {
+  const wrapper = shallow(<SprkListItem idString="list-item-1" />);
+  expect(wrapper.find('li[data-id="list-item-1"]').length).toBe(1);
+});
