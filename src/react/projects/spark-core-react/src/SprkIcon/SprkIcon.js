9d6b8db4dc58e3791062e4c9fe567b9ea8506df7
Rob Copeland <robcopeland@fastmail.com>
2019-01-30 14:17:58 -0500

    setting up for unit tests, SprkIcon done

--- src/react/projects/spark-core-react/src/SprkIcon/SprkIcon.js
+++ src/react/projects/spark-core-react/src/SprkIcon/SprkIcon.js
@@ -2,22 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-function SprkIcon({additionalClasses, viewBox, iconType, ...rest}) {
+function SprkIcon({
+                    additionalClasses,
+                    children,
+                    iconType,
+                    idString,
+                    viewBox,
+                    ...rest
+                  }) {
   return (
-    <svg className={classNames('sprk-c-Icon', additionalClasses)} viewBox={viewBox} {...rest}>
+    <svg
+      className={classNames('sprk-c-Icon', additionalClasses)}
+      viewBox={viewBox}
+      data-id={idString}
+      {...rest}>
       <use xlinkHref={'#' + iconType}/>
     </svg>
   );
 }
 
 SprkIcon.defaultProps = {
+  additionalClasses: "",
   viewBox: '0 0 64 64'
 };
 
 SprkIcon.propTypes = {
   additionalClasses: PropTypes.string,
-  viewBox: PropTypes.string,
-  iconType: PropTypes.string
+  children: PropTypes.node,
+  iconType: PropTypes.string.isRequired,
+  idString: PropTypes.string,
+  viewBox: PropTypes.string
 };
 
 export default SprkIcon;
