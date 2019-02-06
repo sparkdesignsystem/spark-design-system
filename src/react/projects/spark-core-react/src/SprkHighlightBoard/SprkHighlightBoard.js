752346c0f88450b10f61a3112523245c49470e1b
Bob Wise <bobwise@quickenloans.com>
2019-02-06 12:17:57 -0500

    adding logic around properties + unit tests

--- src/react/projects/spark-core-react/src/SprkHighlightBoard/SprkHighlightBoard.js
+++ src/react/projects/spark-core-react/src/SprkHighlightBoard/SprkHighlightBoard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { SprkButton } from '@sparkdesignsystem/spark-core-react';
+import warning from 'warning';
 
 const SprkHighlightBoard = (props) => {
   const {
@@ -24,6 +25,11 @@ const SprkHighlightBoard = (props) => {
     ...other
   } = props;
 
+  warning(
+    (imgSrc && imgAlt) || (!imgSrc && !imgAlt),
+    'SprkHighlightBoard: If imgSrc is provided, then imgAlt is required (and vice versa).'
+  );
+
   const classNames = classnames(
     'sprk-c-HighlightBoard',
     'sprk-u-mbm',
@@ -34,69 +40,67 @@ const SprkHighlightBoard = (props) => {
 
   return (
     <div className={classNames} data-id={idString} {...other}>
-      {imgSrc && 
+      {imgSrc &&
         <img
           className='sprk-c-HighlightBoard__image'
           src={imgSrc}
           alt={imgAlt} />
       }
 
-      { (heading || ctaText || ctaText2) &&
-        <div className='sprk-c-HighlightBoard__content sprk-o-Stack sprk-o-Stack--large'>
-          { heading && 
-            <h1 className="sprk-b-TypeDisplayOne sprk-c-HighlightBoard__heading sprk-o-Stack__item">
-              {heading}
-            </h1>
+      <div className='sprk-c-HighlightBoard__content sprk-o-Stack sprk-o-Stack--large'>
+        { heading &&
+          <h1 className="sprk-b-TypeDisplayOne sprk-c-HighlightBoard__heading sprk-o-Stack__item">
+            {heading}
+          </h1>
+        }
+
+        <div className='sprk-o-Stack__item'>
+          { children }
+        </div>
+
+        { (ctaText || ctaText2) &&
+        <div className= {
+          classnames(
+            'sprk-o-Stack__item',
+            'sprk-o-Stack',
+            'sprk-o-Stack--medium',
+            'sprk-o-Stack--split@xs',
+            'sprk-o-Stack--center-column',
+            {'sprk-o-Stack--center-row' : variant === 'noImage' || variant === 'stacked'}
+          )
+        } >
+
+          { ctaText &&
+            <div className="sprk-o-Stack__item sprk-c-HighlightBoard__cta">
+              <SprkButton
+                element="a"
+                href={ctaHref}
+                analyticsString={ctaAnalytics}
+                idString={ctaIdString}
+                additionalClasses='sprk-c-Button--full@sm' >
+
+                {ctaText}
+              </SprkButton>
+            </div>
           }
 
-          <div className='sprk-o-Stack__item'>
-            { children }
-          </div>
+          { ctaText2 &&
+            <div className="sprk-o-Stack__item sprk-c-HighlightBoard__cta">
+              <SprkButton
+                variant='secondary'
+                element="a"
+                href={ctaHref2}
+                analyticsString={ctaAnalytics2}
+                idString={ctaIdString2}
+                additionalClasses='sprk-c-Button--full@sm' >
 
-          { (ctaText || ctaText2) && 
-          <div className= {
-            classnames(
-              'sprk-o-Stack__item',
-              'sprk-o-Stack',
-              'sprk-o-Stack--medium',
-              'sprk-o-Stack--split@xs',
-              'sprk-o-Stack--center-column',
-              {'sprk-o-Stack--center-row' : variant === 'noImage' || variant === 'stacked'}
-            )
-          } >
-            
-            { ctaText &&
-              <div className="sprk-o-Stack__item sprk-c-HighlightBoard__cta">
-                <SprkButton
-                  element="a"
-                  href={ctaHref}
-                  analyticsString={ctaAnalytics}
-                  idString={ctaIdString}
-                  additionalClasses='sprk-c-Button--full@sm' >
-
-                  {ctaText}
-                </SprkButton>
-              </div>
-            }
-
-            { ctaText2 &&
-              <div className="sprk-o-Stack__item sprk-c-HighlightBoard__cta">
-                <SprkButton
-                  variant='secondary'
-                  element="a"
-                  href={ctaHref2}
-                  analyticsString={ctaAnalytics2}
-                  idString={ctaIdString2}
-                  additionalClasses='sprk-c-Button--full@sm' >
-
-                  {ctaText2}
-                </SprkButton>
-              </div>
-            }
-          </div>
+                {ctaText2}
+              </SprkButton>
+            </div>
           }
         </div>
-      }
+        }
+      </div>
     </div>
   );
 }
@@ -104,9 +108,9 @@ const SprkHighlightBoard = (props) => {
 SprkHighlightBoard.propTypes = {
   // Child elements are rendered in the content section of the component.
   children: PropTypes.node,
-  // The source for the image
+  // The source for the image - required if imgAlt is provided
   imgSrc: PropTypes.string,
-  // The alt text for the image
+  // The alt text for the image - required if imgSrc is provided
   imgAlt: PropTypes.string,
   // The heading text
   heading: PropTypes.string,
--- src/react/projects/spark-core-react/src/SprkHighlightBoard/SprkHighlightBoard.test.js
+++ src/react/projects/spark-core-react/src/SprkHighlightBoard/SprkHighlightBoard.test.js
@@ -2,8 +2,18 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import SprkHighlightBoard from './SprkHighlightBoard';
+import sinon from 'sinon';
 
 Enzyme.configure({ adapter: new Adapter() });
+var stub;
+
+beforeEach(() => {
+  stub = sinon.stub(console, 'error');
+});
+
+afterEach(() => {
+  stub.restore();
+});
 
 it('should display a div element with the correct base class', () => {
   const wrapper = shallow(<SprkHighlightBoard />);
@@ -17,33 +27,17 @@ it('should not render an image if imgSrc is not provided', () => {
   expect(img.length).toBe(0);
 });
 
+it ('should render children nodes if no other props are provided.', () => {
+  const wrapper = shallow(<SprkHighlightBoard>foobar</SprkHighlightBoard>);
+  expect(wrapper.text()).toBe('foobar');
+});
+
 it('should render an image if an imgSrc is provided', () => {
-  const wrapper = shallow(<SprkHighlightBoard imgSrc='foo'/>);
+  const wrapper = shallow(<SprkHighlightBoard imgSrc='foo' imgAlt='bar'/>);
   const img = wrapper.find('img');
 
   expect(img.length).toBe(1);
   expect(img.hasClass('sprk-c-HighlightBoard__image')).toBe(true);
-});
-
-it('should not render a content section if there is no header or ctas', () => {
-  const wrapper = shallow(<SprkHighlightBoard/>);
-  const contentDiv = wrapper.find('div.sprk-c-HighlightBoard__content');
-
-  expect(contentDiv.length).toBe(0);
-});
-
-it('should render a content section when provided a header', () => {
-  const wrapper = shallow(<SprkHighlightBoard heading='This is a test'/>);
-  const contentDiv = wrapper.find('div.sprk-c-HighlightBoard__content');
-
-  expect(contentDiv.length).toBe(1);
-});
-
-it('should render a content section when provided a cta text', () => {
-  const wrapper = shallow(<SprkHighlightBoard ctaText='this is a test'/>);
-  const contentDiv = wrapper.find('div.sprk-c-HighlightBoard__content');
-
-  expect(contentDiv.length).toBe(1);
 });
 
 it('should render both ctas when provided text for both', () => {
@@ -51,4 +45,18 @@ it('should render both ctas when provided text for both', () => {
   const contentDiv = wrapper.find('div.sprk-c-HighlightBoard__cta');
 
   expect(contentDiv.length).toBe(2);
+});
+
+it ('should error if imgSrc is provided without imgAlt', () => {
+  const wrapper = shallow(<SprkHighlightBoard imgSrc='foo'/>);
+  const actual = stub.getCall(0).args[0];
+
+  expect(actual.includes('If imgSrc is provided, then imgAlt is required')).toBe(true);
+});
+
+it ('should error if imgAlt is provided without imgSrc', () => {
+  const wrapper = shallow(<SprkHighlightBoard imgAlt='foo'/>);
+  const actual = stub.getCall(0).args[0];
+
+  expect(actual.includes('If imgSrc is provided, then imgAlt is required')).toBe(true);
 });
