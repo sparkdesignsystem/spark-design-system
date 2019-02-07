037ddf11d87729253a84b17da3e108745c8cf256
Amber Febbraro <amberfebbraro@quickenloans.com>
2019-02-07 08:51:15 -0500

    bring Tab Buttons to 100% coverage

--- src/react/projects/spark-core-react/src/SprkTabs/components/SprkTabsButton/SprkTabsButton.test.js
+++ src/react/projects/spark-core-react/src/SprkTabs/components/SprkTabsButton/SprkTabsButton.test.js
@@ -4,6 +4,7 @@ import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import SprkTabs from '../../SprkTabs';
 import SprkTabsPanel from '../SprkTabsPanel/SprkTabsPanel';
+import SprkTabsButton from './SprkTabsButton';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -16,5 +17,20 @@ describe('SprkTabs Component', () => {
       </SprkTabs>,
     );
     expect(wrapper.find('.sprk-c-Tabs__button').length).toBe(2);
+  });
+
+  it('should focus the next Tab Button when the right arrow key is pressed', () => {
+    const wrapper = mount(
+      <SprkTabs>
+        <SprkTabsPanel isDefaultActive tabBtnChildren="Tab 1">Test Content 1</SprkTabsPanel>
+        <SprkTabsPanel tabBtnChildren="Tab 2">Test Content 2</SprkTabsPanel>
+      </SprkTabs>,
+    );
+    // First, tab into the Tab Button area
+    wrapper.find('div.sprk-c-Tabs__buttons').simulate('keydown', { keyCode: 9 });
+    // Second, hit the right arrow key
+    wrapper.find('div.sprk-c-Tabs__buttons').simulate('keydown', { keyCode: 39 });
+    const button = wrapper.find(SprkTabsButton).last();
+    expect(button.getDOMNode().classList.contains('sprk-c-Tabs__button--active')).toBe(true);
   });
 });
