bbd5a45ee864cdd68bc06fd787d6e2cf34b70e96
Bob Wise <bobwise@quickenloans.com>
2019-02-11 11:07:55 -0500

    sorting react nav, updating header style

--- src/react/src/components/SiteNav/SiteNav.js
+++ src/react/src/components/SiteNav/SiteNav.js
@@ -6,33 +6,13 @@ function SiteNav(props) {
     <nav className="drizzle-c-ReactNav sprk-u-phm">
       <ul className="drizzle-b-List drizzle-b-List--inline drizzle-b-List--spacing-m">
         <li>
-          <Link to="/buttons" className="drizzle-b-Link drizzle-b-Link--light">
-            Buttons
-          </Link>
-        </li>
-        <li>
-          <Link to="/links" className="drizzle-b-Link drizzle-b-Link--light">
-            Links
-          </Link>
-        </li>
-        <li>
-          <Link to="/promos" className="drizzle-b-Link drizzle-b-Link--light">
-            Promos
-          </Link>
-        </li>
-        <li>
-          <Link to="/dividers" className="drizzle-b-Link drizzle-b-Link--light">
-            Dividers
-          </Link>
-        </li>
-        <li>
           <Link to="/alerts" className="drizzle-b-Link drizzle-b-Link--light">
             Alerts
           </Link>
         </li>
         <li>
-          <Link to="/highlightboard" className="drizzle-b-Link drizzle-b-Link--light">
-            Highlight Board
+          <Link to="/buttons" className="drizzle-b-Link drizzle-b-Link--light">
+            Buttons
           </Link>
         </li>
         <li>
@@ -41,8 +21,28 @@ function SiteNav(props) {
           </Link>
         </li>
         <li>
+          <Link to="/dividers" className="drizzle-b-Link drizzle-b-Link--light">
+            Dividers
+          </Link>
+        </li>
+        <li>
+          <Link to="/highlightboard" className="drizzle-b-Link drizzle-b-Link--light">
+            Highlight Board
+          </Link>
+        </li>
+        <li>
+          <Link to="/links" className="drizzle-b-Link drizzle-b-Link--light">
+            Links
+          </Link>
+        </li>
+        <li>
           <Link to="/pagination" className="drizzle-b-Link drizzle-b-Link--light">
             Pagination
+          </Link>
+        </li>
+        <li>
+          <Link to="/promos" className="drizzle-b-Link drizzle-b-Link--light">
+            Promos
           </Link>
         </li>
       </ul>
