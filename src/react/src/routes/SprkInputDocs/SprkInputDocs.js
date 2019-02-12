0e29dd4ee5103607d327edaf86b6169eca48313c
Rob Copeland <robcopeland@fastmail.com>
2019-02-12 10:37:54 -0500

    starting inputs

---
+++ src/react/src/routes/SprkInputDocs/SprkInputDocs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CentralColumnLayout from '../../containers/CentralColumnLayout/CentralColumnLayout';
+import ExampleContainer from '../../containers/ExampleContainer/ExampleContainer';
+
+const SprkInputDocs = () => (
+  <CentralColumnLayout>
+    <ExampleContainer>
+      Inputs!
+    </ExampleContainer>
+  </CentralColumnLayout>
+);
+
+export default SprkInputDocs;
