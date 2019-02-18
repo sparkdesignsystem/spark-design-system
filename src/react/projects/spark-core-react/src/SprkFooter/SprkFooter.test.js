61b886ce29a2e5d31d7debb0073912c2f4f6dc23
Amber Febbraro <amberfebbraro@quickenloans.com>
2019-02-18 10:30:24 -0500

    bring footer unit tests with new subcomponents to 100% coverage

--- src/react/projects/spark-core-react/src/SprkFooter/SprkFooter.test.js
+++ src/react/projects/spark-core-react/src/SprkFooter/SprkFooter.test.js
@@ -3,6 +3,10 @@ import React from 'react';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import SprkFooter from './SprkFooter';
+import SprkIcon from '../SprkIcon/SprkIcon';
+import SprkFooterGlobalSection from './components/SprkFooterGlobalSection/SprkFooterGlobalSection';
+import SprkFooterConnectIcons from './components/SprkFooterConnectIcons/SprkFooterConnectIcons';
+import SprkFooterAwards from './components/SprkFooterAwards/SprkFooterAwards';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -12,5 +16,139 @@ describe('SprkFooter Component', () => {
       <SprkFooter />,
     );
     expect(wrapper.find('.sprk-o-Box.sprk-o-Box--large.sprk-u-BackgroundColor--gray').length).toBe(1);
+  });
+
+  it('should display a Footer element with the global section', () => {
+    const globalItems = {
+      heading: 'Global Links',
+      items: [
+        {
+          mediaType: 'image',
+          mediaSrc: 'https://staging.sparkdesignsystem.com/assets/toolkit/images/spark-logo.svg',
+          altText: 'Spark Logo',
+          mediaAddClasses: 'drizzle-c-Logo drizzle-c-Logo--small',
+          description: 'Lorem ipsum dolor sit amet, consectetur.',
+        },
+      ],
+    };
+    const wrapper = mount(
+      <SprkFooter
+        globalItems={globalItems}
+      />,
+    );
+    expect(wrapper.find(SprkFooterGlobalSection).length).toBe(1);
+  });
+
+  it('should display a Footer element with the column links section', () => {
+    const linkColumns = [
+      {
+        heading: 'Site Links',
+        links: [
+          {
+            href: '#nogo',
+            text: 'About This.',
+          },
+          {
+            href: '#nogo',
+            text: 'About This Other Thing',
+          },
+          {
+            href: '#nogo',
+            text: 'About That',
+          },
+          {
+            href: '#nogo',
+            text: 'Link Item',
+          },
+          {
+            href: '#nogo',
+            text: 'This Link Item',
+          },
+        ],
+      },
+    ];
+    const wrapper = mount(
+      <SprkFooter
+        linkColumns={linkColumns}
+      />,
+    );
+    expect(wrapper.find('h3').getDOMNode().innerHTML).toBe('Site Links');
+  });
+
+  it('should display a Footer element with the connect icons section', () => {
+    const connectIcons = {
+      heading: 'Connect With Us',
+      icons: [
+        {
+          href: '#nogo',
+          name: 'facebook',
+          screenReaderText: 'Facebook',
+        },
+      ],
+    };
+    const wrapper = mount(
+      <SprkFooter
+        connectIcons={connectIcons}
+      />,
+    );
+    expect(wrapper.find(SprkFooterConnectIcons).length).toBe(1);
+  });
+
+  it('should display a Footer element with the awards section', () => {
+    const awards = {
+      heading: 'Awards Heading Title',
+      images: [
+        {
+          href: '#nogo',
+          src: 'https://staging.sparkdesignsystem.com/assets/toolkit/images/spark-logo.svg',
+          altText: 'Spark Logo',
+          addClasses: 'drizzle-c-Logo',
+        },
+        {
+          href: '#nogo',
+          src: 'https://staging.sparkdesignsystem.com/assets/toolkit/images/spark-logo.svg',
+          altText: 'Spark Logo',
+          addClasses: 'drizzle-c-Logo',
+        },
+      ],
+      disclaimerTitle: 'My Award Disclaimer',
+      disclaimerAnalytics: 'test',
+      disclaimerText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in laoreet ante, non feugiat neque. Suspendisse et ipsum leo. Quisque non consectetur justo.',
+    };
+    const wrapper = mount(
+      <SprkFooter
+        awards={awards}
+      />,
+    );
+    expect(wrapper.find(SprkFooterAwards).length).toBe(1);
+  });
+
+  it('should display a Footer element with the additional icons section', () => {
+    const additionalIcons = [
+      {
+        name: 'house',
+        href: '#nogo',
+        addClasses: '',
+        screenReaderText: 'House',
+      },
+    ];
+    const wrapper = mount(
+      <SprkFooter
+        additionalIcons={additionalIcons}
+      />,
+    );
+    expect(wrapper.find(SprkIcon).length).toBe(1);
+  });
+
+  it('should display a Footer element with the paragraphs section', () => {
+    const paragraphs = [
+      { text: 'Lorem ipsum dolor' },
+    ];
+    const wrapper = mount(
+      <SprkFooter
+        paragraphs={paragraphs}
+      />,
+    );
+    expect(wrapper.find('p').length).toBe(1);
   });
 });
