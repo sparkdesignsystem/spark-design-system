fe91cb6776cf6976da74d62f9bd7d4e5b47873b5
Amber Febbraro <amberfebbraro@quickenloans.com>
2019-03-08 10:17:53 -0500

    rebase

--- src/react/projects/spark-core-react/src/index.js
+++ src/react/projects/spark-core-react/src/index.js
@@ -20,6 +20,7 @@ export { default as SprkSpinner } from './SprkSpinner/SprkSpinner';
 export { default as SprkList } from './SprkList/SprkList';
 export { default as SprkListItem } from './SprkList/components/SprkListItem/SprkListItem';
 export { default as SprkTabs } from './SprkTabs/SprkTabs';
+export { default as SprkTabsPanel } from './SprkTabs/components/SprkTabsPanel/SprkTabsPanel';
 export { default as SprkFooter } from './SprkFooter/SprkFooter';
 export { default as SprkToggle } from './SprkToggle/SprkToggle';
 export { default as SprkAccordion } from './SprkAccordion/SprkAccordion';
