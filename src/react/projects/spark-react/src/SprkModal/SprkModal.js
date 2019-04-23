156938f074049409cdaad6418d54ac6c1530a88d
Bob Wise <bobwise@quickenloans.com>
2019-04-23 13:42:17 -0400

    lodash fix

--- src/react/projects/spark-react/src/SprkModal/SprkModal.js
+++ src/react/projects/spark-react/src/SprkModal/SprkModal.js
@@ -4,7 +4,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import noop from 'lodash';
+import * as _ from 'lodash';
 import { getFocusableEls, isActiveElement, isTabPressed, isEscPressed } from '@sparkdesignsystem/spark-core';
 import SprkSpinner from '../SprkSpinner/SprkSpinner';
 import CloseButton from './CloseButton';
@@ -12,23 +12,10 @@ import ModalFooter from './ModalFooter';
 import Mask from './Mask';
 
 class SprkModal extends Component {
-  static makeid() {
-    const length = 3;
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let text = '';
-    let i = 0;
-
-    for (i = 0; i < length; i += 1) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-
-    return text;
-  }
-
   constructor(props) {
     super(props);
 
-    this.aria_id = SprkModal.makeid();
+    this.aria_id = _.uniqueId();
     this.closeButtonRef = React.createRef();
     this.footerRef = React.createRef();
     this.containerRef = React.createRef();
@@ -201,7 +188,7 @@ class SprkModal extends Component {
     this.attachListeners();
 
     return (
-      <div>
+      <React.Fragment>
         <div
           className={
             classnames(
@@ -256,7 +243,7 @@ class SprkModal extends Component {
         </div>
 
         <Mask clicked={this.cancel} />
-      </div>
+      </React.Fragment>
     );
   }
 }
@@ -288,18 +275,13 @@ SprkModal.propTypes = {
 };
 
 SprkModal.defaultProps = {
-  additionalClasses: null,
-  analyticsString: null,
-  children: [],
-  idString: null,
-  title: '',
   variant: 'choice',
   confirmText: 'Confirm',
   cancelText: 'Cancel',
   // Modals are typically hidden until invoked
   isVisible: false,
-  confirmClick: noop,
-  cancelClick: noop,
+  confirmClick: _.noop,
+  cancelClick: _.noop,
   shouldReturnFocusOnClose: true,
 };
 
