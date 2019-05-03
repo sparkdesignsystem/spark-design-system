a7d33ff20ebb10bf7a22681cb85bdb2f1982f2bf
Amber Febbraro <amberfebbraro@quickenloans.com>
2019-05-03 09:33:04 -0400

    build new module files for extras components that are now in spark-angular

--- src/angular/projects/spark-angular/src/lib/components/sprk-card/sprk-card.module.ts
+++ src/angular/projects/spark-angular/src/lib/components/sprk-card/sprk-card.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SparkAngularModule } from '@sparkdesignsystem/spark-angular';
-import { SparkCardComponent } from './sprk-card.component';
+import { SprkLinkModule } from '../sprk-link/sprk-link.module';
+import { SprkIconModule } from '../sprk-icon/sprk-icon.module';
+import { SprkCardComponent } from './sprk-card.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, SparkAngularModule],
-  declarations: [SparkCardComponent],
-  exports: [SparkCardComponent]
+  imports: [CommonModule, RouterModule, SprkIconModule, SprkLinkModule],
+  declarations: [SprkCardComponent],
+  exports: [SprkCardComponent]
 })
 export class SprkCardModule {}
