28d1cb01ddfd126d45b83aa5397d384c4340a1bc
corcorankt1 <guern1kn@gmail.com>
2019-05-17 10:12:20 -0400

    add isElementVisible utility

---
+++ packages/spark/utilities/isElementVisible.js
@@ -0,0 +1,14 @@
+/* global document */
+
+const isElementVisible = selector => {
+  const element = document.querySelector(selector);
+  const elementDisplayValue = window.getComputedStyle(element).display;
+  const elementVisibilityValue = window.getComputedStyle(element).visibility;
+  const elementIsVisible =
+    elementDisplayValue === 'none' || elementVisibilityValue === 'hidden'
+      ? false
+      : true;
+  return elementIsVisible;
+};
+
+export { isElementVisible as default };
