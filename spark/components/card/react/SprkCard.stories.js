c3c288185adc99d5bf63ecf121bcb8a10c1f5496
Amber Febbraro <amberfebbraro@quickenloans.com>
2019-05-23 13:35:17 -0400

    add card stories

--- spark/components/card/react/SprkCard.stories.js
+++ spark/components/card/react/SprkCard.stories.js
@@ -4,7 +4,7 @@ import { Box } from '../../../../.storybook/story-layout';
 import { SprkCard } from '@sparkdesignsystem/spark-react';
 
 storiesOf('Card', module)
-  .add('test', () => (
+  .add('default', () => (
     <Box>
       <SprkCard
         idString="card-1"
@@ -14,5 +14,57 @@ storiesOf('Card', module)
         >
         test
       </SprkCard>
+    </Box>
+  ))
+  .add('standout', () => (
+    <Box>
+      <SprkCard
+        isStandout
+        idString="card-20"
+        additionalContentClasses="
+            sprk-o-Stack
+            sprk-o-Stack--medium"
+        >
+      </SprkCard>
+    </Box>
+  ))
+  .add('highlighted header', () => (
+    <Box>
+      <SprkCard
+        idString="highlighted-header"
+        variant="highlightedHeader"
+        highlightedHeaderConfig={{
+          bodyText: 'Lorem ipsum dolor sit amet, doctus invenire vix te. Facilisi perpetua an pri, errem commune mea at, mei prima tantas signiferumque at. Numquam.',
+          title: 'Card Title',
+          description: 'Description',
+        }}
+      />
+    </Box>
+  ))
+  .add('teaser', () => (
+    <Box>
+      <SprkCard
+        idString="card1"
+        variant="teaser"
+        teaserConfig={{
+          bodyText: 'Lorem ipsum dolor sit amet, doctus invenirevix te. Facilisi perpetua.',
+          cta: {
+            ctaAnalytics: 'test',
+            text: 'Learn More',
+            ctaVariant: 'link',
+            href: 'https://sparkdesignsystem.com/',
+          },
+          media: {
+            href: 'https://sparkdesignsystem.com/',
+            mediaLinkElement: 'a',
+            imgAlt: 'placeholder image',
+            imgSrc: 'https://sparkdesignsystem.com/assets/toolkit/images/desktop.jpg',
+            mediaAnalyticsString: 'Card:teaser-link',
+            mediaVariant: 'img',
+          },
+          title: 'Teaser Card Title',
+          titleFirst: false,
+        }}
+      />
     </Box>
   ));
