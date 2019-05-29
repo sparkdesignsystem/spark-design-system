69541b68ca9340d611852608fab606839fe16972
corcorankt1 <guern1kn@gmail.com>
2019-05-29 14:40:18 -0400

    update scrollYDirection

--- packages/spark/utilities/scrollYDirection.js
+++ packages/spark/utilities/scrollYDirection.js
@@ -3,6 +3,7 @@
 let scrollPosition = 0;
 const scrollYDirection = () => {
   const newScrollPos = window.scrollY;
+  if (newScrollPos < 0) return;
   const diff = newScrollPos - scrollPosition;
   const direction = diff > 0 ? 'down' : 'up';
   scrollPosition = newScrollPos;
