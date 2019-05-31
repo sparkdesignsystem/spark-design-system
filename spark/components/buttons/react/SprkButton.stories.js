e3ab574b76636d6c401b10c962252560c3ce04a2
Amber Febbraro <amberfebbraro@quickenloans.com>
2019-05-31 08:58:04 -0400

    add storybook knobs

--- spark/components/buttons/react/SprkButton.stories.js
+++ spark/components/buttons/react/SprkButton.stories.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { SprkButton } from '@sparkdesignsystem/spark-react';
-storiesOf('Button', module)
-  .add('primary', () => (
+import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+
+const stories = storiesOf('Button', module);
+stories.addDecorator(withKnobs)
+
+stories.add('primary', () => (
     <SprkButton onClick={action('button clicked')}>Button</SprkButton>
   ))
   .add('secondary', () => (
-    <SprkButton variant="secondary">Button</SprkButton>
+    <SprkButton
+      variant="secondary"
+      disabled={boolean('disabled', false)}
+    >
+      {text('button text', 'hello')}
+    </SprkButton>
   ))
   .add('tertiary', () => (
     <SprkButton variant="tertiary">Button</SprkButton>
