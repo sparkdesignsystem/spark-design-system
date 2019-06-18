71c0ef008ba5eb23fd68888fbd06cb96dd9932f9
Amber Febbraro <amberfebbraro@quickenloans.com>
2019-06-18 14:42:15 -0400

    add react site node mods to storybook confif

--- react/.storybook/webpack.config.js
+++ react/.storybook/webpack.config.js
@@ -4,18 +4,18 @@ module.exports = ({ config }) => {
   config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
 
   // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
-  config.module.rules[0].use[0].loader = require.resolve("babel-loader")
+  config.module.rules[0].use[0].loader = require.resolve('babel-loader')
   config.module.rules[0].include.push(path.resolve('../spark'));
 
   // use @babel/preset-react for JSX and env (instead of staged presets)
   config.module.rules[0].use[0].options.presets = [
-    require.resolve("@babel/preset-react"),
-    require.resolve("@babel/preset-env"),
+    require.resolve('@babel/preset-react'),
+    require.resolve('@babel/preset-env'),
   ]
 
   // use @babel/plugin-proposal-class-properties for class arrow functions
   config.module.rules[0].use[0].options.plugins = [
-    require.resolve("@babel/plugin-proposal-class-properties"),
+    require.resolve('@babel/plugin-proposal-class-properties'),
   ]
 
    config.module.rules.push({
@@ -28,12 +28,13 @@ module.exports = ({ config }) => {
   }
 
   // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
-  config.resolve.mainFields = ["browser", "module", "main"];
+  config.resolve.mainFields = ['browser', 'module', 'main'];
 
   config.resolve.modules = [
     ...(config.resolve.modules || []),
-    path.resolve(__dirname, "../"),
-    path.resolve(__dirname, "../node_modules")
+    path.resolve(__dirname, '../../spark/manifests/spark-react'),
+    path.resolve(__dirname, '../../spark/manifests/spark-react/node_modules'),
+    path.resolve(__dirname, '../node_modules')
   ];
 
   return config
