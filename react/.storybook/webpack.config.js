14082163d6d062e649c5aa4a91d25d0be1920f0b
Amber Febbraro <amberfebbraro@quickenloans.com>
2019-06-20 09:22:47 -0400

    update storybook config

--- react/.storybook/webpack.config.js
+++ react/.storybook/webpack.config.js
@@ -1,24 +1,30 @@
 const path = require('path');
 module.exports = ({ config }) => {
   // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
-  config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
+  config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/];
 
   // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
-  config.module.rules[0].use[0].loader = require.resolve('babel-loader')
+  config.module.rules[0].use[0].loader = require.resolve('babel-loader');
   config.module.rules[0].include.push(path.resolve('../spark'));
 
   // use @babel/preset-react for JSX and env (instead of staged presets)
   config.module.rules[0].use[0].options.presets = [
     require.resolve('@babel/preset-react'),
     require.resolve('@babel/preset-env'),
-  ]
+  ];
 
   // use @babel/plugin-proposal-class-properties for class arrow functions
   config.module.rules[0].use[0].options.plugins = [
     require.resolve('@babel/plugin-proposal-class-properties'),
-  ]
+    /*
+     * use babel-plugin-remove-graphql-queries
+     * to remove static queries from components
+     * when rendering in storybook
+     */
+    require.resolve("babel-plugin-remove-graphql-queries"),
+  ];
 
-   config.module.rules.push({
+  config.module.rules.push({
     test: /\.scss$/,
     use: ['style-loader', 'css-loader', 'sass-loader']
   });
@@ -38,5 +44,5 @@ module.exports = ({ config }) => {
     path.resolve(__dirname, '../node_modules')
   ];
 
-  return config
+  return config;
 }
