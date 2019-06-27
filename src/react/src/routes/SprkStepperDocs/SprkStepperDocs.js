5ae86b7ecf5cc176d155efe6ceed32508bb99a44
Bob Wise <bobwise@quickenloans.com>
2019-06-27 11:18:04 -0400

    test pages - array construction

--- src/react/src/routes/SprkStepperDocs/SprkStepperDocs.js
+++ src/react/src/routes/SprkStepperDocs/SprkStepperDocs.js
@@ -1,72 +1,130 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { SprkStepper, SprkStepperStep } from '@sparkdesignsystem/spark-react';
 import CentralColumnLayout from '../../containers/CentralColumnLayout/CentralColumnLayout';
 import ExampleContainer from '../../containers/ExampleContainer/ExampleContainer';
 
-const SprkStepperDocs = () => {
-  return (
-    <CentralColumnLayout>
+class SprkStepperDocs extends Component {
+  constructor(props){
+    super(props);
 
-      <ExampleContainer heading="Base Stepper">
-        <div className="sprk-u-mbm">
-          <SprkStepper>
-            <SprkStepperStep title="Step One" ></SprkStepperStep>
-            <SprkStepperStep title="Step Two" ></SprkStepperStep>
-            <div>This child div should be ignored</div>
-            <SprkStepperStep title="Step Three" ></SprkStepperStep>
-          </SprkStepper>
-        </div>
-      </ExampleContainer>
+    this.state={
+      steps: [
+        {
+          title: 'Step 1',
+          id: 'foo',
+          isSelected: true,
+        },
+        {
+          title: 'Step 2',
+          id: 'bar',
+        },
+        {
+          title: 'Step 3',
+          // id: 'baz',
+        },
+      ]
+    };
+  }
 
-      <ExampleContainer heading="Stepper with Dark Background">
-        <div className="sprk-u-mbm">
-          <SprkStepper hasDarkBackground>
-            <SprkStepperStep title="Step One" ></SprkStepperStep>
-            <SprkStepperStep title="Step Two" ></SprkStepperStep>
-            <SprkStepperStep title="Step Three" ></SprkStepperStep>
-          </SprkStepper>
-        </div>
-      </ExampleContainer>
+  addItem() {
+    // const id = +new Date;
+    this.setState({
+      steps: [
+        {
+          title: 'Im the new first item',
+          id: +new Date(),
+        } ,
+         ...this.state.steps]
+    });
+  }
 
-      <ExampleContainer heading="Stepper with Step Descriptions">
-        <div className="sprk-u-mbm">
-          <SprkStepper>
-            <SprkStepperStep title="Step One" isSelected>
-              Step 1 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </SprkStepperStep>
-            <SprkStepperStep title="Step Two">
-              Step 2 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </SprkStepperStep>
-            <SprkStepperStep title="Step Three">
-              Step 3 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </SprkStepperStep>
-            <SprkStepperStep title="Step Four">
-              Step 4 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </SprkStepperStep>
-          </SprkStepper>
-        </div>
-      </ExampleContainer>
+  render() {
 
-      <ExampleContainer heading="Stepper with Dark Background and Descriptions">
-        <div className="sprk-u-mbm">
-          <SprkStepper hasDarkBackground>
-            <SprkStepperStep title="Step One">
-              Step 1 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </SprkStepperStep>
-            <SprkStepperStep title="Step Two" isSelected>
-              Step 2 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </SprkStepperStep>
-            <SprkStepperStep title="Step Three">
-              Step 3 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </SprkStepperStep>
-            <SprkStepperStep title="Step Four">
-              Step 4 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </SprkStepperStep>
-          </SprkStepper>
-        </div>
-      </ExampleContainer>
-    </CentralColumnLayout>
-  );
+
+    return (
+      <CentralColumnLayout>
+
+        <ExampleContainer heading="Base Stepper">
+          <div className="sprk-u-mbm">
+            <SprkStepper>
+              <SprkStepperStep title="Step One" ></SprkStepperStep>
+              <SprkStepperStep title="Step Two" ></SprkStepperStep>
+              <div>This child div should be ignored</div>
+              <SprkStepperStep title="Step Three" ></SprkStepperStep>
+            </SprkStepper>
+          </div>
+        </ExampleContainer>
+
+        <ExampleContainer heading="Stepper with Dark Background">
+          <div className="sprk-u-mbm">
+            <SprkStepper hasDarkBackground>
+              <SprkStepperStep title="Step One" ></SprkStepperStep>
+              <SprkStepperStep title="Step Two" ></SprkStepperStep>
+              <SprkStepperStep title="Step Three" ></SprkStepperStep>
+            </SprkStepper>
+          </div>
+        </ExampleContainer>
+
+        <ExampleContainer heading="Stepper with Step Descriptions">
+          <div className="sprk-u-mbm">
+            <SprkStepper>
+              <SprkStepperStep title="Step One" isSelected>
+                Step 1 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </SprkStepperStep>
+              <SprkStepperStep title="Step Two">
+                Step 2 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </SprkStepperStep>
+              <SprkStepperStep title="Step Three">
+                Step 3 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </SprkStepperStep>
+              <SprkStepperStep title="Step Four">
+                Step 4 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </SprkStepperStep>
+            </SprkStepper>
+          </div>
+        </ExampleContainer>
+
+        <ExampleContainer heading="Stepper with Dark Background and Descriptions">
+          <div className="sprk-u-mbm">
+            <SprkStepper hasDarkBackground>
+              <SprkStepperStep title="Step One">
+                Step 1 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </SprkStepperStep>
+              <SprkStepperStep title="Step Two" isSelected>
+                Step 2 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </SprkStepperStep>
+              <SprkStepperStep title="Step Three">
+                Step 3 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </SprkStepperStep>
+              <SprkStepperStep title="Step Four">
+                Step 4 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </SprkStepperStep>
+            </SprkStepper>
+          </div>
+        </ExampleContainer>
+
+        <ExampleContainer heading="Stepper Rendered From Array">
+          <button className='btn btn-primary' onClick={this.addItem.bind(this)}><b>Add item</b> to the beginning of the list</button>
+          <div className="sprk-u-mbm">
+            <SprkStepper>
+              { this.state.steps.map((item, index) => {
+                return (
+                  <SprkStepperStep
+                    title={item.title}
+                    isSelected={item.isSelected}
+                    key={+new Date()}
+                    id={item.id}
+                    >
+                      This is my content
+                  </SprkStepperStep>
+                )
+              })}
+            </SprkStepper>
+          </div>
+        </ExampleContainer>
+      </CentralColumnLayout>
+    );
+  }
 };
 
 export default SprkStepperDocs;
