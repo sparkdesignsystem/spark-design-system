e264582120a212472ffd1498d9b0108f292da22a
Rob Copeland <robcopeland@fastmail.com>
2019-07-05 13:42:27 -0400

    setup jest config, fixes imports in a few test files, set up coverage

--- spark/base/inputs/react/SprkRevealInput.test.js
+++ spark/base/inputs/react/SprkRevealInput.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import SprkRevealInput from './SprkRevealInput';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 it('should render an element with the correct class', () => {
   const wrapper = mount(<SprkRevealInput />);
--- spark/manifests/spark-react/jest-config.js
+++ spark/manifests/spark-react/jest-config.js
@@ -3,5 +3,15 @@ const { resolve } = require('path');
 const root = resolve(__dirname, '../..');
 module.exports = {
   rootDir: root,
-  projects: ['/spark/**/react/*.test.js'],
+  setupFilesAfterEnv: ["<rootDir>/manifests/spark-react/setup.test.js"],
+  "testMatch": [
+    "**/?(*.)+(spec|test).[j]s?(x)"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    '<rootDir>/manifests/spark-react/setup.test.js'
+  ],
+  modulePaths: ['<rootDir>manifests/spark-react/node_modules'],
+  coveragePathIgnorePatterns: ["/node_modules/", "/vanilla/"],
+  coverageDirectory: '<rootDir>/coverage/react'
 };
