8c7beb5dabf078fbcba5c465ad6b27ada02ada35
Amber Febbraro <amberfebbraro@quickenloans.com>
2019-07-24 16:00:25 -0400

    start adding alert knobs

--- spark/components/alerts/react/SprkAlert.stories.js
+++ spark/components/alerts/react/SprkAlert.stories.js
@@ -1,44 +1,51 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
-
 import SprkAlert from './SprkAlert';
 
-storiesOf('Components|Alert', module)
+const stories = storiesOf('Components|Alert', module);
+stories.addDecorator(withKnobs);
+
+stories
   .add('default', () => (
     <SprkAlert
-      message="This is an alert."
+      message={text('Message', 'This is an alert.')}
       onDismiss={action('clicked')}
-      isVisible
-      idString="alert-1"
-      analyticsString="alert"
+      isVisible={boolean('isVisible', true)}
+      isDismissible={boolean('isDismissible', true)}
+      idString={text('idString', 'alert-1')}
+      analyticsString={text('analyticsString', 'alert-1')}
     />
   ))
   .add('success', () => (
     <SprkAlert
-      message="This is a success alert."
+      message={text('Message', 'This is a success alert.')}
       onDismiss={action('clicked')}
-      isVisible
+      isVisible={boolean('isVisible', true)}
+      isDismissible={boolean('isDismissible', true)}
       variant="success"
-      idString="alert-success-1"
-      analyticsString="alert"
+      idString={text('idString', 'alert-2')}
+      analyticsString={text('analyticsString', 'alert-2')}
     />
   ))
   .add('info', () => (
     <SprkAlert
-      message="This is important information."
-      isVisible
+      message={text('Message', 'This is important information.')}
+      isVisible={boolean('isVisible', true)}
+      isDismissible={boolean('isDismissible', true)}
       variant="info"
-      idString="alert-info-1"
-      analyticsString="alert"
+      idString={text('idString', 'alert-3')}
+      analyticsString={text('analyticsString', 'alert-3')}
     />
   ))
   .add('fail', () => (
     <SprkAlert
-      message="This is important information."
-      isVisible
+      message={text('Message', 'This is important information.')}
+      isVisible={boolean('isVisible', true)}
+      isDismissible={boolean('isDismissible', true)}
       variant="fail"
-      idString="alert-info-1"
-      analyticsString="alert"
+      idString={text('idString', 'alert-4')}
+      analyticsString={text('analyticsString', 'alert-4')}
     />
   ));
