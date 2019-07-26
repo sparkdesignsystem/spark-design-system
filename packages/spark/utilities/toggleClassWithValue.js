e463b0956c668a8e5e18736252eceac6f51150c1
corcorankt1 <guern1kn@gmail.com>
2019-07-26 15:34:47 -0400

    add js to add/remove class

--- packages/spark/utilities/toggleClassWithValue.js
+++ packages/spark/utilities/toggleClassWithValue.js
@@ -1,11 +1,12 @@
 function toggleClassWithValue(element, className) {
-  // element.addEventListener('focusin', () => {
-  //   element.classList.add(className);
-  // });
-
-  // element.addEventListener('focusout', () => {
-  //   element.classList.remove(className);
-  // });
+  element.addEventListener('input', () => {
+    const value = element.value;
+    if (value) {
+      element.classList.add(className);
+    } else {
+      element.classList.remove(className);
+    }
+  });
 }
 
 export { toggleClassWithValue as default };
