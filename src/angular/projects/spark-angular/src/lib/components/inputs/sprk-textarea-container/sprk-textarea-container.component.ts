a2ae8a60134962d5f2d6043a915e261e359bbf01
Amber Febbraro <amberfebbraro@quickenloans.com>
2019-08-07 08:52:38 -0400

    update contentchild static to true

--- src/angular/projects/spark-angular/src/lib/components/inputs/sprk-textarea-container/sprk-textarea-container.component.ts
+++ src/angular/projects/spark-angular/src/lib/components/inputs/sprk-textarea-container/sprk-textarea-container.component.ts
@@ -22,11 +22,11 @@ export class SprkTextareaContainerComponent implements OnInit {
   @Input()
   iconContainerClasses: string;
 
-  @ContentChild(SprkLabelDirective, { static: false })
+  @ContentChild(SprkLabelDirective, { static: true })
   label: SprkLabelDirective;
-  @ContentChild(SprkInputDirective, { static: false })
+  @ContentChild(SprkInputDirective, { static: true })
   input: SprkInputDirective;
-  @ContentChild(SprkFieldErrorDirective, { static: false })
+  @ContentChild(SprkFieldErrorDirective, { static: true })
   error: SprkFieldErrorDirective;
 
   id = _.uniqueId();
