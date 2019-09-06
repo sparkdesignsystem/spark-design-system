6fa218699f1643e7519e8932dcb9647073d9db98
Rob Copeland <robcopeland@fastmail.com>
2019-09-06 11:07:16 -0400

    add missing install

--- gulp/angular/spark-angular/spark-angular.gulpfile.js
+++ gulp/angular/spark-angular/spark-angular.gulpfile.js
@@ -4,7 +4,7 @@ const runSequence = require('run-sequence');
 const clean = require('gulp-clean');
 
 // assumes previous run of setup-spark-packages
-gulp.task('setup-spark-angular', (cb) => {
+gulp.task('setup-spark-angular', cb => {
   runSequence(
     'link-spark-to-angular-dir',
     'build-spark-angular',
@@ -14,29 +14,39 @@ gulp.task('setup-spark-angular', (cb) => {
   );
 });
 
-gulp.task('build-spark-angular', (cb) => {
-  exec('cd src/angular/projects/spark-angular && npm run build', (err, stdout, stderr) => {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
+gulp.task('build-spark-angular', cb => {
+  exec(
+    'cd src/angular/projects/spark-angular && npm install && npm run build',
+    (err, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    },
+  );
 });
 
-gulp.task('test-spark-angular', (cb) => {
-  exec('cd src/angular/projects/spark-angular && ../../node_modules/.bin/ng test spark-angular --watch=false', (err, stdout, stderr) => {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
+gulp.task('test-spark-angular', cb => {
+  exec(
+    'cd src/angular/projects/spark-angular && ../../node_modules/.bin/ng test spark-angular --watch=false',
+    (err, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    },
+  );
 });
 
-gulp.task('clean-spark-angular', () => gulp.src(['src/angular/dist/spark-angular'], { read: false })
-  .pipe(clean()));
+gulp.task('clean-spark-angular', () =>
+  gulp.src(['src/angular/dist/spark-angular'], { read: false }).pipe(clean()),
+);
 
-gulp.task('link-spark-angular', (cb) => {
-  exec('cd src/angular/dist/spark-angular && npm unlink && npm link', (err, stdout, stderr) => {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
+gulp.task('link-spark-angular', cb => {
+  exec(
+    'cd src/angular/dist/spark-angular && npm unlink && npm link',
+    (err, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    },
+  );
 });
