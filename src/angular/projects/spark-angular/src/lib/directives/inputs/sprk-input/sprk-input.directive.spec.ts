1483aed00714e67a87f1bc0ccea7ff3ef03ee8e7
corcorankt1 <guern1kn@gmail.com>
2019-09-18 11:51:24 -0400

    add unit tests for sprk-input directive

--- src/angular/projects/spark-angular/src/lib/directives/inputs/sprk-input/sprk-input.directive.spec.ts
+++ src/angular/projects/spark-angular/src/lib/directives/inputs/sprk-input/sprk-input.directive.spec.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { SprkInputDirective } from './sprk-input.directive';
 
 @Component({
   selector: 'sprk-test',
   template: `
-    <input sprkInput />
+    <input sprkInput value='Initial Value'/>
   `
 })
 class TestComponent {}
@@ -13,7 +14,7 @@ class TestComponent {}
 describe('Spark Input Directive', () => {
   let component: TestComponent;
   let fixture: ComponentFixture<TestComponent>;
-  let inputElement: HTMLInputElement;
+  let inputElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -24,10 +25,29 @@ describe('Spark Input Directive', () => {
     component = fixture.componentInstance;
 
     fixture.detectChanges();
-    inputElement = fixture.nativeElement.querySelector('input');
+    inputElement = fixture.debugElement.query(By.css('input'));
   }));
 
   it('should create itself', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add the has-floating-label class on change if the input value is not empty', () => {
+    inputElement.nativeElement.value = 'Test Value!';
+    inputElement.nativeElement.dispatchEvent(new Event('change'));
+    fixture.detectChanges();
+    expect(inputElement.nativeElement.classList.contains('sprk-b-Input--has-floating-label')).toEqual(true);
+  });
+
+  it('should not add the has-floating-label class on change if the input value is empty', () => {
+    inputElement.nativeElement.value = '';
+    inputElement.nativeElement.dispatchEvent(new Event('change'));
+    fixture.detectChanges();
+    expect(inputElement.nativeElement.classList.contains('sprk-b-Input--has-floating-label')).toEqual(false);
+  });
+
+  it('should add the has-floating-label class on load if the input value is not empty', () => {
+    expect(inputElement.nativeElement.classList.contains('sprk-b-Input--has-floating-label')).toEqual(true);
+  });
+
 });
