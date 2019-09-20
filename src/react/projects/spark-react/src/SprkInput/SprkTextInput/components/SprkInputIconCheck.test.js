3361585489bc1ee9587bfbb43dc9fe4c6eb86ac1
RVMendoza <RVxMendoza@gmail.com>
2019-09-20 15:55:51 -0400

    Added tests

---
+++ src/react/projects/spark-react/src/SprkInput/SprkTextInput/components/SprkInputIconCheck.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import SprkInputIconCheck from './SprkInputIconCheck';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it('should render base class', () => {
+  const wrapper = mount(<SprkInputIconCheck />);
+  expect(wrapper.find('.sprk-b-InputContainer__icon-container').length).toBe(1);
+});
+
+it('should render as narrow', () => {
+  const wrapper = mount(
+    <SprkInputIconCheck leadingIcon="percent" narrowWidth />,
+  );
+  expect(
+    wrapper.find('.sprk-b-InputContainer__icon-container--narrow').length,
+  ).toBe(1);
+});
+
+it('should render with icon right class', () => {
+  const wrapper = mount(<SprkInputIconCheck leadingIcon="percent" iconRight />);
+  expect(
+    wrapper.find(
+      '.sprk-b-InputContainer__icon.sprk-b-InputContainer__icon--right',
+    ).length,
+  ).toBe(1);
+});
