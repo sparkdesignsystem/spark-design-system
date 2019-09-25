695823889e6546072fbc0ade026683deea0b179c
Bob Wise <bobwise@quickenloans.com>
2019-09-25 13:14:49 -0400

    modal stories

--- spark/components/modals/react/SprkModal.stories.js
+++ spark/components/modals/react/SprkModal.stories.js
@@ -8,9 +8,60 @@ export default {
 };
 
 export const defaultStory = () => (
-  <div>Modal!</div>
+  <SprkModal
+    title="Default Modal"
+    isVisible={true}
+    confirmText="Yes please"
+    cancelText="No thank you"
+    confirmClick={() => {
+      alert('confirm');
+    }}
+    cancelClick={() => {
+      alert('cancel click');
+    }}
+  >
+    Would you like to buy a mortgage?
+  </SprkModal>
 );
 
 defaultStory.story = {
   name: 'Default',
+};
+
+export const infoStory = () => (
+  <SprkModal
+    title="Info Modal"
+    isVisible={true}
+    variant="info"
+    confirmClick={() => {
+      alert('confirm click');
+    }}
+    cancelClick={() => {
+      alert('cancel click');
+    }}
+  >
+    Your mortgage has been downloaded.
+  </SprkModal>
+);
+
+infoStory.story = {
+  name: 'Info',
+};
+
+defaultStory.story = {
+  name: 'Default',
+};
+
+export const waitStory = () => (
+  <SprkModal
+    title="Wait Modal"
+    isVisible={true}
+    variant="wait"
+  >
+    This mortgage can only be closed with javascript
+  </SprkModal>
+);
+
+waitStory.story = {
+  name: 'Wait',
 };
