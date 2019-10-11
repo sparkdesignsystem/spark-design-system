455957f5f5c140009b3938a14f5769417312e65d
Febbraro <afebbraro@c02xc04mjgh5.mi.corp.rockfin.com>
2019-10-11 12:21:38 -0400

    solve vanilla js loading

--- spark/components/accordions/vanilla/accordions.stories.js
+++ spark/components/accordions/vanilla/accordions.stories.js
@@ -1,9 +1,16 @@
+import { useEffect } from '@storybook/client-api';
+import { toggle } from '../../toggle/vanilla/toggle';
+
 export default {
   title: 'Components|Accordion',
 };
 
-export const defaultStory = () => (
-  `<ul class="sprk-c-Accordion sprk-o-VerticalList">
+export const defaultStory = () => {
+  useEffect(() => {
+    toggle();
+  }, []);
+
+  return `<ul class="sprk-c-Accordion sprk-o-VerticalList">
     <li class="sprk-c-Accordion__item" data-sprk-toggle="container">
     <a
       aria-controls="details-one"
@@ -40,7 +47,8 @@ export const defaultStory = () => (
      <svg class="sprk-c-Icon sprk-c-Icon--toggle sprk-c-Icon--l sprk-c-Accordion__icon" data-sprk-toggle="icon" viewBox="0 0 64 64"> <use xlink:href="#chevron-up-circle-two-color" data-sprk-toggle="accordionIconUseElement"></use> </svg> </a><div data-sprk-toggle="content" id="details-three" >
      <div class="sprk-c-Accordion__content sprk-b-TypeBodyTwo"> This is an example of accordion content. This is an example of accordion content. This is an example of accordion content. This is an example of accordion content.</div></div></li>
   </ul>
-`);
+`
+};
 
 defaultStory.story = {
   name: 'default',
--- vanilla/.storybook/config.js
+++ vanilla/.storybook/config.js
@@ -2,11 +2,7 @@ import '../../spark/manifests/spark/_spark.scss';
 import { configure, addParameters, addDecorator } from '@storybook/html';
 import { withA11y } from '@storybook/addon-a11y';
 import sparkTheme from '../../storybook-spark-theme';
-import spark from '../../spark/manifests/spark/spark';
 import { withActions } from '@storybook/addon-actions';
-
-// initialize spark js
-spark();
 
 addDecorator(withA11y);
 addDecorator(withActions('click .sprk-c-Button'));
