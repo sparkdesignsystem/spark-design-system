3fd18a888b18dbee9577c35b83ea497fea4192d3
Rob Copeland <robcopeland@fastmail.com>
2019-10-16 16:23:03 -0400

    remove comment

--- react/rollup.config.js
+++ react/rollup.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from "rollup-plugin-commonjs";
-// import replace from 'rollup-plugin-replace';
 
 export default {
   input: './src/spark-exports-react.js',
