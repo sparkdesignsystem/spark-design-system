150378a059df82759223a2a8d2a943d4551c5a6d
RVMendoza <RVxMendoza@gmail.com>
2019-10-16 10:52:46 -0400

    Add different link stories

--- spark/base/links/vanilla/links.stories.js
+++ spark/base/links/vanilla/links.stories.js
@@ -3,12 +3,61 @@ export default {
 };
 
 export const defaultLink = () => (
-  `<a
-    class="sprk-b-Link"
-    href="#"
-    data-id="link-1"
-    data-analytics="object.action.event">
+  `
+    <a
+      class="sprk-b-Link"
+      href="#"
+      data-id="link-1"
+      data-analytics="object.action.event"
+    >
       Simple Link
+    </a>
+  `
+);
+
+defaultLink.story = {
+  name: 'Default',
+};
+
+export const simple = () => (
+  `
+    <a
+      class="sprk-b-Link sprk-b-Link--simple"
+      href="#"
+      data-id="link-1"
+      data-analytics="object.action.event"
+    >
+      Simple Link
+    </a>
+  `
+);
+
+export const iconWithTextLink = () => (
+  `
+    <a
+      class="sprk-b-Link sprk-b-Link--has-icon sprk-b-Link--simple"
+      href="#nogo"
+      data-id="link-1"
+      data-analytics="object.action.event"
+    >
+      <svg class="sprk-c-Icon sprk-c-Icon--l sprk-u-mrs sprk-c-Icon--stroke-current-color" viewBox="0 0 100 100">
+        <use xlink:href="#communication" />
+      </svg>
+
+      Message Us
+    </a>
+  `
+);
+
+export const disabled = () => (
+  `
+    <a
+      class="sprk-b-Link sprk-b-Link--disabled"
+      href="#nogo"
+      data-id="link-3"
+      data-analytics="object.action.event"
+    >
+      Disabled
     </a>
   `
 );
