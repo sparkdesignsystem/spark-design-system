3f93d53b687cc3faa0e970fe289fedb2f6418882
Febbraro <amberfebbraro@quickenloans.com>
2019-10-19 10:03:49 -0400

    update SB, add accordions/alerts

---
+++ angular/projects/spark-angular/src/lib/components/sprk-accordion/sprk-accordion.stories.ts
@@ -0,0 +1,76 @@
+import { SprkAccordionModule } from './sprk-accordion.module';
+import { SprkAccordionItemModule } from '../sprk-accordion-item/sprk-accordion-item.module';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SprkAccordionComponent } from './sprk-accordion.component';
+
+export default {
+  title: 'Components|Accordions',
+  component: SprkAccordionComponent
+};
+
+const modules = {
+  imports: [
+    SprkAccordionModule,
+    SprkAccordionItemModule,
+    RouterModule.forRoot([], { useHash: true }),
+    BrowserAnimationsModule,
+  ],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }]
+};
+
+export const defaultStory = () => ({
+  moduleMetadata: modules,
+  template: `
+    <sprk-accordion>
+      <sprk-accordion-item
+        title="This is an accordion heading"
+        additionalClasses="sprk-u-mbs"
+        isOpen="true"
+        additionalHeadingClasses="sprk-b-TypeDisplaySeven"
+        idString="accordion-item-1"
+        analyticsString="object.action.event"
+      >
+        <p>
+          This is an example of accordion content.
+          This is an example of accordion content.
+          This is an example of accordion content.
+          This is an example of accordion content.
+        </p>
+      </sprk-accordion-item>
+
+      <sprk-accordion-item
+        title="This is an accordion heading"
+        idString="accordion-item-2"
+        analyticsString="object.action.event"
+        additionalHeadingClasses="sprk-b-TypeDisplaySeven"
+      >
+        <p>
+          This is an example of accordion content.
+          This is an example of accordion content.
+          This is an example of accordion content.
+          This is an example of accordion content.
+        </p>
+      </sprk-accordion-item>
+
+      <sprk-accordion-item
+        title="This is an accordion heading"
+        idString="accordion-item-3"
+        analyticsString="object.action.event"
+        additionalHeadingClasses="sprk-b-TypeDisplaySeven"
+      >
+        <p>
+          This is an example of accordion content.
+          This is an example of accordion content.
+          This is an example of accordion content.
+          This is an example of accordion content.
+        </p>
+      </sprk-accordion-item>
+    </sprk-accordion>
+  `,
+});
+
+defaultStory.story = {
+  name: 'Default',
+};
--- angular/projects/spark-angular/src/lib/components/sprk-alert/sprk-alert.stories.ts
+++ angular/projects/spark-angular/src/lib/components/sprk-alert/sprk-alert.stories.ts
@@ -18,3 +18,44 @@ export const info = () => ({
     </sprk-alert>
   `,
 });
+
+export const success = () => ({
+  moduleMetadata: modules,
+  template: `
+    <sprk-alert
+      alertType="success"
+      idString="alert-success-1"
+      analyticsString="object.action.event"
+    >
+      This is a success message.
+    </sprk-alert>
+  `
+});
+
+export const fail = () => ({
+  moduleMetadata: modules,
+  template: `
+    <sprk-alert
+      alertType="fail"
+      idString="alert-fail-1"
+      analyticsString="object.action.event"
+    >
+      This is a failure message to alert
+      that something was not successful.
+    </sprk-alert>
+  `
+});
+
+export const withNoDismissButton = () => ({
+  moduleMetadata: modules,
+  template: `
+    <sprk-alert
+      alertType="success"
+      [dismissible]="false"
+      idString="alert-success-2"
+      analyticsString="object.action.event"
+    >
+      This success alert has no dismiss button.
+    </sprk-alert>
+  `
+});
