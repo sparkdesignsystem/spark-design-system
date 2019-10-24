19716ba05079890f37d1d898a52f36f67448e6be
RVMendoza <RVxMendoza@gmail.com>
2019-10-24 15:56:27 -0400

    Add the correct directive

--- angular/projects/spark-angular/src/lib/components/inputs/sprk-input.stories.ts
+++ angular/projects/spark-angular/src/lib/components/inputs/sprk-input.stories.ts
@@ -2,6 +2,7 @@ import { SparkInputContainerModule } from './sprk-input-container/sprk-input-con
 import { SprkInputModule } from '../../directives/inputs/sprk-input/sprk-input.module';
 import { SprkHugeInputContainerModule } from './sprk-huge-input-container/sprk-huge-input-container.module';
 import { SprkIconModule } from './../sprk-icon/sprk-icon.module';
+import { SprkLabelModule } from '../../directives/inputs/sprk-label/sprk-label.module';
 
 export default {
   title: 'Components|Inputs',
@@ -10,6 +11,7 @@ export default {
 const modules = {
   imports: [
     SparkInputContainerModule,
+    SprkLabelModule,
     SprkInputModule,
     SprkHugeInputContainerModule,
     SprkIconModule,
@@ -19,25 +21,25 @@ const modules = {
 export const textInput = () => ({
   moduleMetadata: modules,
   template: `
-        <sprk-input-container>
-          <label sprkLabel>Text Input *</label>
-          <input
-            name="text_input"
-            type="text"
-            [(ngModel)]="text_input"
-            required
-            #textInput="ngModel"
-            sprkInput
-          />
-          <p sprkHelperText>Please enter some input.</p>
-          <span [hidden]="textInput.valid || textInput.pristine" sprkFieldError>
-            <sprk-icon
-              iconType="exclamation-filled-small"
-              additionalClasses="sprk-b-ErrorIcon"
-            ></sprk-icon>
-            <div class="sprk-b-ErrorText">This field is required.</div>
-          </span>
-        </sprk-input-container>
+    <sprk-input-container>
+      <label sprkLabel>Text Input Label</label>
+      <input
+        name="text_input"
+        type="text"
+        [(ngModel)]="text_input"
+        required
+        #textInput="ngModel"
+        sprkInput
+      />
+      <p sprkHelperText>Please enter some input.</p>
+      <span [hidden]="textInput.valid || textInput.pristine" sprkFieldError>
+        <sprk-icon
+          iconType="exclamation-filled-small"
+          additionalClasses="sprk-b-ErrorIcon"
+        ></sprk-icon>
+        <div class="sprk-b-ErrorText">This field is required.</div>
+      </span>
+    </sprk-input-container>
   `,
 });
 
