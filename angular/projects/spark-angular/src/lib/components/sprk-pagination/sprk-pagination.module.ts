fe9dfb7b75e3431d4f8ff91480dc37f8b06a9bc9
corcorankt1 <guern1kn@gmail.com>
2019-10-25 15:55:15 -0400

    update sprk-pagination module

--- angular/projects/spark-angular/src/lib/components/sprk-pagination/sprk-pagination.module.ts
+++ angular/projects/spark-angular/src/lib/components/sprk-pagination/sprk-pagination.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SprkIconModule } from '../sprk-icon/sprk-icon.module';
 import { SprkLinkModule } from '../sprk-link/sprk-link.module';
-import { SprkUnorderedListModule } from '../sprk-unordered-list/sprk-unordered-list.module';
+import {
+  SprkUnorderedListModule
+} from '../sprk-unordered-list/sprk-unordered-list.module';
 import { SprkListItemModule } from '../sprk-list-item/sprk-list-item.module';
 import { SprkPaginationComponent } from './sprk-pagination.component';
 
