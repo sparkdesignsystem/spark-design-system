55bcbf5a84eeed6bcdf07c8fd16b2697e8f95963
Rob Copeland <robcopeland@fastmail.com>
2019-10-25 10:57:51 -0400

    adding autoprefixer and grid prefixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,5 +57,11 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [require(`postcss-preset-env`)({ stage: 0 })],
+      },
+    },
   ],
 };
