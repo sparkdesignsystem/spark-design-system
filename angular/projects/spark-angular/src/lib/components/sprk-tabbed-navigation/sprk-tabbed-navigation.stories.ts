2cfc86cf2dc4de42a120f39ccc8fed0b7a0d3649
corcorankt1 <guern1kn@gmail.com>
2019-10-28 15:09:13 -0400

    add sprk-tabbed-navigation story for angular

---
+++ angular/projects/spark-angular/src/lib/components/sprk-tabbed-navigation/sprk-tabbed-navigation.stories.ts
@@ -0,0 +1,75 @@
+import { storyWrapper } from '../../../../../../.storybook/helpers/storyWrapper';
+import { SprkTabbedNavigationModule } from './sprk-tabbed-navigation.module';
+import {
+  SprkTabbedNavigationPanelModule
+} from '../../directives/tabbed-navigation/sprk-tabbed-navigation-panel/sprk-tabbed-navigation-panel.module';
+import {
+  SprkTabbedNavigationTabModule
+} from '../../directives/tabbed-navigation/sprk-tabbed-navigation-tab/sprk-tabbed-navigation-tab.module';
+
+
+export default {
+  title: 'Components|Tabbed Navigation',
+  decorators: [
+    storyWrapper(
+      storyContent => (
+        `<div class="sprk-o-Box sprk-u-JavaScript">${ storyContent }<div>`
+      )
+    )
+  ]
+};
+
+const modules = {
+  imports: [
+    SprkTabbedNavigationModule,
+    SprkTabbedNavigationPanelModule,
+    SprkTabbedNavigationTabModule,
+  ],
+};
+
+export const defaultStory = () => ({
+  moduleMetadata: modules,
+  template: `
+    <sprk-tabbed-navigation idString="tabs-1">
+      <button
+        sprkTabbedNavigationTab
+        analyticsString="Tab: 1"
+        data-id="tab-1"
+      >
+        Tab 1
+      </button>
+      <button
+        sprkTabbedNavigationTab
+        data-id="tab-2"
+        [defaultActive]=true
+      >
+        Tab 2
+      </button>
+      <button
+        sprkTabbedNavigationTab
+        data-id="tab-3"
+      >
+        Tab 3
+      </button>
+      <div sprkTabbedNavigationPanel>
+        <p>Tab 1 Content Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis rhoncus ipsum. Nulla
+        euismod nisi est, vel consequat ante consectetur in. Ut interdum dictum est at ornare. Nam nec dapibus nibh.
+        Integer venenatis ex eu mi euismod, non ultricies lacus venenatis.</p>
+      </div>
+      <div [defaultActive]=true sprkTabbedNavigationPanel>
+        <p>Tab 2 Content Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis rhoncus ipsum. Nulla
+        euismod nisi est, vel consequat ante consectetur in. Ut interdum dictum est at ornare. Nam nec dapibus nibh.
+        Integer venenatis ex eu mi euismod, non ultricies lacus venenatis.</p>
+      </div>
+      <div sprkTabbedNavigationPanel>
+        <p>Tab 3 Content Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis rhoncus ipsum. Nulla
+        euismod nisi est, vel consequat ante consectetur in. Ut interdum dictum est at ornare. Nam nec dapibus nibh.
+        Integer venenatis ex eu mi euismod, non ultricies lacus venenatis.</p>
+      </div>
+    </sprk-tabbed-navigation>
+  `,
+});
+
+defaultStory.story = {
+  name: 'Default',
+};
