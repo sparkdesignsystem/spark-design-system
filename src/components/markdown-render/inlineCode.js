112c858799a83756b3d03fb5f348a8681c29bdf9
Rob Copeland <robcopeland@fastmail.com>
2019-10-30 17:24:18 -0400

    adding prism renderer

--- src/components/markdown-render/inlineCode.js
+++ src/components/markdown-render/inlineCode.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Highlight, {defaultProps} from 'prism-react-renderer'
 
-function InlineCode(props) {
+function InlineCode({children, className}) {
+  const language = className.replace(/language-/, '')
   return (
-    <div className="docs-command">
-      <code className="docs-codehighlight" {...props}></code>
-    </div>
+    <Highlight {...defaultProps} code={children} language={language}>
+      {({className, style, tokens, getLineProps, getTokenProps}) => (
+        <pre className={`docs-code ${className}`} style={{...style, padding: '20px'}}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({line, key: i})}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({token, key})} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
   );
 }
 
