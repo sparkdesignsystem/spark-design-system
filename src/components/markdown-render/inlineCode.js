af8ce3237a19fb3db7a39c95db9b5cd1626615bb
Rob Copeland <robcopeland@fastmail.com>
2019-10-31 09:11:10 -0400

    cleans up a few visual things

--- src/components/markdown-render/inlineCode.js
+++ src/components/markdown-render/inlineCode.js
@@ -6,7 +6,7 @@ function InlineCode({children, className}) {
   return (
     <Highlight {...defaultProps} code={children} language={language}>
       {({className, style, tokens, getLineProps, getTokenProps}) => (
-        <pre className={`docs-code ${className}`} style={{...style, padding: '20px'}}>
+        <pre className={`docs-code ${className}`} style={{...style}}>
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({line, key: i})}>
               {line.map((token, key) => (
