daab3cb549976f9a7c50c6103af30689067346a3
Febbraro <amberfebbraro@quickenloans.com>
2019-10-31 15:20:18 -0400

    trying out iframe-resizer

--- src/components/ComponentPreview.js
+++ src/components/ComponentPreview.js
@@ -1,15 +1,94 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import IframeResizer from 'iframe-resizer-react';
 
-function ComponentPreview({component}) {
-  const componentURL = `https://spark-sb-react.netlify.com/iframe.html?id=components-${component}--default-story`;
+import {
+  SprkLink,
+  SprkList,
+  SprkListItem
+} from '@sparkdesignsystem/spark-react';
 
-  return (
-    <iframe
-      className="docs-c-ComponentPreview"
-      title="Storybook Component Preview"
-      src={componentURL}>
-    </iframe>
-  );
-}
+class ComponentPreview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.iframeRef = React.createRef();
+  }
+
+  // resizePreview(elementRef) {
+  //   console.log(elementRef, 'cats')
+  //   elementRef.current.style.height = elementRef.current.contentWindow.document.body.scrollHeight + 'px';
+  // }
+
+  // componentDidMount() {
+  //   this.resizePreview(this.iframeRef);
+  // }
+
+  render() {
+    const {
+      componentName,
+      hasReact,
+      hasAngular,
+      hasHTML
+    } = this.props;
+    const componentURL = `https://spark-sb-html.netlify.com/iframe.html?id=components-${componentName}`;
+
+    return (
+      <div className="sprk-o-Box">
+        <div className="sprk-u-AbsoluteCenter">
+          <IframeResizer
+            title="Component Preview"
+            className="docs-c-ComponentPreview"
+            width="100%"
+            src={componentURL}
+          />
+        </div>
+
+        {(hasHTML || hasAngular || hasReact) && (
+          <>
+          <SprkList
+            element="ul"
+            variant="bare"
+            additionalClasses="sprk-o-HorizontalList"
+          >
+            {hasHTML && (
+              <SprkListItem>
+                <SprkLink href="#">
+                  HTML
+                </SprkLink>
+              </SprkListItem>
+            )}
+
+            {hasReact && (
+              <SprkListItem>
+                <SprkLink href="#">
+                  React
+                </SprkLink>
+              </SprkListItem>
+            )}
+
+            {hasAngular && (
+              <SprkListItem>
+                <SprkLink href="#">
+                  Angular
+                </SprkLink>
+              </SprkListItem>
+            )}
+          </SprkList>
+          </>
+        )}
+      </div>
+    );
+  }
+};
+
+ComponentPreview.propTypes = {
+  isVisible: PropTypes.string,
+};
+
+ComponentPreview.defaultProps = {
+  hasAngular: true,
+  hasReact: true,
+  hasHTML: true,
+};
 
 export default ComponentPreview;
--- src/components/markdown-render/ul.js
+++ src/components/markdown-render/ul.js
@@ -4,7 +4,6 @@ function Ul(props) {
   return (
     <ul className="
       sprk-b-List
-      sprk-b-List--bare
       sprk-b-List--indented
       sprk-u-mbl" {...props}/>
   );
