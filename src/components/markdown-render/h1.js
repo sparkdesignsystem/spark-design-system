d4c2a845326b10d8b435417046a2f737c32520c9
Febbraro <amberfebbraro@quickenloans.com>
2019-11-03 19:20:01 -0500

    add page mark to h1s

--- src/components/markdown-render/h1.js
+++ src/components/markdown-render/h1.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function H1(props) {
   return (
-    <h1 className="docs-b-h1 sprk-b-TypeDisplayOne sprk-u-mbh sprk-u-Measure" {...props}></h1>
+    <h1 className="docs-b-h1 sprk-b-PageTitle sprk-b-TypeDisplayOne sprk-u-mbh sprk-u-Measure" {...props}></h1>
   );
 }
 
