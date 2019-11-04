91b0f3978b5ff5ee1b5ea9ad68b269eae223fdf9
Rob Copeland <robcopeland@fastmail.com>
2019-11-04 14:55:59 -0500

    testing

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,12 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 exports.onRouteUpdate = () => {
-  if (docsearch) {
-    docsearch({ // eslint-disable-line no-undef
-      apiKey: '9486baf438a1d7fd0b6c982838d6f9bc',
-      indexName: 'sparkdesignsystem',
-      inputSelector: '.docs-header-search input',
-      debug: 'false'
-    });
-  }
+  // docsearch({ // eslint-disable-line no-undef
+  //   apiKey: '9486baf438a1d7fd0b6c982838d6f9bc',
+  //   indexName: 'sparkdesignsystem',
+  //   inputSelector: '.docs-header-search input',
+  //   debug: 'false'
+  // });
 }
