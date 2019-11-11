c5fb26fa454c1c8d5d11e0612ba23990cfb6c4a4
Rob Copeland <robcopeland@fastmail.com>
2019-11-11 11:07:34 -0500

    adding box story and preview css

--- html/objects/box.stories.js
+++ html/objects/box.stories.js
@@ -1,3 +1,11 @@
 export default {
-  title: 'Objects|Box',
+  title: 'Components|Box',
+  decorators: [
+    story => `<div class="sprk-o-Box sb-preview-box">${story()}</div>`,
+  ],
 };
+
+export const Default = () => (
+  `
+    <div class="sprk-o-Box"></div>
+  `);
