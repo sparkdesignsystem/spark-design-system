1a2ec839004fa0118ef7b3d41123c1c7481343a1
Febbraro <amberfebbraro@quickenloans.com>
2019-11-18 15:39:30 -0500

    try to include bablerc in webpack config

--- html/.storybook/webpack.config.js
+++ html/.storybook/webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 module.exports = ({ config }) => {
   config.resolve.modules = [
     ...(config.resolve.modules || []),
-    path.resolve(__dirname, '../node_modules')
+    path.resolve(__dirname, '../node_modules'),
+    path.resolve(__dirname, '../.babelrc'),
   ];
 
   return config;
