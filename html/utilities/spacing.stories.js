70b3c1b3c85378e0989f84527de8a31996a696bf
Rob Copeland <robcopeland@fastmail.com>
2019-11-18 11:34:15 -0500

    removed remaining docs classes from spacing

--- html/utilities/spacing.stories.js
+++ html/utilities/spacing.stories.js
@@ -79,27 +79,27 @@ insetSpacingShort.story = {
 
 export const insetSpacingTall = () => (
   `
-    <div class="sb-spacing-inset-tall-xs sprk-u-mbm">
+    <div class="sprk-u-mbm">
       <p class="sprk-b-TypeBodyTwo">
         The grey border shows the extra small inset tall of 6px 4px 6px 4px.
       </p>
     </div>
-    <div class="sb-spacing-inset-tall-s sprk-u-mbm">
+    <div class="sprk-u-mbm">
       <p class="sprk-b-TypeBodyTwo">
         The grey border shows the small inset tall of 12px 8px 12px 8px.
       </p>
     </div>
-    <div class="sb-spacing-inset-tall-m sprk-u-mbm">
+    <div class="sprk-u-mbm">
       <p class="sprk-b-TypeBodyTwo">
         The grey border shows the medium inset tall of 24px 16px 24px 16px.
       </p>
     </div>
-    <div class="sb-spacing-inset-tall-l sprk-u-mbm">
+    <div class="sprk-u-mbm">
       <p class="sprk-b-TypeBodyTwo">
         The grey border shows the large inset tall of 48px 32px 48px 32px.
       </p>
     </div>
-    <div class="sb-spacing-inset-tall-xl sprk-u-mbm">
+    <div class="sprk-u-mbm">
       <p class="sprk-b-TypeBodyTwo">
         The grey border shows the extra large inset tall of 96px 64px 96px 64px.
       </p>
@@ -109,31 +109,32 @@ export const insetSpacingTall = () => (
 
 insetSpacingTall.story = {
   name: 'Inset Tall',
+  decorators: [storyFn => `<div class="sb-inset-spacing-tall-example">${storyFn()}</div>`],
 };
 
 export const stackSpacing = () => (
   `
-    <div class="sb-spacing-stack-xs">
+    <div>
       <p class="sprk-b-TypeBodyTwo">
         The grey shows the extra small stack spacing of 4px.
       </p>
     </div>
-    <div class="sb-spacing-stack-s sprk-u-man">
+    <div class="sprk-u-man">
       <p class="sprk-b-TypeBodyTwo">
         The grey shows the small stack spacing of 8px.
       </p>
     </div>
-    <div class="sb-spacing-stack-m sprk-u-man">
+    <div class="sprk-u-man">
       <p class="sprk-b-TypeBodyTwo">
         The grey shows the medium stack spacing of 16px.
       </p>
     </div>
-    <div class="sb-spacing-stack-l sprk-u-man">
+    <div class="sprk-u-man">
       <p class="sprk-b-TypeBodyTwo">
         The grey shows the large stack spacing of 32px.
       </p>
     </div>
-    <div class="sb-spacing-stack-xl sprk-u-man">
+    <div class="sprk-u-man">
       <p class="sprk-b-TypeBodyTwo">
         The grey shows the extra large stack spacing of 64px.
       </p>
@@ -143,6 +144,7 @@ export const stackSpacing = () => (
 
 stackSpacing.story = {
   name: 'Stack',
+  decorators: [storyFn => `<div class="sb-inset-spacing-stack-example">${storyFn()}</div>`],
 };
 
 
@@ -151,37 +153,38 @@ export const inlineSpacing = () => (
     <p>
       Inline items with xs spacing.
     </p>
-    <span class="sb-spacing-inline-right-xs">Cats</span><span class="sb-spacing-inline-right-xs">Dogs</span><span class="sb-spacing-inline-right-xs">Birds</span>
+    <span>Cats</span><span>Dogs</span><span>Birds</span>
 
     <p>
       Inline items with s spacing.
     </p>
-    <span class="sb-spacing-inline-right-s">Cats</span><span class="sb-spacing-inline-right-s">Dogs</span><span class="sb-spacing-inline-right-s">Birds</span>
+    <span>Cats</span><span>Dogs</span><span>Birds</span>
 
     <p>
       Inline items with m spacing.
     </p>
-    <span class="sb-spacing-inline-right-m">Cats</span><span class="sb-spacing-inline-right-m">Dogs</span><span class="sb-spacing-inline-right-m">Birds</span>
+    <span>Cats</span><span>Dogs</span><span>Birds</span>
 
     <p>
       Inline items with l spacing.
     </p>
-    <span class="sb-spacing-inline-right-l">Cats</span><span class="sb-spacing-inline-right-l">Dogs</span><span class="sb-spacing-inline-right-l">Birds</span>
+    <span>Cats</span><span>Dogs</span><span>Birds</span>
 
     <p>
       Inline items with xl spacing.
     </p>
-    <span class="sb-spacing-inline-right-xl">Cats</span><span class="sb-spacing-inline-right-xl">Dogs</span><span class="sb-spacing-inline-right-xl">Birds</span>
+    <span>Cats</span><span>Dogs</span><span>Birds</span>
 
     <p>
       Inline items with no right margin on the last item.
     </p>
-    <span class="sb-spacing-inline-right-m">Fish</span><span class="sb-spacing-inline-right-m">Frogs</span><span>Turtles</span> 
+    <span>Fish</span><span>Frogs</span><span>Turtles</span> 
   `
 );
 
 inlineSpacing.story = {
   name: 'Inline',
+  decorators: [storyFn => `<div class="sb-inset-spacing-inline-example">${storyFn()}</div>`],
 };
 
 export const inlineSpacingMisc = () => (
@@ -189,25 +192,26 @@ export const inlineSpacingMisc = () => (
     <p>
       The grey shows a margin right using the the misc "a" spacing of 24px.
     </p>
-    <span class="sb-spacing-inline-right-misc-a">Cats</span><span class="sb-spacing-inline-right-misc-a">Dogs</span><span class="sb-spacing-inline-right-misc-a">Birds</span>
+    <span>Cats</span><span>Dogs</span><span>Birds</span>
 
     <p>
       The grey shows a margin right using the misc "b" spacing of 40px.
     </p>
-    <span class="sb-spacing-inline-right-misc-b">Cats</span><span class="sb-spacing-inline-right-misc-b">Dogs</span><span class="sb-spacing-inline-right-misc-b">Birds</span>
+    <span>Cats</span><span>Dogs</span><span>Birds</span>
 
     <p>
       The grey shows a margin right using the misc "c" spacing of 48px.
     </p>
-    <span class="sb-spacing-inline-right-misc-c">Cats</span><span class="sb-spacing-inline-right-misc-c">Dogs</span><span class="sb-spacing-inline-right-misc-c">Birds</span>
+    <span>Cats</span><span>Dogs</span><span>Birds</span>
 
     <p>
       The grey shows a margin right using the misc "d" spacing of 80px.
     </p>
-    <span class="sb-spacing-inline-right-misc-d">Cats</span><span class="sb-spacing-inline-right-misc-d">Dogs</span><span class="sb-spacing-inline-right-misc-d">Birds</span>
+    <span>Cats</span><span>Dogs</span><span>Birds</span>
   `
 );
 
 inlineSpacingMisc.story = {
   name: 'Misc',
+  decorators: [storyFn => `<div class="sb-inset-spacing-misc-example">${storyFn()}</div>`],
 };
