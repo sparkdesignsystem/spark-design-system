d56b7dfd6ae89e1cbdd61c62038a077388ee8ad1
Rob Copeland <robcopeland@fastmail.com>
2019-11-19 09:24:16 -0500

    CR updates

--- html/base/typography.stories.js
+++ html/base/typography.stories.js
@@ -1,10 +1,15 @@
 export default {
-  title: 'Fundamentals|Typography',
+  title: 'Fundamentals/Typography',
   decorators: [
     story => `
       <div class="sprk-o-Box sb-decorate">${story()}</div>
     `,
   ],
+  parameters: {
+    info: `
+##### For design and usage information check out the [documentation.](https://spark-docs.netlify.com/using-spark/foundations/typography)
+    `,
+  },
 };
 
 export const pageTitle = () => `
--- html/utilities/spacing.stories.js
+++ html/utilities/spacing.stories.js
@@ -1,10 +1,15 @@
 export default {
-  title: 'Fundamentals|Spacing',
+  title: 'Fundamentals/Spacing',
   decorators: [
     story => `
       <div class="sprk-o-Box sb-decorate">${story()}</div>
     `,
   ],
+  parameters: {
+    info: `
+##### For design and usage information check out the [documentation.](https://spark-docs.netlify.com/using-spark/foundations/spacing)
+    `,
+  },
 };
 
 export const insetSpacing = () => (
