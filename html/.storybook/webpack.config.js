850dce3a052612a5f8c9f4c77b9dd9b197bee1dc
Febbraro <amberfebbraro@quickenloans.com>
2019-11-19 08:30:00 -0500

    revert testing changes

--- html/.storybook/webpack.config.js
+++ html/.storybook/webpack.config.js
@@ -3,7 +3,6 @@ module.exports = ({ config }) => {
   config.resolve.modules = [
     ...(config.resolve.modules || []),
     path.resolve(__dirname, '../node_modules'),
-    path.resolve(__dirname, '../.babelrc'),
   ];
 
   return config;
