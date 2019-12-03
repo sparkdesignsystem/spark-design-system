84227ed568bad7158d8db505b9332ee5ef47a5ba
Rob Copeland <robcopeland@fastmail.com>
2019-12-03 11:28:26 -0500

    adding states for textarea

--- html/base/inputs/Textarea.stories.js
+++ html/base/inputs/Textarea.stories.js
@@ -38,3 +38,73 @@ export const textarea = () => (
 textarea.story = {
   name: 'Default',
 };
+
+export const invalidTextarea = () => (
+  `
+    <div class="sprk-b-InputContainer">
+      <label
+        for="textarea-error"
+        class="sprk-b-Label"
+      >
+        Description
+      </label>
+
+      <textarea
+        class="sprk-b-TextArea sprk-b-TextArea--error sprk-u-Width-100"
+        id="textarea-error"
+        data-id="textarea-error"
+        aria-describedby="textarea-error--error-container"
+        aria-invalid="true"
+      ></textarea>
+
+      <div
+        class="sprk-b-ErrorContainer"
+        id="textarea-error--error-container"
+      >
+        <svg
+         class="sprk-c-Icon sprk-c-Icon--m sprk-b-ErrorIcon"
+         viewBox="0 0 64 64"
+        >
+          <use xlink:href="#exclamation-filled-small" />
+        </svg>
+        <div class="sprk-b-ErrorText">
+          There is an error on this field.
+        </div>
+      </div>
+    </div>
+  `
+);
+
+invalidTextarea.story = {
+  name: 'Invalid',
+};
+
+export const disabledTextarea = () => (
+  `
+    <div class="sprk-b-InputContainer">
+      <label
+        for="textarea-disabled"
+        class="sprk-b-Label sprk-b-Label--disabled"
+      >
+        Description
+      </label>
+
+      <textarea
+        class="sprk-b-TextArea sprk-u-Width-100"
+        id="textarea-disabled"
+        data-id="textarea-disabled"
+        aria-describedby="textarea-disabled--error-container"
+        disabled
+      ></textarea>
+
+      <div
+        class="sprk-b-ErrorContainer"
+        id="textarea--error-container"
+      ></div>
+    </div>
+  `
+);
+
+disabledTextarea.story = {
+  name: 'Disabled',
+};
