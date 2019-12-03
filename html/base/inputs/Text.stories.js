885598968f6ea58360a08b30a15152014551fc9b
Rob Copeland <robcopeland@fastmail.com>
2019-12-03 11:20:28 -0500

    adding states for text input

--- html/base/inputs/Text.stories.js
+++ html/base/inputs/Text.stories.js
@@ -1,6 +1,3 @@
-import { useEffect } from '@storybook/client-api';
-import { requiredTextInput } from '../requiredTextInput';
-
 export default {
   title: 'Components/Input/Text',
   decorators: [
@@ -13,12 +10,7 @@ export default {
   },
 };
 
-export const textInput = () => {
-  useEffect(() => {
-    requiredTextInput();
-  }, []);
-
-  return `
+export const textInput = () => `
     <div class="sprk-b-InputContainer">
       <label
         for="text-input"
@@ -42,8 +34,76 @@ export const textInput = () => {
       ></div>
     </div>
   `;
-};
 
 textInput.story = {
   name: 'Default',
+};
+
+export const invalidTextInput = () => `
+    <div class="sprk-b-InputContainer">
+      <label
+        for="text-input-error"
+        class="sprk-b-Label"
+      >
+        Text Input Label
+      </label>
+
+      <input
+        class="sprk-b-TextInput sprk-b-TextInput--error sprk-u-Width-100"
+        id="text-input-error"
+        data-id="text-input-error"
+        type="text"
+        value=""
+        aria-describedby="text-input-error--error-container"
+      >
+
+      <div
+        class="sprk-b-ErrorContainer"
+        id="text-input-error--error-container"
+      >
+        <svg
+         class="sprk-c-Icon sprk-c-Icon--m sprk-b-ErrorIcon"
+         viewBox="0 0 64 64"
+        >
+          <use xlink:href="#exclamation-filled-small" />
+        </svg>
+        <div class="sprk-b-ErrorText">
+          There is an error on this field.
+        </div>
+      </div>
+    </div>
+  `;
+
+invalidTextInput.story = {
+  name: 'Invalid',
+};
+
+export const disabledTextInput = () => `
+    <div class="sprk-b-InputContainer">
+      <label
+        for="text-input-disabled"
+        class="sprk-b-Label sprk-b-Label--disabled"
+      >
+        Text Input Label
+      </label>
+
+      <input
+        class="sprk-b-TextInput sprk-u-Width-100"
+        id="text-input-disabled"
+        data-id="text-input-disabled"
+        type="text"
+        value=""
+        aria-describedby="text-input-disabled--error-container"
+        disabled
+      >
+
+      <div
+        class="sprk-b-ErrorContainer"
+        id="text-input-disabled--error-container"
+      ></div>
+    </div>
+  `;
+
+disabledTextInput.story = {
+  name: 'Disabled',
 };
