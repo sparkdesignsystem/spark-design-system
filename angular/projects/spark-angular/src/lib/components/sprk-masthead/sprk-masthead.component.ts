b57d67de770f3ab3cdb595c5fd22d5b203519ad3
Febbraro <amberfebbraro@quickenloans.com>
2019-12-04 09:03:09 -0500

    add missing analyticsString="link.analyticsString" to sprk-masthead

--- angular/projects/spark-angular/src/lib/components/sprk-masthead/sprk-masthead.component.ts
+++ angular/projects/spark-angular/src/lib/components/sprk-masthead/sprk-masthead.component.ts
@@ -122,12 +122,14 @@ import {
                   additionalTriggerClasses="sprk-b-Link--plain sprk-c-Masthead__link sprk-c-Masthead__link--big-nav"
                   additionalClasses="sprk-u-TextAlign--left"
                   triggerIconType="chevron-down"
+                  analyticsString="link.analyticsString"
                   [triggerText]="link.text"
                 ></sprk-dropdown>
               </div>
               <div *ngIf="!link.subNav">
                 <sprk-link
                   linkType="plain"
+                  analyticsString="link.analyticsString"
                   additionalClasses="sprk-c-Masthead__link sprk-c-Masthead__link--big-nav"
                   [href]="link.href"
                 >
