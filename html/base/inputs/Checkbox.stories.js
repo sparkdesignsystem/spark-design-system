6e6fbd4035062b0406fe1b484ec86f4ccb621010
Rob Copeland <robcopeland@fastmail.com>
2019-12-04 14:57:33 -0500

    adds state for checkbox and date

--- html/base/inputs/Checkbox.stories.js
+++ html/base/inputs/Checkbox.stories.js
@@ -1,6 +1,3 @@
-import { useEffect } from '@storybook/addons';
-import { requiredTick } from '../requiredTick';
-
 export default {
   title: 'Components/Input/Checkbox',
   decorators: [
@@ -13,12 +10,7 @@ export default {
   },
 };
 
-export const checkbox = () => {
-  useEffect(() => {
-    requiredTick();
-  }, []);
-
-  return `
+export const checkbox = () => `
     <div class="sprk-b-InputContainer">
       <fieldset class="sprk-b-Fieldset">
         <legend class="sprk-b-Legend">
@@ -83,8 +75,159 @@ export const checkbox = () => {
       </div>
     </div>
   `;
-};
 
 checkbox.story = {
   name: 'Default',
+};
+
+export const invalidCheckbox = () => `
+    <div class="sprk-b-InputContainer">
+      <fieldset class="sprk-b-Fieldset">
+        <legend class="sprk-b-Legend">
+          <label class="sprk-b-Label">
+            Checkbox Group Label
+          </label>
+        </legend>
+
+        <div class="sprk-b-SelectionContainer">
+          <input
+            id="checkbox-1"
+            data-id="checkbox-1"
+            type="checkbox"
+            aria-describedby="checkbox--error-container"
+          >
+
+          <label
+            for="checkbox-1"
+            class="sprk-b-Label sprk-b-Label--inline"
+          >
+            Checkbox Item 1
+          </label>
+        </div>
+
+        <div class="sprk-b-SelectionContainer">
+          <input
+            id="checkbox-2"
+            data-id="checkbox-2"
+            type="checkbox"
+            aria-describedby="checkbox--error-container"
+          >
+
+          <label
+            for="checkbox-2"
+            class="sprk-b-Label sprk-b-Label--inline"
+          >
+            Checkbox Item 2
+          </label>
+        </div>
+
+        <div class="sprk-b-SelectionContainer">
+          <input
+            id="checkbox-3"
+            data-id="checkbox-3"
+            type="checkbox"
+            aria-describedby="checkbox--error-container"
+          >
+
+          <label
+            for="checkbox-3"
+            class="sprk-b-Label sprk-b-Label--inline"
+          >
+            Checkbox Item 3
+          </label>
+        </div>
+      </fieldset>
+
+      <div
+        class="sprk-b-ErrorContainer"
+        id="checkbox--error-container"
+      >
+        <svg
+          class="sprk-c-Icon sprk-c-Icon--m sprk-b-ErrorIcon"
+          viewBox="0 0 64 64"
+        >
+          <use xlink:href="#exclamation-filled-small" />
+        </svg>
+        <div class="sprk-b-ErrorText">
+          There is an error on this field.
+        </div>
+      </div>
+    </div>
+  `;
+
+invalidCheckbox.story = {
+  name: 'Invalid',
+};
+
+export const disabledCheckbox = () => `
+    <div class="sprk-b-InputContainer">
+      <fieldset class="sprk-b-Fieldset">
+        <legend class="sprk-b-Legend">
+          <label class="sprk-b-Label sprk-b-Label--disabled">
+            Checkbox Group Label
+          </label>
+        </legend>
+
+        <div class="sprk-b-SelectionContainer">
+          <input
+            id="checkbox-1"
+            data-id="checkbox-1"
+            type="checkbox"
+            aria-describedby="checkbox--error-container"
+            disabled
+          >
+
+          <label
+            for="checkbox-1"
+            class="sprk-b-Label sprk-b-Label--inline sprk-b-Label--disabled"
+          >
+            Checkbox Item 1
+          </label>
+        </div>
+
+        <div class="sprk-b-SelectionContainer">
+          <input
+            id="checkbox-2"
+            data-id="checkbox-2"
+            type="checkbox"
+            aria-describedby="checkbox--error-container"
+            disabled
+          >
+
+          <label
+            for="checkbox-2"
+            class="sprk-b-Label sprk-b-Label--inline sprk-b-Label--disabled"
+          >
+            Checkbox Item 2
+          </label>
+        </div>
+
+        <div class="sprk-b-SelectionContainer">
+          <input
+            id="checkbox-3"
+            data-id="checkbox-3"
+            type="checkbox"
+            aria-describedby="checkbox--error-container"
+            disabled
+          >
+
+          <label
+            for="checkbox-3"
+            class="sprk-b-Label sprk-b-Label--inline sprk-b-Label--disabled"
+          >
+            Checkbox Item 3
+          </label>
+        </div>
+      </fieldset>
+
+      <div
+        class="sprk-b-ErrorContainer"
+        id="checkbox--error-container"
+      >
+      </div>
+    </div>
+  `;
+
+disabledCheckbox.story = {
+  name: 'Disabled',
 };
