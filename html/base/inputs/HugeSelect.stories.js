0e2db01e4855cc495ed89a929e7ed978ecbc8ce7
Rob Copeland <robcopeland@fastmail.com>
2019-12-04 09:52:08 -0500

    adds states for huge text and huge select

--- html/base/inputs/HugeSelect.stories.js
+++ html/base/inputs/HugeSelect.stories.js
@@ -1,5 +1,4 @@
 import { useEffect } from '@storybook/client-api';
-import { requiredSelect } from '../requiredSelect';
 import { hugeInput } from '../hugeInput';
 import '../../utilities/polyfills/classListSVG';
 
@@ -17,7 +16,6 @@ export default {
 
 export const hugeSelectBox = () => {
   useEffect(() => {
-    requiredSelect();
     hugeInput();
   }, []);
 
@@ -74,4 +72,135 @@ export const hugeSelectBox = () => {
 
 hugeSelectBox.story = {
   name: 'Default',
+};
+
+export const invalidHugeSelectBox = () => {
+  useEffect(() => {
+    hugeInput();
+  }, []);
+
+  return `
+    <div class="sprk-b-InputContainer sprk-b-InputContainer--huge">
+      <select
+        class="sprk-b-Select sprk-b-Select--error"
+        id="select-huge"
+        data-id="select-huge"
+        aria-describedby="select-huge--error-container"
+        data-sprk-input="huge"
+      >
+        <option
+          value=""
+          disabled
+          selected
+          hidden
+        ></option>
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+        <optgroup label="Grouped Options">
+          <option value="g1">Grouped Option 1</option>
+          <option value="g2">Grouped Option 2</option>
+          <option value="g3">Grouped Option 3</option>
+        </optgroup>
+      </select>
+
+      <label
+        for="select-huge"
+        class="sprk-b-Label"
+      >
+        Huge Select Label
+      </label>
+
+      <svg
+        class="
+          sprk-c-Icon
+          sprk-c-Icon--stroke-current-color
+          sprk-b-SelectContainer__icon
+        "
+        viewBox="0 0 64 64"
+      >
+        <use xlink:href="#chevron-down" />
+      </svg>
+
+      <div
+        class="sprk-b-ErrorContainer"
+        id="select-huge--error-container"
+      >
+        <svg
+          class="sprk-c-Icon sprk-c-Icon--m sprk-b-ErrorIcon"
+          viewBox="0 0 64 64"
+        >
+          <use xlink:href="#exclamation-filled-small" />
+        </svg>
+        <div class="sprk-b-ErrorText">
+          There is an error on this field.
+        </div>
+      </div>
+    </div>
+  `;
+};
+
+invalidHugeSelectBox.story = {
+  name: 'Invalid',
+};
+
+export const disabledHugeSelectBox = () => {
+  useEffect(() => {
+    hugeInput();
+  }, []);
+
+  return `
+    <div class="sprk-b-InputContainer sprk-b-InputContainer--huge">
+      <select
+        class="sprk-b-Select"
+        id="select-huge"
+        data-id="select-huge"
+        aria-describedby="select-huge--error-container"
+        data-sprk-input="huge"
+        disabled
+      >
+        <option
+          value=""
+          disabled
+          selected
+          hidden
+        ></option>
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+        <optgroup label="Grouped Options">
+          <option value="g1">Grouped Option 1</option>
+          <option value="g2">Grouped Option 2</option>
+          <option value="g3">Grouped Option 3</option>
+        </optgroup>
+      </select>
+
+      <label
+        for="select-huge"
+        class="sprk-b-Label sprk-b-Label--disabled"
+      >
+        Huge Select Label
+      </label>
+
+      <svg
+        class="
+          sprk-c-Icon
+          sprk-c-Icon--stroke-current-color
+          sprk-b-SelectContainer__icon
+        "
+        viewBox="0 0 64 64"
+      >
+        <use xlink:href="#chevron-down" />
+      </svg>
+
+      <div
+        class="sprk-b-ErrorContainer"
+        id="select-huge--error-container"
+      ></div>
+    </div>
+  `;
+};
+
+disabledHugeSelectBox.story = {
+  name: 'Disabled',
 };
--- html/base/inputs/HugeText.stories.js
+++ html/base/inputs/HugeText.stories.js
@@ -50,3 +50,91 @@ export const hugeTextInput = () => {
 hugeTextInput.story = {
   name: 'Default',
 };
+
+export const invalidHugeTextInput = () => {
+  useEffect(() => {
+    requiredTextInput();
+    hugeInput();
+  }, []);
+
+  return `
+    <div class="sprk-b-InputContainer sprk-b-InputContainer--huge">
+      <input
+        class="
+          sprk-b-TextInput
+          sprk-b-TextInput--error
+          sprk-u-Width-100"
+        data-sprk-input="huge"
+        id="text-input-huge"
+        data-id="text-input-huge"
+        type="text"
+        aria-describedby="text-input-huge--error-container"
+        placeholder="Placeholder"
+      >
+
+      <label
+        for="text-input-huge"
+        class="sprk-b-Label"
+      >
+        Text Input Label
+      </label>
+
+      <div
+        class="sprk-b-ErrorContainer"
+        id="text-input-huge--error-container"
+      >
+        <svg
+          class="sprk-c-Icon sprk-c-Icon--m sprk-b-ErrorIcon"
+          viewBox="0 0 64 64"
+        >
+          <use xlink:href="#exclamation-filled-small" />
+        </svg>
+        <div class="sprk-b-ErrorText">
+          There is an error on this field.
+        </div>
+      </div>
+    </div>
+  `;
+};
+
+invalidHugeTextInput.story = {
+  name: 'Invalid',
+};
+
+export const disabledHugeTextInput = () => {
+  useEffect(() => {
+    requiredTextInput();
+    hugeInput();
+  }, []);
+
+  return `
+    <div class="sprk-b-InputContainer sprk-b-InputContainer--huge">
+      <input
+        class="sprk-b-TextInput sprk-u-Width-100"
+        data-sprk-input="huge"
+        id="text-input-huge"
+        data-id="text-input-huge"
+        type="text"
+        aria-describedby="text-input-huge--error-container"
+        placeholder="Placeholder"
+        disabled
+      >
+
+      <label
+        for="text-input-huge"
+        class="sprk-b-Label sprk-b-Label--disabled"
+      >
+        Text Input Label
+      </label>
+
+      <div
+        class="sprk-b-ErrorContainer"
+        id="text-input-huge--error-container"
+      ></div>
+    </div>
+  `;
+};
+
+disabledHugeTextInput.story = {
+  name: 'Disabled',
+};
