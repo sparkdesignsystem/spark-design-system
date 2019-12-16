9b889abcd3f6c40b9c4450475eab529bf6f97017
Rob Copeland <robcopeland@fastmail.com>
2019-12-16 16:42:28 -0500

    Added invalid / disabled states for select, ssn, text and textarea.

--- react/src/base/inputs/Text.stories.js
+++ react/src/base/inputs/Text.stories.js
@@ -22,14 +22,46 @@ export const textInput = () => (
   <SprkTextInput
     label="Text Input Label"
     name="text-input-label"
-    valid={true}
-    disabled={false}
-    errorMessage="There is an error on this field."
   />
 );
 
 textInput.story = {
   name: 'Default',
+  parameters: {
+    jest: [
+      'SprkTextInput',
+    ]
+  },
+};
+
+export const invalidTextInput = () => (
+  <SprkTextInput
+    label="Text Input Label"
+    name="text-input-label"
+    valid={false}
+    errorMessage="There is an error on this field."
+  />
+);
+
+invalidTextInput.story = {
+  name: 'Invalid',
+  parameters: {
+    jest: [
+      'SprkTextInput',
+    ]
+  },
+};
+
+export const disabledTextInput = () => (
+  <SprkTextInput
+    label="Text Input Label"
+    name="text-input-label"
+    disabled
+  />
+);
+
+disabledTextInput.story = {
+  name: 'Disabled',
   parameters: {
     jest: [
       'SprkTextInput',
