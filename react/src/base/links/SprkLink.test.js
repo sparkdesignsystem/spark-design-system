129b4fbdcb228e37eba5d51769d33da4777f8055
corcorankt1 <guern1kn@gmail.com>
2019-12-23 11:16:57 -0500

    update unit test labels - SprkLink & SprkList

--- react/src/base/links/SprkLink.test.js
+++ react/src/base/links/SprkLink.test.js
@@ -5,26 +5,26 @@ import SprkLink from './SprkLink';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-it('should display a link element with the correct base class if the variant'
+it('SprkLink: Should display a link element with the correct base class if the variant'
   + ' is not unstyled', () => {
   const wrapper = shallow(<SprkLink />);
   expect(wrapper.find('a.sprk-b-Link').length).toBe(1);
 });
 
-it('should display a link element without the sprk-b-Link class if the'
+it('SprkLink: Should display a link element without the sprk-b-Link class if the'
   + ' variant is unstyled', () => {
   const wrapper = shallow(<SprkLink variant="unstyled" />);
   expect(wrapper.find('a').hasClass('sprk-b-Link')).toBe(false);
 });
 
-it('should display a link element with correct classes when variant'
+it('SprkLink: Should display a link element with correct classes when variant'
   + ' is simple', () => {
   const wrapper = shallow(<SprkLink variant="simple" />);
   expect(wrapper.find('a').hasClass('sprk-b-Link')).toBe(true);
   expect(wrapper.find('a').hasClass('sprk-b-Link--simple')).toBe(true);
 });
 
-it('should display a link element with correct classes when variant is '
+it('SprkLink: Should display a link element with correct classes when variant is '
   + 'has-icon', () => {
   const wrapper = shallow(<SprkLink variant="has-icon" />);
   expect(wrapper.find('a').hasClass('sprk-b-Link')).toBe(true);
@@ -32,26 +32,26 @@ it('should display a link element with correct classes when variant is '
   expect(wrapper.find('a').hasClass('sprk-b-Link--has-icon')).toBe(true);
 });
 
-it('should display a link element with correct classes when variant is'
+it('SprkLink: Should display a link element with correct classes when variant is'
   + ' plain', () => {
   const wrapper = shallow(<SprkLink variant="plain" />);
   expect(wrapper.find('a').hasClass('sprk-b-Link')).toBe(true);
   expect(wrapper.find('a').hasClass('sprk-b-Link--plain')).toBe(true);
 });
 
-it('should display a link element with correct classes when variant is'
+it('SprkLink: Should display a link element with correct classes when variant is'
   + ' disabled', () => {
   const wrapper = shallow(<SprkLink variant="disabled" />);
   expect(wrapper.find('a').hasClass('sprk-b-Link')).toBe(true);
   expect(wrapper.find('a').hasClass('sprk-b-Link--disabled')).toBe(true);
 });
 
-it('href should equal # if not provided', () => {
+it('SprkLink: href should equal # if not provided', () => {
   const wrapper = shallow(<SprkLink />);
   expect(wrapper.find('a[href="#"]').length).toBe(1);
 });
 
-it('should prevent default when no href is provided', () => {
+it('SprkLink: Should prevent default when no href is provided', () => {
   const wrapper = shallow(<SprkLink />);
   let prevented = false;
   wrapper.find('a').simulate('click', {
@@ -62,7 +62,7 @@ it('should prevent default when no href is provided', () => {
   expect(prevented).toBe(true);
 });
 
-it('should not render an href if none is provided and element passed is a'
+it('SprkLink: Should not render an href if none is provided and element passed is a'
   + ' router link', () => {
   const wrapper = shallow(<SprkLink element={Link} to="button" />);
   expect(wrapper.find('a[href="#"]').length).toBe(0);
