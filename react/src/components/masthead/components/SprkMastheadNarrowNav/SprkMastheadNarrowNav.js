0a43bf2be47ec465f267d92c79fe2158c7aed468
RVMendoza <RVxMendoza@gmail.com>
2020-01-07 16:52:31 -0500

    Improve SprkMastheadNarrowNav comments

--- react/src/components/masthead/components/SprkMastheadNarrowNav/SprkMastheadNarrowNav.js
+++ react/src/components/masthead/components/SprkMastheadNarrowNav/SprkMastheadNarrowNav.js
@@ -40,29 +40,69 @@ SprkMastheadNarrowNav.propTypes = {
    */
   isOpen: PropTypes.bool,
   /**
-   * Data to build the links inside the Narrow Navigation.
+   * Configuration to build the links inside the Narrow Navigation.
    */
   links: PropTypes.arrayOf(
     PropTypes.shape({
+      /**
+       * Determines if link renders as an anchor tag, or router link.
+       */
       element: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+      /**
+       * Text of the link
+       */
       text: PropTypes.string,
+      /**
+       * Expects an array containing link objects.
+       * Will be treated as a subnav to the link.
+       */
       subNavLinks: PropTypes.arrayOf(
         PropTypes.shape({
+          /** Text of subnav link */
           text: PropTypes.string,
+          /**
+           * Determines if link renders as an anchor tag, or router link.
+           */
           element: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
         }),
       ),
     }),
   ).isRequired,
+  /**
+   * Expects a selector object that
+   * represents choices to be supplied
+   * to the selector in the wide viewport
+   * version of the masthead.
+   */
   selector: PropTypes.shape({
+    /** The function that runs once a choice is made. */
     choiceFunction: PropTypes.func,
+    /**
+     * 	Expects a Component to render at the foot of the open selector.
+     */
     footer: PropTypes.node,
+    /**
+     * Object that configures the items inside the selector
+     */
     items: PropTypes.arrayOf(
       PropTypes.shape({
+        /**
+         * Determines if link renders as an anchor tag, or router link.
+         */
         element: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+        /**
+         * Additional text providing context for item.
+         */
         information: PropTypes.string,
+        // TODO: get rid of this -- it does nothing but add a nonvalid "text" attribute on the item
         text: PropTypes.string,
+        /**
+         * The main headline of the item
+         */
         title: PropTypes.string,
+        /**
+         * The value assigned for the selector
+         */
         value: PropTypes.string,
       }),
     ),
