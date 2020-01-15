62ece412f9eff2e427231234a380abf8b79dd47e
Febbraro <amberfebbraro@quickenloans.com>
2020-01-15 14:28:47 -0500

    add proptypes to context

--- src/components/ContextMenu.js
+++ src/components/ContextMenu.js
@@ -2,38 +2,87 @@ import React from 'react';
 import { SprkLink } from '@sparkdesignsystem/spark-react';
 import { Link } from 'gatsby';
 import classnames from 'classnames';
+import PropTypes from 'prop-types';
 
-function ContextMenu({context, setContext, className, autoNav}) {
+function ContextMenu({ context, setContext, className, autoNav }) {
   return (
     <div className="docs-context-menu">
       <ul className={className}>
         <li>
           <SprkLink
-            additionalClasses={classnames({'docs-context-menu__item--active': context==='installing-spark'})}
-            onClick={(e) => {if (!autoNav) {e.preventDefault();} setContext('installing-spark');}}
+            additionalClasses={
+              classnames(
+                {
+                  'docs-context-menu__item--active': context === 'installing-spark',
+                },
+              )
+            }
+            onClick={
+              (e) => {
+                if (!autoNav) {
+                  e.preventDefault();
+                }
+                setContext('installing-spark');
+              }
+            }
             variant="simple"
             element={Link}
-            to="/installing-spark">Installing Spark</SprkLink>
+            to="/installing-spark/setting-up-your-environment"
+          >
+            Installing Spark
+          </SprkLink>
         </li>
         <li>
           <SprkLink
-            additionalClasses={classnames({'docs-context-menu__item--active': context==='using-spark'})}
-            onClick={(e) => {if (!autoNav) {e.preventDefault();} setContext('using-spark');}}
+            additionalClasses={
+              classnames(
+                {
+                  'docs-context-menu__item--active': context === 'using-spark',
+                },
+              )
+            }
+            onClick={
+              (e) => {
+                if (!autoNav) {
+                  e.preventDefault();
+                }
+                setContext('using-spark');
+              }
+            }
             variant="simple"
-            element={Link}
-            to="/using-spark">Using Spark</SprkLink>
+            href="#nogo"
+          >
+            Using Spark
+          </SprkLink>
         </li>
         <li>
           <SprkLink
-            additionalClasses={classnames({'docs-context-menu__item--active': context==='principles'})}
-            onClick={(e) => {if (!autoNav) {e.preventDefault();} setContext('principles');}}
-            element={Link}
-            variant='simple'
-            to="/principles/design-principles">Principles</SprkLink>
+            additionalClasses={
+              classnames(
+                { 'docs-context-menu__item--active': context === 'principles' },
+              )
+            }
+            onClick={
+              (e) => {
+                if (!autoNav) { e.preventDefault(); } setContext('principles');
+              }
+            }
+            variant="simple"
+            href="#nogo"
+          >
+            Principles
+          </SprkLink>
         </li>
       </ul>
     </div>
   );
 }
+
+ContextMenu.propTypes = {
+  context: PropTypes.string,
+  setContext: PropTypes.func,
+  className: PropTypes.string,
+  autoNav: PropTypes.bool,
+};
 
 export default ContextMenu;
