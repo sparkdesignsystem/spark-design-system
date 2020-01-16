50c35564ad89e39d24ea983cfc45c2cb49f80324
Febbraro <amberfebbraro@quickenloans.com>
2020-01-16 11:31:32 -0500

    selector is picky

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,7 +8,7 @@ exports.onRouteUpdate = () => {
     docsearch({ // eslint-disable-line no-undef
       apiKey: '9486baf438a1d7fd0b6c982838d6f9bc',
       indexName: 'sparkdesignsystem',
-      inputSelector: '.docs-header-search',
+      inputSelector: '.docs-header-search .sprk-u-Width-100',
       debug: 'false',
     });
   }
