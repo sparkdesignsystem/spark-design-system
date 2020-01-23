25ee0728fa6cd576b671ae73074764adbe9766ad
Bob Wise <bobwise@quickenloans.com>
2020-01-23 14:06:15 -0500

    convert => arrow functions

--- src/components/markdown-render/blockquote.js
+++ src/components/markdown-render/blockquote.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-function Blockquote(props) {
-  return (
-    <blockquote
-      className="docs-b-Blockquote sprk-u-mvl sprk-u-Measure"
-      {...props}
-    />
-  );
-}
+const Blockquote = props => (
+  <blockquote
+    className="docs-b-Blockquote sprk-u-mvl sprk-u-Measure"
+    {...props}
+  />
+);
 
 export default Blockquote;
