f4d26526cb1f722e3bbf29eb5b05d6ae679d19a0
Bob Wise <bobwise@quickenloans.com>
2020-01-30 15:06:12 -0500

    adding css utilities docs

---
+++ src/components/markdown-render/h4.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function H4(props) {
+  return (
+    <h4 className="docs-b-h4 sprk-b-TypeDisplayFour sprk-u-mbm sprk-u-Measure" {...props} />
+  );
+}
+
+export default H4;
