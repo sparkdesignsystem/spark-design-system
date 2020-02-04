abed96994a9ae63eeb6dd3c47585409e57031d4a
corcorankt1 <guern1kn@gmail.com>
2020-02-04 17:08:57 -0500

    update storybook instance function to include netlify previews

--- storybook-utilities/getStorybookInstance.js
+++ storybook-utilities/getStorybookInstance.js
@@ -2,14 +2,22 @@ const getStorybookInstance = () => {
   const storybookUrl = window.location.href;
   let storybookInstance;
 
-  if (storybookUrl.includes('6006') || storybookUrl.includes('react.sparkdesignsystem')) {
+  if (
+    storybookUrl.includes('6006')
+    || storybookUrl.includes('react.sparkdesignsystem')
+    || storybookUrl.includes('spark-sb-react')
+  ) {
     storybookInstance = 'react';
   } else if (
-    storybookUrl.includes('8006') || storybookUrl.includes('angular.sparkdesignsystem')
+    storybookUrl.includes('8006')
+    || storybookUrl.includes('angular.sparkdesignsystem')
+    || storybookUrl.includes('spark-sb-angular')
   ) {
     storybookInstance = 'angular';
   } else if (
-    storybookUrl.includes('7006') || storybookUrl.includes('html.sparkdesignsystem')
+    storybookUrl.includes('7006')
+    || storybookUrl.includes('html.sparkdesignsystem')
+    || storybookUrl.includes('spark-sb-html')
   ) {
     storybookInstance = 'html';
   } else {
