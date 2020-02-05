5aca1046cfcde739152af3acce5c5a31be3ca3ee
RVMendoza <RVxMendoza@gmail.com>
2020-02-05 13:35:34 -0500

    Left align descriptions and wider tables

--- src/components/markdown-render/table.js
+++ src/components/markdown-render/table.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Table = props => (
-  <div className="sprk-b-TableContainer sprk-u-mvl sprk-u-Measure">
+  <div className="sprk-b-TableContainer sprk-u-mvl docs-b-Table--left-align-last">
     <table className="sprk-b-Table sprk-b-Table--spacing-medium" {...props} />
   </div>
 );
