319c99629ca919b539def6da51e2439cc9d9f865
RVMendoza <RVxMendoza@gmail.com>
2020-02-11 17:47:55 -0500

    Turn into host binding situation

--- angular/projects/spark-angular/src/lib/directives/sprk-box/sprk-box.directive.ts
+++ angular/projects/spark-angular/src/lib/directives/sprk-box/sprk-box.directive.ts
@@ -1,9 +1,9 @@
-import { Directive, ElementRef, OnInit, Input, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnInit, Input, Renderer2, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[sprkBox]'
 })
-export class SprkBoxDirective implements OnInit {
+export class SprkBoxDirective {
   @Input() spacing:
       'flush' |
       'tiny' |
@@ -26,34 +26,33 @@ export class SprkBoxDirective implements OnInit {
       'miscD'
   ;
 
-  ngOnInit(): void {
-    const spaces = {
-      flush: 'sprk-o-Box--flush',
-      tiny: 'sprk-o-Box--tiny',
-      small: 'sprk-o-Box--small',
-      large: 'sprk-o-Box--large',
-      huge: 'sprk-o-Box--huge',
-      insetShortTiny: 'sprk-o-Box--inset-short-tiny',
-      insetShortSmall: 'sprk-o-Box--inset-short-small',
-      insetShortMedium: 'sprk-o-Box--inset-short-medium',
-      insetShortLarge: 'sprk-o-Box--inset-short-large',
-      insetShortHuge: 'sprk-o-Box--inset-short-huge',
-      insetTallTiny: 'sprk-o-Box--inset-tall-tiny',
-      insetTallSmall: 'sprk-o-Box--inset-tall-small',
-      insetTallMedium: 'sprk-o-Box--inset-tall-medium',
-      insetTallLarge: 'sprk-o-Box--inset-tall-large',
-      insetTallHuge: 'sprk-o-Box--inset-tall-huge',
-      miscA: 'sprk-o-Box--misc-a',
-      miscB: 'sprk-o-Box--misc-b',
-      miscC: 'sprk-o-Box--misc-c',
-      miscD: 'sprk-o-Box--misc-d',
-    };
+  spacesList = {
+    flush: 'sprk-o-Box--flush',
+    tiny: 'sprk-o-Box--tiny',
+    small: 'sprk-o-Box--small',
+    large: 'sprk-o-Box--large',
+    huge: 'sprk-o-Box--huge',
+    insetShortTiny: 'sprk-o-Box--inset-short-tiny',
+    insetShortSmall: 'sprk-o-Box--inset-short-small',
+    insetShortMedium: 'sprk-o-Box--inset-short-medium',
+    insetShortLarge: 'sprk-o-Box--inset-short-large',
+    insetShortHuge: 'sprk-o-Box--inset-short-huge',
+    insetTallTiny: 'sprk-o-Box--inset-tall-tiny',
+    insetTallSmall: 'sprk-o-Box--inset-tall-small',
+    insetTallMedium: 'sprk-o-Box--inset-tall-medium',
+    insetTallLarge: 'sprk-o-Box--inset-tall-large',
+    insetTallHuge: 'sprk-o-Box--inset-tall-huge',
+    miscA: 'sprk-o-Box--misc-a',
+    miscB: 'sprk-o-Box--misc-b',
+    miscC: 'sprk-o-Box--misc-c',
+    miscD: 'sprk-o-Box--misc-d'
+  };
 
-    this.renderer.addClass(this.ref.nativeElement, 'sprk-o-Box');
-
-    if (this.spacing) {
-      this.renderer.addClass(this.ref.nativeElement, spaces[this.spacing]);
+  @HostBinding('class') get boxClass() {
+    if (this.spacing && this.spacing === this.spacesList[this.spacing]) {
+      return `sprk-o-Box ${this.spacesList[this.spacing]}`;
     }
+    return `sprk-o-Box`;
   }
 
   /**
