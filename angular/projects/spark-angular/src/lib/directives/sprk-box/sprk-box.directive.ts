539180193b69957361bd45757e6b5d70111b1137
RVMendoza <RVxMendoza@gmail.com>
2020-02-12 14:59:18 -0500

    Turn into individual host listeners

--- angular/projects/spark-angular/src/lib/directives/sprk-box/sprk-box.directive.ts
+++ angular/projects/spark-angular/src/lib/directives/sprk-box/sprk-box.directive.ts
@@ -5,54 +5,121 @@ import { Directive, ElementRef, OnInit, Input, Renderer2, HostBinding } from '@a
 })
 export class SprkBoxDirective {
   @Input() spacing:
-      'flush' |
-      'tiny' |
-      'small' |
-      'large' |
-      'huge' |
-      'insetShortTiny' |
-      'insetShortSmall' |
-      'insetShortMedium' |
-      'insetShortLarge' |
-      'insetShortHuge' |
-      'insetTallTiny' |
-      'insetTallSmall' |
-      'insetTallMedium' |
-      'insetTallLarge' |
-      'insetTallHuge' |
-      'miscA' |
-      'miscB' |
-      'miscC' |
-      'miscD'
-  ;
+    | 'flush'
+    | 'tiny'
+    | 'small'
+    | 'large'
+    | 'huge'
+    | 'insetShortTiny'
+    | 'insetShortSmall'
+    | 'insetShortMedium'
+    | 'insetShortLarge'
+    | 'insetShortHuge'
+    | 'insetTallTiny'
+    | 'insetTallSmall'
+    | 'insetTallMedium'
+    | 'insetTallLarge'
+    | 'insetTallHuge'
+    | 'miscA'
+    | 'miscB'
+    | 'miscC'
+    | 'miscD';
 
-  spacesList = {
-    flush: 'sprk-o-Box--flush',
-    tiny: 'sprk-o-Box--tiny',
-    small: 'sprk-o-Box--small',
-    large: 'sprk-o-Box--large',
-    huge: 'sprk-o-Box--huge',
-    insetShortTiny: 'sprk-o-Box--inset-short-tiny',
-    insetShortSmall: 'sprk-o-Box--inset-short-small',
-    insetShortMedium: 'sprk-o-Box--inset-short-medium',
-    insetShortLarge: 'sprk-o-Box--inset-short-large',
-    insetShortHuge: 'sprk-o-Box--inset-short-huge',
-    insetTallTiny: 'sprk-o-Box--inset-tall-tiny',
-    insetTallSmall: 'sprk-o-Box--inset-tall-small',
-    insetTallMedium: 'sprk-o-Box--inset-tall-medium',
-    insetTallLarge: 'sprk-o-Box--inset-tall-large',
-    insetTallHuge: 'sprk-o-Box--inset-tall-huge',
-    miscA: 'sprk-o-Box--misc-a',
-    miscB: 'sprk-o-Box--misc-b',
-    miscC: 'sprk-o-Box--misc-c',
-    miscD: 'sprk-o-Box--misc-d'
-  };
+  @HostBinding('class.sprk-o-Box') true;
 
-  @HostBinding('class') get boxClass() {
-    if (this.spacing && this.spacing === this.spacesList[this.spacing]) {
-      return `sprk-o-Box ${this.spacesList[this.spacing]}`;
-    }
-    return `sprk-o-Box`;
+  @HostBinding('class.sprk-o-Box--flush')
+  get flush() {
+    return this.spacing === 'flush';
+  }
+
+  @HostBinding('class.sprk-o-Box--tiny')
+  get tiny() {
+    return this.spacing === 'tiny';
+  }
+
+  @HostBinding('class.sprk-o-Box--small')
+  get small() {
+    return this.spacing === 'small';
+  }
+
+  @HostBinding('class.sprk-o-Box--large')
+  get large() {
+    return this.spacing === 'large';
+  }
+
+  @HostBinding('class.sprk-o-Box--huge')
+  get huge() {
+    return this.spacing === 'huge';
+  }
+
+  @HostBinding('class.sprk-o-Box--inset-short-tiny')
+  get insetShortTiny() {
+    return this.spacing === 'insetShortTiny';
+  }
+
+  @HostBinding('class.sprk-o-Box--inset-short-small')
+  get insetShortSmall() {
+    return this.spacing === 'insetShortSmall';
+  }
+
+  @HostBinding('class.sprk-o-Box--inset-short-medium')
+  get insetShortMedium() {
+    return this.spacing === 'insetShortMedium';
+  }
+
+  @HostBinding('class.sprk-o-Box--inset-short-large')
+  get insetShortLarge() {
+    return this.spacing === 'insetShortLarge';
+  }
+
+  @HostBinding('class.sprk-o-Box--inset-short-huge')
+  get insetShortHuge() {
+    return this.spacing === 'insetShortHuge';
+  }
+
+  @HostBinding('class.sprk-o-Box--inset-tall-tiny')
+  get insetTallTiny() {
+    return this.spacing === 'insetTallTiny';
+  }
+
+  @HostBinding('class.sprk-o-Box--inset-tall-small')
+  get insetTallSmall() {
+    return this.spacing === 'insetTallSmall';
+  }
+
+  @HostBinding('class.sprk-o-Box--inset-tall-medium')
+  get insetTallMedium() {
+    return this.spacing === 'insetTallMedium';
+  }
+
+  @HostBinding('class.sprk-o-Box--inset-tall-large')
+  get insetTallLarge() {
+    return this.spacing === 'insetTallLarge';
+  }
+
+  @HostBinding('class.sprk-o-Box--inset-tall-huge')
+  get insetTallHuge() {
+    return this.spacing === 'insetTallHuge';
+  }
+
+  @HostBinding('class.sprk-o-Box--misc-a')
+  get miscA() {
+    return this.spacing === 'miscA';
+  }
+
+  @HostBinding('class.sprk-o-Box--misc-b')
+  get miscB() {
+    return this.spacing === 'miscB';
+  }
+
+ @HostBinding('class.sprk-o-Box--misc-c')
+  get miscC() {
+    return this.spacing === 'miscC';
+  }
+
+ @HostBinding('class.sprk-o-Box--misc-d')
+  get miscD() {
+    return this.spacing === 'miscD';
   }
 
   /**
