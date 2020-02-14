b2105c51a1365448752689d2a852ac34682eb2be
RVMendoza <RVxMendoza@gmail.com>
2020-02-14 16:26:18 -0500

    Remove unecessary comments and clean up some wording

--- angular/projects/spark-angular/src/lib/directives/sprk-box/sprk-box.stories.ts
+++ angular/projects/spark-angular/src/lib/directives/sprk-box/sprk-box.stories.ts
@@ -4,22 +4,10 @@ import { SprkBoxModule } from './sprk-box.module';
 import { markdownDocumentationLinkBuilder } from '../../../../../../../storybook-utilities/markdownDocumentationLinkBuilder';
 
 export default {
-  title: "Components/Box",
+  title: 'Components/Box',
   component: SprkBoxDirective,
   decorators: [storyWrapper(storyContent => `<div class="sprk-o-Box sb-decorate">${storyContent}<div>`)],
   parameters: {
-    info: `
-${markdownDocumentationLinkBuilder('box')}
-Box is a layout component that separates a group of content from its surroundings.
-
-Use Box when you have a group of content that needs to be visually separated from other content on the page through padding.
-
-Since the effects of Box are only seen through whitespace, the examples have a background color to illustrate how Box works.
-
-Box has built in padding sizes that are paired with our Spacing values. Otherwise, it will default to medium spacing.
-
-Refer to [Class Modifiers section](#class-modifiers) for default pixel values.
-`,
     docs: { iframeHeight: 100 }
   }
 };
