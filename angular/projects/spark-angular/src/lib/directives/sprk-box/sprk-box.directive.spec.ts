956da3844bbf8fbbe2fa8169fd19a5cfd2ea808c
Rob Copeland <robcopeland@fastmail.com>
2020-02-24 11:46:29 -0500

    replace test runner with jest

--- angular/projects/spark-angular/src/lib/directives/sprk-box/sprk-box.directive.spec.ts
+++ angular/projects/spark-angular/src/lib/directives/sprk-box/sprk-box.directive.spec.ts
@@ -94,86 +94,86 @@ describe('Spark Box Directive', () => {
   });
 
   it('should have the correct base classes', () => {
-    expect(item0Element).toHaveClass('sprk-o-Box');
+    expect(item0Element.classList.contains('sprk-o-Box'));
   });
 
   it('should apply correct spacing classes for flush spacing', () => {
-    expect(item1Element).toHaveClass('sprk-o-Box--flush');
+    expect(item1Element.classList.contains('sprk-o-Box--flush'));
   });
 
   it('should apply correct spacing classes for tiny spacing', () => {
-    expect(item2Element).toHaveClass('sprk-o-Box--tiny');
+    expect(item2Element.classList.contains('sprk-o-Box--tiny'));
   });
 
   it('should apply correct spacing classes for small spacing', () => {
-    expect(item3Element).toHaveClass('sprk-o-Box--small');
+    expect(item3Element.classList.contains('sprk-o-Box--small'));
   });
 
   it('should apply correct spacing classes for medium spacing', () => {
-    expect(item4Element).toHaveClass('sprk-o-Box--medium');
+    expect(item4Element.classList.contains('sprk-o-Box--medium'));
   });
 
   it('should apply correct spacing classes for large spacing', () => {
-    expect(item5Element).toHaveClass('sprk-o-Box--large');
+    expect(item5Element.classList.contains('sprk-o-Box--large'));
   });
 
   it('should apply correct spacing classes for huge spacing', () => {
-    expect(item6Element).toHaveClass('sprk-o-Box--huge');
+    expect(item6Element.classList.contains('sprk-o-Box--huge'));
   });
 
   it('should apply correct spacing classes for inset-short-tiny spacing', () => {
-    expect(item7Element).toHaveClass('sprk-o-Box--inset-short-tiny');
+    expect(item7Element.classList.contains('sprk-o-Box--inset-short-tiny'));
   });
 
   it('should apply correct spacing classes for inset-short-small spacing', () => {
-    expect(item8Element).toHaveClass('sprk-o-Box--inset-short-small');
+    expect(item8Element.classList.contains('sprk-o-Box--inset-short-small'));
   });
 
   it('should apply correct spacing classes for inset-short-medium spacing', () => {
-    expect(item9Element).toHaveClass('sprk-o-Box--inset-short-medium');
+    expect(item9Element.classList.contains('sprk-o-Box--inset-short-medium'));
   });
 
   it('should apply correct spacing classes for inset-short-large spacing', () => {
-    expect(item10Element).toHaveClass('sprk-o-Box--inset-short-large');
+    expect(item10Element.classList.contains('sprk-o-Box--inset-short-large'));
   });
 
   it('should apply correct spacing classes for inset-short-huge spacing', () => {
-    expect(item11Element).toHaveClass('sprk-o-Box--inset-short-huge');
+    expect(item11Element.classList.contains('sprk-o-Box--inset-short-huge'));
   });
 
   it('should apply correct spacing classes for inset-tall-tiny spacing', () => {
-    expect(item12Element).toHaveClass('sprk-o-Box--inset-tall-tiny');
+    expect(item12Element.classList.contains('sprk-o-Box--inset-tall-tiny'));
   });
 
   it('should apply correct spacing classes for inset-tall-small spacing', () => {
-    expect(item13Element).toHaveClass('sprk-o-Box--inset-tall-small');
+    expect(item13Element.classList.contains('sprk-o-Box--inset-tall-small'));
   });
 
   it('should apply correct spacing classes for inset-tall-medium spacing', () => {
-    expect(item14Element).toHaveClass('sprk-o-Box--inset-tall-medium');
+    expect(item14Element.classList.contains('sprk-o-Box--inset-tall-medium'));
   });
 
   it('should apply correct spacing classes for inset-tall-large spacing', () => {
-    expect(item15Element).toHaveClass('sprk-o-Box--inset-tall-large');
+    expect(item15Element.classList.contains('sprk-o-Box--inset-tall-large'));
   });
 
   it('should apply correct spacing classes for inset-tall-huge spacing', () => {
-    expect(item16Element).toHaveClass('sprk-o-Box--inset-tall-huge');
+    expect(item16Element.classList.contains('sprk-o-Box--inset-tall-huge'));
   });
 
   it('should apply correct spacing classes for misc-a spacing', () => {
-    expect(item17Element).toHaveClass('sprk-o-Box--misc-a');
+    expect(item17Element.classList.contains('sprk-o-Box--misc-a'));
   });
 
   it('should apply correct spacing classes for misc-b spacing', () => {
-    expect(item18Element).toHaveClass('sprk-o-Box--misc-b');
+    expect(item18Element.classList.contains('sprk-o-Box--misc-b'));
   });
 
   it('should apply correct spacing classes for misc-c spacing', () => {
-    expect(item19Element).toHaveClass('sprk-o-Box--misc-c');
+    expect(item19Element.classList.contains('sprk-o-Box--misc-c'));
   });
 
   it('should apply correct spacing classes for misc-d spacing', () => {
-    expect(item20Element).toHaveClass('sprk-o-Box--misc-d');
+    expect(item20Element.classList.contains('sprk-o-Box--misc-d')).toBe(true);
   });
 });
