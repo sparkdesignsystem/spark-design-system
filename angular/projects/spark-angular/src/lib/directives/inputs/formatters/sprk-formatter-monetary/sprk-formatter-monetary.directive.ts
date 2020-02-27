c39c81f28bd05b6eba8e057531cf4901e22526f5
RVMendoza <RVxMendoza@gmail.com>
2020-02-27 11:20:02 -0500

    remove unecessary code

--- angular/projects/spark-angular/src/lib/directives/inputs/formatters/sprk-formatter-monetary/sprk-formatter-monetary.directive.ts
+++ angular/projects/spark-angular/src/lib/directives/inputs/formatters/sprk-formatter-monetary/sprk-formatter-monetary.directive.ts
@@ -5,7 +5,7 @@ import { Validators } from '@angular/forms';
   selector: '[sprkFormatterMonetary]'
 })
 export class SprkFormatterMonetaryDirective {
-  constructor(public ref: ElementRef, @Attribute('validateEqual') public validateEqual: string) {}
+  constructor(public ref: ElementRef) {}
 
 
   @HostListener('blur', ['this.ref.nativeElement.value'])
@@ -14,16 +14,6 @@ export class SprkFormatterMonetaryDirective {
   }
 
   formatMonetary(value): void {
-    // figure out if the field is valid based on ANGULAR'S decision if it's valid and not this pattern
-    // Why does the validator not run again on it's own when it's suppose to (34.345d => erase d)
-
-    // tslint:disable-next-line: max-line-length
-    // Hypothesis: maybe the formatter is writing to the field with the formatted value, and this formatting does not trigger validation.If so, they maybe that's legit, but we'd like to know that's the reason. Otherwise this feels like a bug workaround
-
-    // tslint:disable-next-line: max-line-length
-    // Go on a starter code box, make an input, bind an input event handler, write code that updates the value, check whether or not the handler gets run
-
-
     let number;
     if (this.ref.nativeElement.validity.valid) {
       number = Number(value.replace(/[\$,]/g, ''));
@@ -36,7 +26,6 @@ export class SprkFormatterMonetaryDirective {
         .toLocaleString('en-US', { style: 'currency', currency: 'USD' })
         .replace(/\$/g, '');
     }
-
     return value;
   }
 }
