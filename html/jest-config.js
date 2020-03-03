6049c13427182ce0feabd71fe4334131c260641b
Febbraro <amberfebbraro@quickenloans.com>
2020-03-03 15:41:45 -0500

    add jest config settings

---
+++ html/jest-config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path');
+
+const root = resolve(__dirname, '.');
+module.exports = {
+  rootDir: root,
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.js"],
+  "testMatch": [
+    "**/?(*.)+(tests).[j]s?(x)"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    '<rootDir>/setup-jest.js'
+  ],
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  coverageDirectory: '<rootDir>/coverage'
+};
---
+++ html/setup-jest.js
@@ -0,0 +1,17 @@
+import sinon from 'sinon';
+global.sinon = sinon;
+global.document = window.document;
+global.window = window;
+window.requestAnimationFrame = sinon.stub((func) => {
+  func();
+});
+
+// Simulate window resize event
+const resizeEvent = document.createEvent('Event');
+resizeEvent.initEvent('resize', true, true);
+
+global.window.resizeTo = (width, height) => {
+  global.window.innerWidth = width || global.window.innerWidth;
+  global.window.innerHeight = height || global.window.innerHeight;
+  global.window.dispatchEvent(resizeEvent);
+};
