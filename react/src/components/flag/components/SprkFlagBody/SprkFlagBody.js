568b847d98fc55a78ccd7396e3c2f9a773fe9a0f
RVMendoza <RVxMendoza@gmail.com>
2020-03-17 12:42:56 -0400

    Add additional props to flag and figure

--- react/src/components/flag/components/SprkFlagBody/SprkFlagBody.js
+++ react/src/components/flag/components/SprkFlagBody/SprkFlagBody.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 const SprkFlagBody = (props) => {
-  const { children } = props;
+  const {
+    children,
+    idString,
+    additionalClasses,
+    ...other
+  } = props;
   return (
-    <div className="sprk-o-Flag__body">
+    <div
+      classname={classnames(
+        'sprk-o-Flag__body',
+        additionalClasses
+      )}
+      {...other}
+      data-id={idString}
+    >
       {children}
     </div>
   );
+};
+
+SprkFlagBody.propTypes = {
+  /**
+   * Assigned to the `data-id` attribute serving as a unique selector for automated tools.
+   */
+  idString: PropTypes.string,
+  /**
+   * A space-separated string of classes to add to the outermost container of the component.
+   */
+  additionalClasses: PropTypes.string,
 };
 
 export default SprkFlagBody;
--- react/src/components/flag/components/SprkFlagFigure/SprkFlagFigure.js
+++ react/src/components/flag/components/SprkFlagFigure/SprkFlagFigure.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 const SprkFlagFigure = (props) => {
-  const { children } = props;
+  const {
+    children,
+    idString,
+    additionalClasses,
+    ...other
+  } = props;
   return (
-    <div className="sprk-o-Flag__figure">
+    <div
+      classname={classnames(
+        'sprk-o-Flag__figure',
+        additionalClasses
+      )}
+      data-id={idString}
+      {...other}
+    >
       {children}
     </div>
   );
+};
+
+SprkFlagFigure.propTypes = {
+  /**
+   * Assigned to the `data-id` attribute serving as a unique selector for automated tools.
+   */
+  idString: PropTypes.string,
+  /**
+   * A space-separated string of classes to add to the outermost container of the component.
+   */
+  additionalClasses: PropTypes.string,
 };
 
 export default SprkFlagFigure;
