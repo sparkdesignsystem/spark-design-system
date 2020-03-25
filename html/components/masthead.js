b52cd54e83d311ed11c77323f5ac47377bd6c5c9
Bob Wise <bobwise@quickenloans.com>
2020-03-25 18:33:48 -0400

    cleanup

--- html/components/masthead.js
+++ html/components/masthead.js
@@ -1,5 +1,5 @@
 /* global document window */
-import { throttle, uniqueId } from 'lodash';
+import { throttle } from 'lodash';
 import getElements from '../utilities/getElements';
 import { focusFirstEl } from '../utilities/elementState';
 import { isEscPressed } from '../utilities/keypress';
