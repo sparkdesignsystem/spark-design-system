f0d47197f0129a840db44e697c2a9cf8bdb2fb26
Katie <guern1kn@gmail.com>
2020-04-03 10:27:44 -0400

    Update footer.stories.js

--- html/components/footer.stories.js
+++ html/components/footer.stories.js
@@ -264,7 +264,7 @@ export const defaultStory = () => {
 
             <div class="sprk-o-Stack__item">
               <div data-sprk-toggle="container">
-                <button class="sprk-b-TypeBodyFour sprk-b-Link sprk-b-Link--simple sprk-b-Link--has-icon sprk-u-FontWeight--normal sprk-c-Toggle__trigger" data-sprk-toggle="trigger">
+                <button class="sprk-c-Toggle__trigger sprk-b-TypeBodyFour sprk-u-FontWeight--normal" data-sprk-toggle="trigger">
                   <svg class="sprk-c-Icon sprk-c-Icon--l sprk-c-Icon--stroke-current-color sprk-c-Icon--toggle sprk-u-mrs" data-sprk-toggle="icon" viewBox="0 0 64 64">
                     <use xlink:href="#chevron-down-circle-two-color"></use>
                   </svg>
