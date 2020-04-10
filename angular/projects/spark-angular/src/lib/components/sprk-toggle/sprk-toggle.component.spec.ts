2f8c1bdd1bd80e99df90c59b7697b78274356651
corcorankt1 <guern1kn@gmail.com>
2020-04-10 14:08:53 -0400

    update sprk-toggle and tests

--- angular/projects/spark-angular/src/lib/components/sprk-toggle/sprk-toggle.component.spec.ts
+++ angular/projects/spark-angular/src/lib/components/sprk-toggle/sprk-toggle.component.spec.ts
@@ -91,51 +91,16 @@ describe('SprkToggleComponent', () => {
     expect(element.getAttribute('data-id')).toBeNull();
   });
 
-  it('should generate values if neither aria-controls nor id is present', () => {
-    triggerElement.removeAttribute('aria-controls');
-    contentElement.removeAttribute('id');
+  it('should add aria-controls and id if contentId is not passed', () => {
     fixture.detectChanges();
-
-    expect(triggerElement.hasAttribute('aria-controls')).toEqual(true);
-    expect(contentElement.hasAttribute('id')).toEqual(true);
+    expect(contentElement.getAttribute('id')).toMatch(/sprk_toggle_content_\d/);
     expect(triggerElement.getAttribute('aria-controls')).toEqual(contentElement.getAttribute('id'));
   });
 
-  it('should not change values if aria-controls is provided but id is missing', () => {
-    triggerElement.setAttribute('aria-controls', 'foo');
-    contentElement.removeAttribute('id');
+  it('should add correct aria-controls and id if contentId is passed', () => {
+    component.contentId = 'test_id';
     fixture.detectChanges();
-
-    expect(triggerElement.getAttribute('aria-controls')).toEqual('foo');
-    expect(contentElement.hasAttribute('id')).toEqual(false);
-  });
-
-  it('should use the provided value when aria-controls is missing but id is present', () => {
-    triggerElement.removeAttribute('aria-controls');
-    contentElement.setAttribute('id', 'foo');
-    expect(triggerElement.hasAttribute('aria-controls')).toEqual(false);
-    fixture.detectChanges();
-
-    expect(triggerElement.hasAttribute('aria-controls')).toEqual(true);
-    expect(triggerElement.getAttribute('aria-controls')).toEqual('foo');
-    expect(contentElement.getAttribute('id')).toEqual('foo');
-  });
-
-  it('should not change values if aria-controls and id are both present but have different values', () => {
-    triggerElement.setAttribute('aria-controls', 'foo');
-    contentElement.setAttribute('id', 'bar');
-    fixture.detectChanges();
-
-    expect(triggerElement.getAttribute('aria-controls')).toEqual('foo');
-    expect(contentElement.getAttribute('id')).toEqual('bar');
-  });
-
-  it('should not change values if aria-controls and id are both present and have correct values', () => {
-    triggerElement.setAttribute('aria-controls', 'foo');
-    contentElement.setAttribute('id', 'foo');
-    fixture.detectChanges();
-
-    expect(triggerElement.getAttribute('aria-controls')).toEqual('foo');
-    expect(contentElement.getAttribute('id')).toEqual('foo');
+    expect(contentElement.getAttribute('id')).toEqual('test_id');
+    expect(triggerElement.getAttribute('aria-controls')).toEqual(contentElement.getAttribute('id'));
   });
 });
