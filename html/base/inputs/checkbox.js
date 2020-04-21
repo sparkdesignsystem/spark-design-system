7dffd694e547f3867f20d1b84e4009b0a7bce3a7
corcorankt1 <guern1kn@gmail.com>
2020-04-21 09:22:04 -0400

    write checkbox function for accessibility

--- html/base/inputs/checkbox.js
+++ html/base/inputs/checkbox.js
@@ -1,29 +1,35 @@
 import getElements from '../../utilities/getElements';
 import { uniqueId } from 'lodash';
 
-// Need some help naming this one.
 const generateIdForCheckbox = (element) => {
-
-  // This is going to change based on the structure we decide to go with - currently accounts for prototypes 1-3
+  // Account for label being after the input or before.
   const labelElement = (element.nextElementSibling.tagName === 'LABEL') ? element.nextElementSibling : element.previousElementSibling;
   let inputId = element.getAttribute('id');
+  let labelFor;
   if(labelElement) {
-    let labelFor = labelElement.htmlFor;
+    labelFor = labelElement.htmlFor;
   }
-  /**
-   * Using the same idea as the generateAriaControls function:
-   * 
-   * get id of input
-   * get for of sibling label
-   * 
-   * Warn if for exists but the id does not
-   * 
-   * Warn if for and id both exist but don't match
-   * 
-   * If we don't have a valid id, generate one with lodash
-   * set the value of for
-   * 
-   */
+
+  // Warn if 'for' exists but the 'id' does not
+  if (labelFor && !inputId) {
+    console.warn(`Spark Design System Warning - The value of 'for' on ${labelElement} expects a matching 'id' on ${element}.`);
+    return;
+  }
+
+  // Warn if 'for' and 'id' both exist but don't match
+  if (inputId && labelFor && inputId !== labelFor) {
+    console.warn(`Spark Design System Warning - The value of 'for' on ${labelElement} should match the 'id' on ${element}.`);
+    return;
+  }
+
+  // If we don't have a valid id, generate one with lodash
+  if (!inputId) {
+    inputId = uniqueId(`sprk_checkbox_input_`);
+    element.setAttribute('id', inputId);
+  }
+
+  // set the value of 'for'
+  labelElement.htmlFor = inputId;
 }
 
 const bindCheckboxUIEvents = (element) => {
