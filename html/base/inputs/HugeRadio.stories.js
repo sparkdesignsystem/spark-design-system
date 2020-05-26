f01d0dbe7e04f103656db9474016948f3789a042
corcorankt1 <guern1kn@gmail.com>
2020-05-26 11:57:10 -0400

    state updates and add stories

--- html/base/inputs/HugeRadio.stories.js
+++ html/base/inputs/HugeRadio.stories.js
@@ -106,3 +106,289 @@ defaultStory.story = {
     jest: ['radioInput'],
   }
 };
+
+export const invalidRadioButton = () => {
+  useEffect(() => {
+    radioInput();
+  }, []);
+
+  return `
+    <div class="sprk-b-InputContainer">
+      <fieldset class="sprk-b-Fieldset">
+        <legend class="sprk-b-Legend">
+          <label class="sprk-b-Label">
+            Radio Group Label
+          </label>
+        </legend>
+
+        <div
+          class="sprk-b-SelectionContainer sprk-b-Radio sprk-b-Radio--huge"
+          data-sprk-input="radio"
+        >
+          <input
+            class="sprk-b-Radio__input"
+            id="radio-1"
+            data-id="radio-1"
+            type="radio"
+            name="radio"
+            aria-describedby="radio--error-container"
+          >
+
+          <label
+            for="radio-1"
+            class="sprk-b-Label sprk-b-Label--inline sprk-b-Radio__label"
+          >
+            Radio Item 1
+          </label>
+        </div>
+
+        <div
+          class="sprk-b-SelectionContainer sprk-b-Radio sprk-b-Radio--huge"
+          data-sprk-input="radio"
+        >
+          <input
+            class="sprk-b-Radio__input"
+            id="radio-2"
+            data-id="radio-2"
+            type="radio"
+            name="radio"
+            aria-describedby="radio--error-container"
+          >
+
+          <label
+            for="radio-2"
+            class="sprk-b-Label sprk-b-Label--inline sprk-b-Radio__label"
+          >
+            Radio Item 2
+          </label>
+        </div>
+
+        <div
+          class="sprk-b-SelectionContainer sprk-b-Radio sprk-b-Radio--huge"
+          data-sprk-input="radio"
+        >
+          <input
+            class="sprk-b-Radio__input"
+            id="radio-3"
+            data-id="radio-3"
+            type="radio"
+            name="radio"
+            aria-describedby="radio--error-container"
+          >
+          <label
+            for="radio-3"
+            class="sprk-b-Label sprk-b-Label--inline sprk-b-Radio__label"
+          >
+            Radio Item 3
+          </label>
+        </div>
+      </fieldset>
+
+      <div
+        class="sprk-b-ErrorContainer"
+        id="radio--error-container"
+      >
+        <svg
+          class="sprk-c-Icon sprk-c-Icon--m sprk-b-ErrorIcon"
+          viewBox="0 0 64 64"
+        >
+          <use xlink:href="#exclamation-filled-small" />
+        </svg>
+        <div class="sprk-b-ErrorText">
+          There is an error on this field.
+        </div>
+      </div>
+    </div>
+  `
+};
+
+invalidRadioButton.story = {
+  name: 'Invalid',
+  parameters: {
+    jest: ['radioInput'],
+  }
+};
+
+export const disabledRadioButton = () => {
+  useEffect(() => {
+    radioInput();
+  }, []);
+
+  return`
+    <div class="sprk-b-InputContainer">
+      <fieldset class="sprk-b-Fieldset">
+        <legend class="sprk-b-Legend">
+          <label class="sprk-b-Label sprk-b-Label--disabled">
+            Radio Group Label
+          </label>
+        </legend>
+
+        <div
+          class="sprk-b-SelectionContainer sprk-b-Radio sprk-b-Radio--huge"
+          data-sprk-input="radio"
+        >
+          <input
+            class="sprk-b-Radio__input"
+            id="radio-1"
+            data-id="radio-1"
+            type="radio"
+            name="radio"
+            aria-describedby="radio--error-container"
+            disabled
+          >
+
+          <label
+            for="radio-1"
+            class="sprk-b-Label sprk-b-Label--disabled sprk-b-Label--inline sprk-b-Radio__label"
+          >
+            Radio Item 1
+          </label>
+        </div>
+
+        <div
+          class="sprk-b-SelectionContainer sprk-b-Radio sprk-b-Radio--huge"
+          data-sprk-input="radio"
+        >
+          <input
+            class="sprk-b-Radio__input"
+            id="radio-2"
+            data-id="radio-2"
+            type="radio"
+            name="radio"
+            aria-describedby="radio--error-container"
+            disabled
+          >
+
+          <label
+            for="radio-2"
+            class="sprk-b-Label sprk-b-Label--disabled sprk-b-Label--inline sprk-b-Radio__label"
+          >
+            Radio Item 2
+          </label>
+        </div>
+
+        <div
+          class="sprk-b-SelectionContainer sprk-b-Radio sprk-b-Radio--huge"
+          data-sprk-input="radio"
+        >
+          <input
+            class="sprk-b-Radio__input"
+            id="radio-3"
+            data-id="radio-3"
+            type="radio"
+            name="radio"
+            aria-describedby="radio--error-container"
+            disabled
+          >
+          <label
+            for="radio-3"
+            class="sprk-b-Label sprk-b-Label--disabled sprk-b-Label--inline sprk-b-Radio__label"
+          >
+            Radio Item 3
+          </label>
+        </div>
+      </fieldset>
+
+      <div
+        class="sprk-b-ErrorContainer"
+        id="radio--error-container"
+      ></div>
+    </div>
+  `
+};
+
+disabledRadioButton.story = {
+  name: 'Disabled',
+  parameters: {
+    jest: ['radioInput'],
+  }
+};
+
+export const twoRadioButton = () => {
+  useEffect(() => {
+    radioInput();
+  }, []);
+
+  return`
+    <div class="sprk-b-InputContainer">
+      <fieldset class="sprk-b-Fieldset">
+        <legend class="sprk-b-Legend">
+          <label class="sprk-b-Label">
+            Radio Group Label
+          </label>
+        </legend>
+        <div
+          class="
+            sprk-o-Stack
+            sprk-o-Stack--split@s
+          "
+        >
+          <div class="sprk-o-Stack__item sprk-o-Stack__item--half@s">
+            <div
+              class="
+                sprk-b-SelectionContainer
+                sprk-b-Radio
+                sprk-b-Radio--huge
+              "
+              data-sprk-input="radio"
+            >
+              <input
+                class="sprk-b-Radio__input"
+                id="radio-1"
+                data-id="radio-1"
+                type="radio"
+                name="radio"
+                aria-describedby="radio--error-container"
+              >
+
+              <label
+                for="radio-1"
+                class="sprk-b-Label sprk-b-Label--inline sprk-b-Radio__label"
+              >
+                Radio Item 1
+              </label>
+            </div>
+          </div>
+          <div class="sprk-o-Stack__item sprk-o-Stack__item--half@s">
+            <div
+              class="
+                sprk-b-SelectionContainer
+                sprk-b-Radio
+                sprk-b-Radio--huge
+              "
+              data-sprk-input="radio"
+            >
+              <input
+                class="sprk-b-Radio__input"
+                id="radio-2"
+                data-id="radio-2"
+                type="radio"
+                name="radio"
+                aria-describedby="radio--error-container"
+              >
+
+              <label
+                for="radio-2"
+                class="sprk-b-Label sprk-b-Label--inline sprk-b-Radio__label"
+              >
+                Radio Item 2
+              </label>
+            </div>
+          </div>
+        </div>
+      </fieldset>
+
+      <div
+        class="sprk-b-ErrorContainer"
+        id="radio--error-container"
+      ></div>
+    </div>
+  `
+};
+
+twoRadioButton.story = {
+  name: 'Radio Layout - Two',
+  parameters: {
+    jest: ['radioInput'],
+  }
+};
