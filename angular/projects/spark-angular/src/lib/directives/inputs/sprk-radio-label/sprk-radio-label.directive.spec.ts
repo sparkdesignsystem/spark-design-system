0cec403e52934294396660d2543d2511cd73fc12
Febbraro <amberfebbraro@quickenloans.com>
2020-06-18 12:22:41 -0400

    add tests for radio label

--- angular/projects/spark-angular/src/lib/directives/inputs/sprk-radio-label/sprk-radio-label.directive.spec.ts
+++ angular/projects/spark-angular/src/lib/directives/inputs/sprk-radio-label/sprk-radio-label.directive.spec.ts
@@ -6,7 +6,8 @@ import { SprkRadioLabelDirective } from './sprk-radio-label.directive';
   selector: 'sprk-test',
   template: `
     <label sprkRadioLabel>Label</label>
-  `
+    <label sprkRadioLabel variant="huge">Label</label>
+  `,
 })
 class TestComponent {}
 
@@ -14,19 +15,27 @@ describe('Spark Helper Text Directive', () => {
   let component: TestComponent;
   let fixture: ComponentFixture<TestComponent>;
   let labelElement: HTMLElement;
+  let itemElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SprkRadioLabelDirective, TestComponent]
+      declarations: [SprkRadioLabelDirective, TestComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
     labelElement = fixture.nativeElement.querySelector('label');
+    itemElement = fixture.nativeElement.querySelectorAll('label')[1];
   }));
 
   it('should create itself', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add the correct classes to the applied element when variant is huge', () => {
+    expect(itemElement.classList.contains('sprk-b-Radio__label--huge')).toBe(
+      true,
+    );
   });
 });
