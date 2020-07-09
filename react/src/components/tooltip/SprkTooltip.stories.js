9f7f4301f2141fcd610b4300e7746ddd37acf44b
Bob Wise <bobwise@quickenloans.com>
2020-07-09 13:57:54 -0400

    adding more stories to test with

--- react/src/components/tooltip/SprkTooltip.stories.js
+++ react/src/components/tooltip/SprkTooltip.stories.js
@@ -5,7 +5,9 @@ import { markdownDocumentationLinkBuilder } from '../../../../storybook-utilitie
 export default {
   title: 'Components/Tooltip',
   component: SprkTooltip,
-  decorators: [(story) => <div className="sprk-o-Box">{story()}</div>],
+  decorators: [
+    story => <div className="sprk-o-Box">{story()}</div>
+  ],
   parameters: {
     jest: ['SprkTooltip'],
     info: `${markdownDocumentationLinkBuilder('tooltip')}`,
@@ -13,12 +15,46 @@ export default {
 };
 
 export const defaultStory = () => (
-  <SprkTooltip iconAdditionalClasses="sprk-c-Icon--filled">
-    Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco
-    deserunt aute id dee doo da fauxet la gigman roo.
+  <SprkTooltip iconAdditionalClasses='sprk-c-Icon--filled'>
+    Sit nulla est ex deserunt exercitation anim occaecat. Nostrud
+    ullamco deserunt aute id dee doo da fauxet la gigman roo.
   </SprkTooltip>
 );
 
 defaultStory.story = {
   name: 'Default',
+};
+
+export const cornersStory = () => (
+  <>
+    <span style={{ position: 'absolute', left: '5%', top: '5%' }}>
+      <SprkTooltip isDefaultOpen={true} iconAdditionalClasses='sprk-c-Icon--filled'>^------</SprkTooltip>
+    </span>
+    <span style={{ position: 'absolute', left: '55%', top: '5%' }}>
+      <SprkTooltip isDefaultOpen={true} iconAdditionalClasses='sprk-c-Icon--filled'>---------^</SprkTooltip>
+    </span>
+    <span style={{ position: 'absolute', left: '5%', top: '55%' }}>
+      <SprkTooltip isDefaultOpen={true} iconAdditionalClasses='sprk-c-Icon--filled'>v------</SprkTooltip>
+    </span>
+    <span style={{ position: 'absolute', left: '55%', top: '55%' }}>
+      <SprkTooltip isDefaultOpen={true} iconAdditionalClasses='sprk-c-Icon--filled'>-------v</SprkTooltip>
+    </span>
+  </>
+);
+
+cornersStory.story = {
+  name: 'Corners',
+};
+
+export const textStory = () => (
+  <>
+    lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum
+    <SprkTooltip iconAdditionalClasses='sprk-c-Icon--filled'>Sit nulla est ex deserunt exercitation anim occaecat. Nostrud
+    ullamco deserunt aute id dee doo da fauxet la gigman roo.</SprkTooltip>
+    lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum
+  </>
+);
+
+textStory.story = {
+  name: 'Inline',
 };
