b434e6ddb7dbea0bff6013f4d82dce90fc64ddc7
Bob Wise <bobwise@quickenloans.com>
2020-07-09 14:09:16 -0400

    cleanup

--- react/src/components/tooltip/SprkTooltip.stories.js
+++ react/src/components/tooltip/SprkTooltip.stories.js
@@ -5,7 +5,7 @@ import { markdownDocumentationLinkBuilder } from '../../../../storybook-utilitie
 export default {
   title: 'Components/Tooltip',
   component: SprkTooltip,
-  decorators: [story => <div className="sprk-o-Box">{story()}</div>],
+  decorators: [(story) => <div className="sprk-o-Box">{story()}</div>],
   parameters: {
     jest: ['SprkTooltip'],
     info: `${markdownDocumentationLinkBuilder('tooltip')}`,
@@ -13,7 +13,7 @@ export default {
 };
 
 export const defaultStory = () => (
-  <SprkTooltip iconAdditionalClasses='sprk-c-Icon--filled'>
+  <SprkTooltip iconAdditionalClasses="sprk-c-Icon--filled">
     Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco
     deserunt aute id dee doo da fauxet la gigman roo.
   </SprkTooltip>
