b3e511d541d43740ec3fd88625dab9676efef945
RVMendoza <RVxMendoza@gmail.com>
2020-07-09 16:00:32 -0400

    Recreate bug successfully

--- angular/projects/spark-angular/src/lib/components/sprk-tab-duplicate/sprk-tab-duplicate.component.ts
+++ angular/projects/spark-angular/src/lib/components/sprk-tab-duplicate/sprk-tab-duplicate.component.ts
@@ -6,58 +6,27 @@ import { Component, ElementRef, ChangeDetectorRef } from '@angular/core';
     <div>
       <button (click)="changeMe()">Change Color</button>
       <sprk-tabbed-navigation idString="tabs-1" [additionalClasses]="newColor">
-        <button
+        <button *ngFor="let item of list; let i = index"
           sprkTabbedNavigationTab
-          analyticsString="Tab: 1"
-          data-id="tab-1"
+          analyticsString="Tab: {{i}}"
+          data-id="tab-{{i}}"
         >
-          Tab 1
+          {{item}}
         </button>
-        <button
-          sprkTabbedNavigationTab
-          data-id="tab-2"
-          [defaultActive]=true
-        >
-          Tab 2
-        </button>
-        <button
-          sprkTabbedNavigationTab
-          data-id="tab-3"
-        >
-          Tab 3
-        </button>
-        <div sprkTabbedNavigationPanel>
-          <p>Tab 1 Content Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis rhoncus ipsum. Nulla
-          euismod nisi est, vel consequat ante consectetur in. Ut interdum dictum est at ornare. Nam nec dapibus nibh.
-          Integer venenatis ex eu mi euismod, non ultricies lacus venenatis.</p>
-        </div>
-        <div [defaultActive]=true sprkTabbedNavigationPanel>
-          <p>Tab 2 Content Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis rhoncus ipsum. Nulla
-          euismod nisi est, vel consequat ante consectetur in. Ut interdum dictum est at ornare. Nam nec dapibus nibh.
-          Integer venenatis ex eu mi euismod, non ultricies lacus venenatis.</p>
-        </div>
-        <div sprkTabbedNavigationPanel>
-          <p>Tab 3 Content Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis rhoncus ipsum. Nulla
-          euismod nisi est, vel consequat ante consectetur in. Ut interdum dictum est at ornare. Nam nec dapibus nibh.
-          Integer venenatis ex eu mi euismod, non ultricies lacus venenatis.</p>
+        <div
+        sprkTabbedNavigationPanel
+        *ngFor="let item of list; let i = index">
+          <p>{{item}}</p>
         </div>
       </sprk-tabbed-navigation>
     </div>
   `
 })
 export class SprkTabDuplicateComponent {
-  // constructor(public ref: ElementRef, public appRef: ChangeDetectorRef) {
-  //   setInterval(() => {
-  //     console.log('cats set interval')
-  //     this.appRef.markForCheck();
-  //   }, 1000);
-  // }
-
+  public list = ["Tab 1", "Tab 2"];
+  public newList = ["Tab 3", "Tab 4"]
   newColor= "sprk-u-BackgroundColor--blue";
   changeMe(): void {
-    if (this.newColor === "sprk-u-BackgroundColor--red") {
-      this.newColor= "sprk-u-BackgroundColor--blue";
-    } else { this.newColor = "sprk-u-BackgroundColor--red"}
+    this.list = this.newList;
   }
-
 }
