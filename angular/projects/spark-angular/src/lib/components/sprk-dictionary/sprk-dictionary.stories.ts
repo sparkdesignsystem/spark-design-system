a71b356c19be222b7cc2c8364582e4c581912c69
Bob Wise <bobwise@quickenloans.com>
2020-07-30 13:47:55 -0400

    updated stories

--- angular/projects/spark-angular/src/lib/components/sprk-dictionary/sprk-dictionary.stories.ts
+++ angular/projects/spark-angular/src/lib/components/sprk-dictionary/sprk-dictionary.stories.ts
@@ -27,13 +27,13 @@ export const defaultStory = () => ({
     <sprk-dictionary
      idString="dictionary-default"
      [keyValuePairs]="{
-       'Email Address':'sparkdesignsystem@quickenloans.com',
-       'Mailing Address': '123 Main Street, Detroit, MI, 48216',
-       'Home Phone': '(123) 456-7890',
-       'Cell Phone': '(098) 765-4321',
-       'Work Phone': '(555) 555-5555',
-       'Work Extension': '55555'
-     }"
+      'Email Address':'sparkdesignsystem@quickenloans.com',
+      'Mailing Address': '123 Main Street, Detroit, MI, 48216',
+      'Home Phone': '(123) 456-7890',
+      'Cell Phone': '(098) 765-4321',
+      'Work Phone': '(555) 555-5555',
+      'Work Extension': '55555'
+    }"
     ></sprk-dictionary>
   `,
 });
