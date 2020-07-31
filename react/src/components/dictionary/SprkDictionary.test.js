b2646180049c7f2085e95251fcd2dcc8bded93dc
Bob Wise <bobwise@quickenloans.com>
2020-07-31 10:59:11 -0400

    tests

--- react/src/components/dictionary/SprkDictionary.test.js
+++ react/src/components/dictionary/SprkDictionary.test.js
@@ -48,4 +48,81 @@ describe('SprkDictionary:', () => {
       ).toBe(true);
     },
   );
+
+  it('should add data-id', () => {
+    const testKeyValuePairs = {
+      'Email Address': 'sparkdesignsystem@quickenloans.com',
+      'Mailing Address': '123 Main Street, Detroit, MI, 48216',
+      'Home Phone': '(123) 456-7890',
+      'Cell Phone': '(098) 765-4321',
+      'Work Phone': '(555) 555-5555',
+      'Work Extension': '55555',
+    };
+    const wrapper = shallow(
+      <SprkDictionary keyValuePairs={testKeyValuePairs} idString="asdf" />,
+    );
+    expect(wrapper.find('div.sprk-c-Dictionary[data-id="asdf"]').length).toBe(
+      1,
+    );
+  });
+
+  it('should add additionalClasses', () => {
+    const testKeyValuePairs = {
+      'Email Address': 'sparkdesignsystem@quickenloans.com',
+      'Mailing Address': '123 Main Street, Detroit, MI, 48216',
+      'Home Phone': '(123) 456-7890',
+      'Cell Phone': '(098) 765-4321',
+      'Work Phone': '(555) 555-5555',
+      'Work Extension': '55555',
+    };
+    const wrapper = shallow(
+      <SprkDictionary
+        keyValuePairs={testKeyValuePairs}
+        additionalClasses="asdf"
+      />,
+    );
+    expect(wrapper.find('div.sprk-c-Dictionary').hasClass('asdf')).toBe(true);
+  });
+
+  it('should add additionalKeysClasses', () => {
+    const testKeyValuePairs = {
+      'Email Address': 'sparkdesignsystem@quickenloans.com',
+      'Mailing Address': '123 Main Street, Detroit, MI, 48216',
+      'Home Phone': '(123) 456-7890',
+      'Cell Phone': '(098) 765-4321',
+      'Work Phone': '(555) 555-5555',
+      'Work Extension': '55555',
+    };
+    const wrapper = shallow(
+      <SprkDictionary
+        keyValuePairs={testKeyValuePairs}
+        additionalKeysClasses="asdf"
+      />,
+    );
+
+    wrapper.find('.sprk-c-Dictionary__key').forEach((key) => {
+      expect(key.hasClass('asdf')).toBe(true);
+    });
+  });
+
+  it('should add additionalValuesClasses', () => {
+    const testKeyValuePairs = {
+      'Email Address': 'sparkdesignsystem@quickenloans.com',
+      'Mailing Address': '123 Main Street, Detroit, MI, 48216',
+      'Home Phone': '(123) 456-7890',
+      'Cell Phone': '(098) 765-4321',
+      'Work Phone': '(555) 555-5555',
+      'Work Extension': '55555',
+    };
+    const wrapper = shallow(
+      <SprkDictionary
+        keyValuePairs={testKeyValuePairs}
+        additionalValuesClasses="asdf"
+      />,
+    );
+
+    wrapper.find('sprk-c-Dictionary__value').forEach((value) => {
+      expect(value.hasClass('asdf')).toBe(true);
+    });
+  });
 });
