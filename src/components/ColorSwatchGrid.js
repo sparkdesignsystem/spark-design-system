f4d9e72c9ae3982928d733c0b8422577307a6016
RVMendoza <RVxMendoza@gmail.com>
2020-08-17 16:46:00 -0400

    Attempt graphql in mdx

--- src/components/ColorSwatchGrid.js
+++ src/components/ColorSwatchGrid.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import withColorData from '../util/withColorData';
 
 const SwatchGrid = ({ children }) => {
   return <div>{children}</div>;
 };
 
-const ColorSwatchGrid = () => {
-  return withColorData(SwatchGrid);
-};
-
 SwatchGrid.propTypes = {
   children: PropTypes.node,
 };
-export default ColorSwatchGrid;
+
+export default SwatchGrid;
--- src/util/withColorData.js
+++ src/util/withColorData.js
@@ -1,37 +1,42 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 
-const withColorData = (IncomingComponent) => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query SassColors {
-          allSassVarsJson {
-            edges {
-              node {
-                description
-                context {
-                  name
-                  value
+const UpdatedComponent = (IncomingComponent) => {
+  class NewComponent extends React.Component {
+    render() {
+      return (
+        <StaticQuery
+          query={graphql`
+            query SassColors {
+              allSassVarsJson {
+                edges {
+                  node {
+                    description
+                    context {
+                      name
+                      value
+                    }
+                    file {
+                      name
+                    }
+                    group
+                    type
+                  }
                 }
-                file {
-                  name
-                }
-                group
-                type
               }
             }
-          }
-        }
-      `}
-      render={(data) => {
-        const sassColorData = data.allSassVarsJson.edges.filter((item) => {
-          return item.node.file.name === '_colors.scss';
-        });
-        return <IncomingComponent colorData={sassColorData} />;
-      }}
-    />
-  );
+          `}
+          render={(data) => {
+            const sassColorData = data.allSassVarsJson.edges.filter((item) => {
+              return item.node.file.name === '_colors.scss';
+            });
+            return <IncomingComponent colorData={sassColorData} />;
+          }}
+        />
+      );
+    }
+  }
+  return NewComponent;
 };
 
-export default withColorData;
+export default UpdatedComponent;
