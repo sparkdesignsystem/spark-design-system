ef489188db03dfb248148201b94649a3cef7bb80
Bob Wise <bobwise@quickenloans.com>
2020-08-19 14:16:28 -0400

    test stories

--- angular/projects/spark-angular/src/lib/components/sprk-tooltip/sprk-tooltip.stories.ts
+++ angular/projects/spark-angular/src/lib/components/sprk-tooltip/sprk-tooltip.stories.ts
@@ -31,16 +31,79 @@ export const defaultStory = () => ({
   moduleMetadata: modules,
   template: `
     <sprk-tooltip
+      triggerIconType="question-filled"
       iconAdditionalClasses="sprk-c-Icon--filled"
+      idString='idstring-1'
+      analyticsString='analyticsString-1'
+      additionalClasses='spark-example'
     >
-      Use Tooltips to provide info that’s not vital to completing the task.
-      Keep the text short and stick to what’s helpful and relevant.
+      Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt aute id dee doo da fauxet la gigman roo.
     </sprk-tooltip>
   `,
 });
 
 defaultStory.story = {
   name: 'Default',
+  parameters: {
+    jest: [
+      'sprk-tooltip.component',
+    ],
+  },
+};
+
+export const cornersStory = () => ({
+  moduleMetadata: modules,
+  template: `
+    <span class="sprk-b-TypeBodyTwo" style="position: absolute; left: 5%; top: 5%;">
+      <sprk-tooltip isToggled="true">
+        top left corner
+      </sprk-tooltip>
+    </span>
+    <span class="sprk-b-TypeBodyTwo" style="position: absolute; left: 55%; top: 5%;">
+      <sprk-tooltip isToggled="true">
+        top right corner
+      </sprk-tooltip>
+    </span>
+    <span class="sprk-b-TypeBodyTwo" style="position: absolute; left: 5%; top: 55%;">
+      <sprk-tooltip isToggled="true">
+        bottom left corner
+      </sprk-tooltip>
+    </span>
+    <span class="sprk-b-TypeBodyTwo" style="position: absolute; left: 55%; top: 55%;">
+      <sprk-tooltip isToggled="true">
+        bottom right corner
+      </sprk-tooltip>
+    </span>
+  `,
+});
+
+cornersStory.story = {
+  name: 'Corners',
+  parameters: {
+    jest: [
+      'sprk-tooltip.component',
+    ],
+  },
+};
+
+export const withText = () => ({
+  moduleMetadata: modules,
+  template: `
+  Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+    <sprk-tooltip
+      triggerIconType="question-filled"
+      iconAdditionalClasses="sprk-c-Icon--filled"
+      idString='idstring-1'
+      analyticsString='analyticsString-1'
+      additionalClasses='spark-example'
+    >
+      Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt aute id dee doo da fauxet la gigman roo.
+    </sprk-tooltip>
+  `,
+});
+
+withText.story = {
+  name: 'Inline',
   parameters: {
     jest: [
       'sprk-tooltip.component',
