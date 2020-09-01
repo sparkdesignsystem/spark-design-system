ef6dd39b41772f2c4caf2bf587f372eb9d03734f
Bob Wise <bobwise@quickenloans.com>
2020-09-01 15:02:19 -0400

    color names and outlines

--- src/components/ColorSwatch.js
+++ src/components/ColorSwatch.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import theme from 'prism-react-renderer/themes/github';
+import theme from 'prism-react-renderer/themes/github';
 import { SprkStack, SprkStackItem } from '@sparkdesignsystem/spark-react';
-// import InlineCode from './markdown-render/inlineCode';
+import InlineCode from './markdown-render/inlineCode';
 import useColorData from '../hooks/use-color-data';
 
-// const nameFormatter = (name) => {
-//   return name
-//     .replace(/-/g, ' ')
-//     .replace('sprk', '')
-//     .split(' ')
-//     .map((word) => word.substring(0, 1).toUpperCase() + word.substring(1))
-//     .join(' ');
-// };
+const nameFormatter = (name) => {
+  return name
+    .replace(/-/g, ' ')
+    .replace('sprk', '')
+    .split(' ')
+    .map((word) => word.substring(0, 1).toUpperCase() + word.substring(1))
+    .join(' ');
+};
 
 const ColorSwatch = (props) => {
   const colorData = useColorData();
 
-  const { variableName, isCompact, hasOutline } = props;
+  const { variableName, colorName, isCompact, hasOutline } = props;
 
   // return a single object containing data for one color
   const colorNode = colorData.filter((item) => {
     return item.node.context.name === variableName;
   })[0];
 
+  const calculatedName = nameFormatter(colorNode.node.context.name);
   const rgbValue = colorNode.node.context.value;
-  // const { name } = nameFormatter(colorNode.node.context.name);
   const { description } = colorNode.node;
   const hexValue = colorNode.node.type;
   const sassVar = `$${colorNode.node.context.name}`;
@@ -56,11 +56,12 @@ const ColorSwatch = (props) => {
               width: '85px',
               marginRight: '10px',
               backgroundColor: hexValue,
+              border: hasOutline ? '1px solid black' : 'none',
             }}
           />
           {!isCompact && (
             <SprkStackItem>
-              <div>NAME</div>
+              <div>{colorName || calculatedName}</div>
               <div
                 style={
                   {
@@ -78,15 +79,33 @@ const ColorSwatch = (props) => {
         <>
           <SprkStackItem additionalClasses="sprk-o-Stack__item--fifth@s">
             <div className="colorSwatchLabel">Hex</div>
-            {hexValue}
+            <InlineCode
+              className="css"
+              theme={theme}
+              additionalPreClasses="sprk-u-pas sprk-u-man"
+            >
+              {hexValue}
+            </InlineCode>
           </SprkStackItem>
           <SprkStackItem additionalClasses="sprk-o-Stack__item--fifth@s">
             <div className="colorSwatchLabel">RGB</div>
-            {rgbValue}
+            <InlineCode
+              className="css"
+              theme={theme}
+              additionalPreClasses="sprk-u-pas sprk-u-man"
+            >
+              {rgbValue}
+            </InlineCode>
           </SprkStackItem>
           <SprkStackItem additionalClasses="sprk-o-Stack__item--fifth@s">
             <div className="colorSwatchLabel">Sass</div>
-            {sassVar}
+            <InlineCode
+              className="css"
+              theme={theme}
+              additionalPreClasses="sprk-u-pas sprk-u-man"
+            >
+              {sassVar}
+            </InlineCode>
           </SprkStackItem>
         </>
       )}
@@ -98,6 +117,7 @@ ColorSwatch.propTypes = {
   variableName: PropTypes.string,
   isCompact: PropTypes.bool,
   hasOutline: PropTypes.bool,
+  colorName: PropTypes.string,
 };
 
 export default ColorSwatch;
