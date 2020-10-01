2bfa97bf6b6057f0fa779658fa6c2d54e854477f
Ahmed Mohamadeen <2m.fcih@fci.helwan.edu.eg>
2020-10-01 18:06:30 +0200

    fix linting errors

--- src/components/layouts/PrinciplesLayout.js
+++ src/components/layouts/PrinciplesLayout.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Layout from './Layout';
 
 function PrinciplesLayout({ children, location }) {
   return (
-    <Layout initialContext={'principles'} location={location}>{children}</Layout>
+    <Layout initialContext="principles" location={location}>
+      {children}
+    </Layout>
   );
 }
 
