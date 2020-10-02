2595c82563dda20dfb45dd22337dd3cfa8678615
motionsuggests <181652+motionsuggests@users.noreply.github.com>
2020-10-02 11:01:47 -0400

    add PropTypes

--- src/components/ContentWrapper.js
+++ src/components/ContentWrapper.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function ContentWrapper({children}) {
   return (
     <div>{children}</div>
   );
 }
+
+ContentWrapper.propTypes = {
+  /**
+  * Content to render inside of the component.
+  */
+  children: PropTypes.node
+};
 
 export default ContentWrapper;
