4a39754e4bdc3433921bdc6a8499ef1fac3afd58
Sravan7 <esd14i017@iiitdm.ac.in>
2020-10-02 11:24:29 +0530

    makes  "Want to contribute" to black text #3492

--- src/pages/index.js
+++ src/pages/index.js
@@ -222,7 +222,7 @@ const IndexPage = () => (
       <SprkStackItem>
         <SprkStack itemSpacing={spacingBtwHeadingAndContent}>
           <SprkStackItem>
-            <h2 className="sprk-b-TypeDisplayTwo sprk-u-Color--red">
+            <h2 className="sprk-b-TypeDisplayTwo sprk-u-Color--black">
               Want To Contribute?
             </h2>
           </SprkStackItem>
