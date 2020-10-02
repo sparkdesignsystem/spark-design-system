2d5c0f2a8986af3eb8fa35c9a8f0b4e6cea65076
Eduardo <lalo_pichu22@hotmail.com>
2020-10-02 14:24:54 -0500

    Updated isValidMonetary to be an arrow function

--- html/utilities/validation/isValidMonetary.js
+++ html/utilities/validation/isValidMonetary.js
@@ -1,6 +1,6 @@
 const isValidMonetary = (value) => {
   const expression = /(^\$?(\d+|\d{1,3}(,\d{3})*)(\.\d+)?$)|^$/;
   return expression.test(value);
-}
+};
 
 export { isValidMonetary as default };
