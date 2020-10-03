7cbe2c7ddc66c4c8fb12331d9bc33b6c3bb90f1c
Vivek T A <vivekta20@gmail.com>
2020-10-03 12:21:03 +0530

    Convert ComponentPreview.js into a functional component

--- src/components/ComponentPreview.js
+++ src/components/ComponentPreview.js
@@ -3,54 +3,48 @@ import PropTypes from 'prop-types';
 import IframeResizer from 'iframe-resizer-react';
 import DevDocLinks from './DevDocLinks';
 
-class ComponentPreview extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function ComponentPreview(props) {
+  const {
+    componentName,
+    hasReact,
+    hasAngular,
+    hasHTML,
+    componentType,
+    minHeight,
+    maxHeight,
+    maxWidth,
+    allowScrolling,
+  } = props;
+  const iframeURL = `https://html.sparkdesignsystem.com/iframe.html?id=${componentType}-${componentName}`;
 
-  render() {
-    const {
-      componentName,
-      hasReact,
-      hasAngular,
-      hasHTML,
-      componentType,
-      minHeight,
-      maxHeight,
-      maxWidth,
-      allowScrolling,
-    } = this.props;
-    const iframeURL = `https://html.sparkdesignsystem.com/iframe.html?id=${componentType}-${componentName}`;
-
-    return (
-      <div className="sprk-u-mbm sprk-u-ptl">
-        <div className="sprk-u-mbl">
-          <IframeResizer
-            style={{
-              minHeight,
-              maxHeight,
-              maxWidth,
-            }}
-            scrolling={allowScrolling}
-            title="Component Preview"
-            className="docs-c-ComponentPreview sprk-o-Box"
-            src={iframeURL}
-            loading="lazy"
-          />
-        </div>
-
-        {(hasHTML || hasAngular || hasReact) && (
-          <DevDocLinks
-            hasAngular={hasAngular}
-            hasHTML={hasHTML}
-            hasReact={hasReact}
-            componentType={componentType}
-            componentName={componentName}
-          />
-        )}
+  return (
+    <div className="sprk-u-mbm sprk-u-ptl">
+      <div className="sprk-u-mbl">
+        <IframeResizer
+          style={{
+            minHeight,
+            maxHeight,
+            maxWidth,
+          }}
+          scrolling={allowScrolling}
+          title="Component Preview"
+          className="docs-c-ComponentPreview sprk-o-Box"
+          src={iframeURL}
+          loading="lazy"
+        />
       </div>
-    );
-  }
+
+      {(hasHTML || hasAngular || hasReact) && (
+        <DevDocLinks
+          hasAngular={hasAngular}
+          hasHTML={hasHTML}
+          hasReact={hasReact}
+          componentType={componentType}
+          componentName={componentName}
+        />
+      )}
+    </div>
+  );
 }
 
 ComponentPreview.propTypes = {
