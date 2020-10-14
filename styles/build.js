73cc501345a02a1f7b95392bc6e180a94f70e049
Febbraro <amberfebbraro@quickenloans.com>
2020-10-14 08:46:31 -0600

    condense logic for build js

--- styles/build.js
+++ styles/build.js
@@ -4,11 +4,9 @@ StyleDictionary.registerFormat({
   name: 'themable-scss/web',
   formatter: (tokens) => {
     const formattedTokens = tokens.allProperties.map((token) => {
-      if (token.themable === true) {
-        // eslint-disable-next-line max-len
-        return `/// ${token.comment} \n$${token.name}: ${token.value} !default;`;
-      }
-      return `/// ${token.comment} \n$${token.name}: ${token.value};`;
+      return `/// ${token.comment} \n$${token.name}: ${token.value}${
+        token.themable ? ' !default' : ''
+      };`;
     });
     return formattedTokens.join('\n');
   },
