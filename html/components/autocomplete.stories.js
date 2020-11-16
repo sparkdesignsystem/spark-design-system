def774f40ed14f404f5b4b2500f6eab60708ae9b
Bob Wise <bobwise@quickenloans.com>
2020-11-16 16:40:51 -0500

    strip it back to just presentational

--- html/components/autocomplete.stories.js
+++ html/components/autocomplete.stories.js
@@ -21,7 +21,7 @@ export const defaultStory = () => {
 
   return `
     <div data-sprk-autocomplete="container">
-      <div aria-live="polite" data-sprk-autocomplete="announcement"></div>
+      <div aria-live="polite"></div>
 
       <div class="sprk-b-InputContainer">
         <label
@@ -40,7 +40,6 @@ export const defaultStory = () => {
           </svg>
 
           <input
-            data-sprk-autocomplete="input"
             class="
               sprk-b-TextInput
               sprk-b-TextInput--has-svg-icon
@@ -52,107 +51,14 @@ export const defaultStory = () => {
           >
         </div>
 
-
-
-
         <ul
-          data-sprk-autocomplete="filtered"
-          class="sprk-c-Autocomplete__results sprk-u-Display--none"
+          class="sprk-c-Autocomplete__results"
         >
-          <span class="sprk-c-Autocomplete__results_header">
-            Filtered Results
-          </span>
-          <li
-            id="filtered_1"
-            data-sprk-autocomplete-value="AK"
-            data-sprk-autocomplete-text="Alaska"
-          >
-            Alaska
-          </li>
-          <li
-            id="filtered_2"
-            data-sprk-autocomplete-value="AL"
-            data-sprk-autocomplete-text="Alabama"
-          >
-            Alabama
-          </li>
-          <li
-            id="filtered_3"
-            data-sprk-autocomplete-value="AR"
-            data-sprk-autocomplete-text="Arkansas"
-          >
-            Arkansas
-          </li>
-          <li
-            id="filtered_4"
-            data-sprk-autocomplete-value="MI"
-            data-sprk-autocomplete-text="Michigan"
-          >
-            Michigan
-          </li>
-        </ul>
-
-        <ul
-          data-sprk-autocomplete="recents"
-          class="sprk-c-Autocomplete__results sprk-u-Display--none"
-        >
-          <span class="sprk-c-Autocomplete__results_header"
-            data-todo="TODO spans are not valid children of ul"
-          >
-            Recent Results
-          </span>
-          <li
-            id="recents_1"
-            data-sprk-autocomplete-value="afebbraro"
-            data-sprk-autocomplete-text="Amber Febrraro"
-          >
-            <div class="sprk-u-FontWeight--bold">Amber</div>
-            <div class="sprk-u-FontStyle--italic">Febbraro</div>
-          </li>
-          </span>
-          <li
-            id="recents_1"
-            data-sprk-autocomplete-value="rcopeland"
-            data-sprk-autocomplete-text="Rob Copeland"
-          >
-            <div class="sprk-u-FontWeight--bold">Rob</div>
-            <div class="sprk-u-FontStyle--italic">Copeland</div>
-          </li>
-          </span>
-          <li
-            id="recents_1"
-            data-sprk-autocomplete-value="rmendoza"
-            data-sprk-autocomplete-text="RV Mendoza"
-          >
-            <div class="sprk-u-FontWeight--bold">RV</div>
-            <div class="sprk-u-FontStyle--italic">Mendoza</div>
-          </li>
-          </span>
-          <li
-            id="recents_1"
-            data-sprk-autocomplete-value="kguernsey"
-            data-sprk-autocomplete-text="Katie Guernsey"
-          >
-            <div class="sprk-u-FontWeight--bold">Katie</div>
-            <div class="sprk-u-FontStyle--italic">Guernsey</div>
-          </li>
-          </span>
-          <li
-            id="recents_1"
-            data-sprk-autocomplete-value="driquelme"
-            data-sprk-autocomplete-text="Diana Riquelme"
-          >
-            <div class="sprk-u-FontWeight--bold">Diana</div>
-            <div class="sprk-u-FontStyle--italic">Riquelme</div>
-          </li>
-          </span>
-          <li
-            data-sprk-autocomplete-value="bwise"
-            data-sprk-autocomplete-text="Bob Wise"
-          >
-            <div class="sprk-u-FontWeight--bold">Bob</div>
-            <div class="sprk-u-FontStyle--italic">Wise</div>
-          </li>
+          <li>Apples</li>
+          <li>Oranges</li>
+          <li>Bananas</li>
+          <li>Strawberries</li>
+          <li>Lemons</li>
         </ul>
 
         <div
@@ -160,10 +66,6 @@ export const defaultStory = () => {
           id="input--error-container"
         ></div>
       </div>
-
-
-
-
     </div>
   `;
 };
