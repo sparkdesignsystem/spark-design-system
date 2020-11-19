9a1ae1959f185ab56998e6d795d2b53684e69337
RVMendoza <RVxMendoza@gmail.com>
2020-11-19 14:56:45 -0500

    change default to chevron-down

--- angular/projects/spark-angular/src/lib/components/sprk-masthead/sprk-masthead-accordion-item/sprk-masthead-accordion-item.component.ts
+++ angular/projects/spark-angular/src/lib/components/sprk-masthead/sprk-masthead-accordion-item/sprk-masthead-accordion-item.component.ts
@@ -94,13 +94,13 @@ export class SprkMastheadAccordionItemComponent implements OnInit {
    * for the clickable close icon.
    */
   @Input()
-  iconTypeClosed = 'chevron-down-circle';
+  iconTypeClosed = 'chevron-down';
   /**
    * The name of the icon to use
    * for the clickable open icon.
    */
   @Input()
-  iconTypeOpen = 'chevron-down-circle';
+  iconTypeOpen = 'chevron-down';
   /**
    * Expects a name of an icon to use
    * as a leading icon for the Masthead accordion item.
