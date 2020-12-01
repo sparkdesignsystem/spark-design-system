eb89104c46e88a58a00f668fce4719c2fe6b25e3
RVMendoza <RVxMendoza@gmail.com>
2020-12-01 14:31:32 -0500

    Add use effect

--- src/components/TokenTable.js
+++ src/components/TokenTable.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
-import { SprkTable } from '@sparkdesignsystem/spark-react';
+import { SprkTable, SprkTextInput } from '@sparkdesignsystem/spark-react';
 
 const TokenTable = () => {
   const tokenData = useStaticQuery(
@@ -26,54 +26,61 @@ const TokenTable = () => {
     description: item.node.description,
   }));
 
-  const [data, setData] = useState(sassVarData);
+  const [filteredData, setFilteredData] = useState(sassVarData);
+  const [query, setQuery] = useState('');
 
-  const handleInputChange = (event) => {
-    const query = event.target.value;
-    // need to search full data instead of existing data
-    const filteredData = data.filter((post) => {
-      const { name, value, description } = post;
-      return (
-        name.toLowerCase().includes(query.toLowerCase()) ||
-        value.toLowerCase().includes(query.toLowerCase()) ||
-        description.toLowerCase().includes(query.toLowerCase())
-      );
-    });
-    // This needs to reset somehow
-    setData(filteredData);
-  };
+  useEffect(() => {
+    if (query === '') {
+      setFilteredData(sassVarData);
+      return;
+    }
+    const getFilteredData = sassVarData.filter(
+      ({ name, value, description }) => {
+        const joinedValue = [name, value, description].join(' ').toLowerCase();
+        return joinedValue.includes(query.toLowerCase());
+      },
+    );
+    setFilteredData(getFilteredData);
+  }, [query]);
 
   return (
     <>
-      <input
-        type="text"
-        aria-label="Search"
-        placeholder="Type to filter posts..."
-        onChange={handleInputChange}
+      <SprkTextInput
+        label="Filter through tokens"
+        name="text-input-label"
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
       />
-      <SprkTable
-        additionalTableClasses="
-        docs-b-Table--left-align-last
-        sprk-b-Table--spacing-medium
-        sprk-b-Table--striped-even
-        sprk-u-TextAlign--right
-      "
-        columns={[
-          {
-            name: 'name',
-            header: 'Token Name',
-          },
-          {
-            name: 'value',
-            header: 'Default Value',
-          },
-          {
-            name: 'description',
-            header: 'Description',
-          },
-        ]}
-        rows={data}
-      />
+      {filteredData.length <= 0 ? (
+        // MAKE THIS LOOK BETTER
+        `No results for '${query}'. Try another keyword.`
+      ) : (
+        <SprkTable
+          additionalTableClasses="
+         docs-b-Table--left-align-last
+         sprk-b-Table--spacing-medium
+         sprk-b-Table--striped-even
+         sprk-u-TextAlign--right
+       "
+          columns={[
+            {
+              name: 'name',
+              header: 'Token Name',
+            },
+            {
+              name: 'value',
+              header: 'Default Value',
+            },
+            {
+              name: 'description',
+              header: 'Description',
+            },
+          ]}
+          rows={filteredData}
+        />
+      )}
     </>
   );
 };
