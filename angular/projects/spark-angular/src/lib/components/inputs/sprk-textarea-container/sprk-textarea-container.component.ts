a83a0d0e3055504635a179608e1ad51e5367b5cb
corcorankt1 <guern1kn@gmail.com>
2020-12-30 09:37:52 -0500

    add id/for functionality to textarea-container

--- angular/projects/spark-angular/src/lib/components/inputs/sprk-textarea-container/sprk-textarea-container.component.ts
+++ angular/projects/spark-angular/src/lib/components/inputs/sprk-textarea-container/sprk-textarea-container.component.ts
@@ -9,6 +9,7 @@ import {
 import { uniqueId } from 'lodash';
 import { SprkFieldErrorDirective } from '../../../directives/inputs/sprk-field-error/sprk-field-error.directive';
 import { SprkInputDirective } from '../../../directives/inputs/sprk-input/sprk-input.directive';
+import { SprkTextareaDirective } from '../../../directives/inputs/sprk-textarea/sprk-textarea.directive';
 import { SprkLabelDirective } from '../../../directives/inputs/sprk-label/sprk-label.directive';
 
 @Component({
@@ -61,6 +62,12 @@ export class SprkTextareaContainerComponent implements OnInit {
   @ContentChild(SprkInputDirective, { static: true })
   input: SprkInputDirective;
   /**
+   * This component expects a child input element
+   * with the `sprkInput` directive.
+   */
+  @ContentChild(SprkTextareaDirective, { static: true })
+  textarea: SprkTextareaDirective;
+  /**
    * This component expects a child element
    * with the `sprkFieldError` directive.
    */
@@ -98,25 +105,127 @@ export class SprkTextareaContainerComponent implements OnInit {
     return classArray.join(' ');
   }
 
+  hasID(element): boolean {
+    return element.id.length > 0;
+  }
+
+  hasFor(element): boolean {
+    return element.htmlFor.length > 0;
+  }
+
+  hasMatchingForAndID(label, input): boolean {
+    if (!this.hasFor(label) || !this.hasID(input)) {
+      return;
+    }
+    return label.htmlFor === input.id;
+  }
+
   ngOnInit(): void {
-    if (this.label && this.input) {
+    const inputEl = this.textarea || this.input;
+    if (!this.label || !inputEl) {
+      return;
+    }
+
+    /**
+     * If the label and the input both do not
+     * have a for or id then add custom ones.
+     */
+    if (
+      !this.hasID(inputEl.ref.nativeElement) &&
+      !this.hasFor(this.label.ref.nativeElement)
+    ) {
       this.renderer.setAttribute(
         this.label.ref.nativeElement,
         'for',
         this.input_id,
       );
       this.renderer.setAttribute(
-        this.input.ref.nativeElement,
+        inputEl.ref.nativeElement,
         'id',
         this.input_id,
       );
     }
-    if (this.input && this.error) {
+
+    /**
+     * If the input has an id
+     * but the label has no for
+     * then set the for the match the id.
+     */
+    if (
+      this.hasID(inputEl.ref.nativeElement) &&
+      !this.hasFor(this.label.ref.nativeElement)
+    ) {
       this.renderer.setAttribute(
-        this.input.ref.nativeElement,
+        this.label.ref.nativeElement,
+        'for',
+        inputEl.ref.nativeElement.id,
+      );
+    }
+
+    /**
+     * If the label has a for
+     * but the input has no id
+     * then set the id the match the for.
+     */
+    if (
+      this.hasFor(this.label.ref.nativeElement) &&
+      !this.hasID(inputEl.ref.nativeElement)
+    ) {
+      this.renderer.setAttribute(
+        inputEl.ref.nativeElement,
+        'id',
+        this.label.ref.nativeElement.htmlFor,
+      );
+    }
+
+    /**
+     * If the label for and the input id
+     * are there but mismatching, use the id
+     * value for the for attribute on the label.
+     * This is because input ids and labels
+     * need to match and there is no reason
+     * they should not match for a11y.
+     */
+    if (
+      !this.hasMatchingForAndID(
+        this.label.ref.nativeElement,
+        inputEl.ref.nativeElement,
+      )
+    ) {
+      this.renderer.setAttribute(
+        this.label.ref.nativeElement,
+        'for',
+        inputEl.ref.nativeElement.id,
+      );
+    }
+
+    /**
+     * If there is an error container
+     * with an id then match the aria-describedby
+     * on the input to the id on the
+     * error container.
+     */
+    if (this.error && this.hasID(this.error.ref.nativeElement)) {
+      this.renderer.setAttribute(
+        inputEl.ref.nativeElement,
+        'aria-describedby',
+        this.error.ref.nativeElement.id,
+      );
+    }
+
+    /**
+     * If there is an error container
+     * and it does not have an id
+     * then use custom id for the
+     * error and aria.
+     */
+    if (this.error && !this.hasID(this.error.ref.nativeElement)) {
+      this.renderer.setAttribute(
+        inputEl.ref.nativeElement,
         'aria-describedby',
         this.error_id,
       );
+
       this.renderer.setAttribute(
         this.error.ref.nativeElement,
         'id',
