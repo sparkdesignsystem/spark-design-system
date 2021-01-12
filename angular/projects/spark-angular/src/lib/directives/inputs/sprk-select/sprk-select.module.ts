ff0102bf48fe0ddb6137bfa43659ebb823ced585
corcorankt1 <guern1kn@gmail.com>
2021-01-12 17:28:24 -0500

    remove routerModule from sprkSelect

--- angular/projects/spark-angular/src/lib/directives/inputs/sprk-select/sprk-select.module.ts
+++ angular/projects/spark-angular/src/lib/directives/inputs/sprk-select/sprk-select.module.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { SprkSelectDirective } from './sprk-select.directive';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule],
   declarations: [SprkSelectDirective],
   exports: [SprkSelectDirective],
 })
