d247b02806dd43699e1b6a8764ba6a9bfebf2c55
corcorankt1 <guern1kn@gmail.com>
2021-01-14 16:58:13 -0500

    start updating story files for SprkInput

--- react/src/base/inputs/Search.stories.js
+++ react/src/base/inputs/Search.stories.js
@@ -1,23 +1,52 @@
 import React from 'react';
 import SprkTextInput from './SprkTextInput/SprkTextInput';
+import SprkInput from './SprkInput/SprkInput';
 import { markdownDocumentationLinkBuilder } from '../../../../storybook-utilities/markdownDocumentationLinkBuilder';
 
 export default {
   title: 'Components/Input/Search',
-  decorators: [
-    story => <div className="sprk-o-Box">{story()}</div>
-  ],
-  component: SprkTextInput,
+  decorators: [(story) => <div className="sprk-o-Box">{story()}</div>],
+  component: SprkInput,
   parameters: {
-    jest: [
-      'SprkErrorContainer',
-      'SprkInputIconCheck',
-    ],
+    jest: ['SprkErrorContainer', 'SprkInputIconCheck'],
     info: `${markdownDocumentationLinkBuilder('input')}`,
   },
 };
 
 export const searchInput = () => (
+  <SprkInput type="search" placeholder="Search" />
+);
+
+searchInput.story = {
+  name: 'Default',
+  parameters: {
+    jest: ['SprkInput'],
+  },
+};
+
+export const invalidSearchInput = () => (
+  <SprkInput type="search" placeholder="Search" isValid={false} />
+);
+
+invalidSearchInput.story = {
+  name: 'Invalid',
+  parameters: {
+    jest: ['SprkInput'],
+  },
+};
+
+export const disabledSearchInput = () => (
+  <SprkInput type="search" placeholder="Search" isDisabled />
+);
+
+disabledSearchInput.story = {
+  name: 'Disabled',
+  parameters: {
+    jest: ['SprkInput'],
+  },
+};
+
+export const legacySearchInput = () => (
   <SprkTextInput
     type="search"
     leadingIcon="search"
@@ -27,16 +56,14 @@ export const searchInput = () => (
   />
 );
 
-searchInput.story = {
-  name: 'Default',
+legacySearchInput.story = {
+  name: 'Legacy (Deprecated)',
   parameters: {
-    jest: [
-      'SprkTextInput',
-    ]
+    jest: ['SprkTextInput'],
   },
 };
 
-export const invalidSearchInput = () => (
+export const legacyInvalidSearchInput = () => (
   <SprkTextInput
     type="search"
     leadingIcon="search"
@@ -48,16 +75,14 @@ export const invalidSearchInput = () => (
   />
 );
 
-invalidSearchInput.story = {
-  name: 'Invalid',
+legacyInvalidSearchInput.story = {
+  name: 'Legacy Invalid (Deprecated)',
   parameters: {
-    jest: [
-      'SprkTextInput',
-    ]
+    jest: ['SprkTextInput'],
   },
 };
 
-export const disabledSearchInput = () => (
+export const legacyDisabledSearchInput = () => (
   <SprkTextInput
     type="search"
     leadingIcon="search"
@@ -68,11 +93,9 @@ export const disabledSearchInput = () => (
   />
 );
 
-disabledSearchInput.story = {
-  name: 'Disabled',
+legacyDisabledSearchInput.story = {
+  name: 'Legacy Disabled (Deprecated)',
   parameters: {
-    jest: [
-      'SprkTextInput',
-    ]
+    jest: ['SprkTextInput'],
   },
 };
--- react/src/base/inputs/Text.stories.js
+++ react/src/base/inputs/Text.stories.js
@@ -1,92 +1,160 @@
 import React from 'react';
 import SprkTextInput from './SprkTextInput/SprkTextInput';
+import SprkInput from './SprkInput/SprkInput';
 import { markdownDocumentationLinkBuilder } from '../../../../storybook-utilities/markdownDocumentationLinkBuilder';
 
 export default {
   title: 'Components/Input/Text',
   decorators: [(story) => <div className="sprk-o-Box">{story()}</div>],
-  component: SprkTextInput,
+  component: SprkInput,
   parameters: {
     jest: ['SprkErrorContainer', 'SprkInputIconCheck'],
     info: `${markdownDocumentationLinkBuilder('input')}`,
   },
 };
 
-export const textInput = () => (
-  <SprkTextInput label="Text Input Label" name="text-input-label" />
-);
+export const textInput = () => <SprkInput />;
 
 textInput.story = {
   name: 'Default',
   parameters: {
-    jest: ['SprkTextInput'],
+    jest: ['SprkInput'],
   },
 };
 
-export const invalidTextInput = () => (
-  <SprkTextInput
-    label="Text Input Label"
-    name="text-input-label"
-    valid={false}
-    errorMessage="There is an error on this field."
-  />
-);
+export const invalidTextInput = () => <SprkInput isValid={false} />;
 
 invalidTextInput.story = {
   name: 'Invalid',
   parameters: {
-    jest: ['SprkTextInput'],
+    jest: ['SprkInput'],
   },
 };
 
-export const disabledTextInput = () => (
-  <SprkTextInput label="Text Input Label" name="text-input-label" disabled />
-);
+export const disabledTextInput = () => <SprkInput isDisabled />;
 
 disabledTextInput.story = {
   name: 'Disabled',
   parameters: {
-    jest: ['SprkTextInput'],
+    jest: ['SprkInput'],
   },
 };
 
 export const hugeTextInput = () => (
-  <SprkTextInput
-    type="hugeTextInput"
-    label="Text Input Label"
-    name="text-input-label"
-    placeholder="Huge Text Placeholder"
-  />
+  <SprkInput variant="huge" placeholder="Huge Text Placeholder" />
 );
 
 hugeTextInput.story = {
   name: 'Huge',
-  component: SprkTextInput,
+  component: SprkInput,
   parameters: {
-    jest: ['SprkTextInput'],
+    jest: ['SprkInput'],
   },
 };
 
 export const invalidHugeTextInput = () => (
-  <SprkTextInput
-    type="hugeTextInput"
-    label="Text Input Label"
-    name="text-input-label"
+  <SprkInput
+    variant="huge"
     placeholder="Huge Text Placeholder"
-    valid={false}
-    errorMessage="There is an error on this field."
+    isValid={false}
   />
 );
 
 invalidHugeTextInput.story = {
   name: 'Huge Invalid',
+  component: SprkInput,
+  parameters: {
+    jest: ['SprkInput'],
+  },
+};
+
+export const disabledHugeTextInput = () => (
+  <SprkInput variant="huge" placeholder="Huge Text Placeholder" isDisabled />
+);
+
+disabledHugeTextInput.story = {
+  name: 'Huge Disabled',
+  component: SprkInput,
+  parameters: {
+    jest: ['SprkInput'],
+  },
+};
+
+export const legacyTextInput = () => (
+  <SprkTextInput label="Text Input Label" name="text-input-label" />
+);
+
+legacyTextInput.story = {
+  name: 'Legacy (Deprecated)',
+  parameters: {
+    jest: ['SprkTextInput'],
+  },
+};
+
+export const legacyInvalidTextInput = () => (
+  <SprkTextInput
+    label="Text Input Label"
+    name="text-input-label"
+    valid={false}
+    errorMessage="There is an error on this field."
+  />
+);
+
+legacyInvalidTextInput.story = {
+  name: 'Legacy Invalid (Deprecated)',
+  parameters: {
+    jest: ['SprkTextInput'],
+  },
+};
+
+export const legacyDisabledTextInput = () => (
+  <SprkTextInput label="Text Input Label" name="text-input-label" disabled />
+);
+
+legacyDisabledTextInput.story = {
+  name: 'Legacy Disabled (Deprecated)',
+  parameters: {
+    jest: ['SprkTextInput'],
+  },
+};
+
+export const legacyHugeTextInput = () => (
+  <SprkTextInput
+    type="hugeTextInput"
+    label="Text Input Label"
+    name="text-input-label"
+    placeholder="Huge Text Placeholder"
+  />
+);
+
+legacyHugeTextInput.story = {
+  name: 'Legacy Huge (Deprecated)',
   component: SprkTextInput,
   parameters: {
     jest: ['SprkTextInput'],
   },
 };
 
-export const disabledHugeTextInput = () => (
+export const legacyInvalidHugeTextInput = () => (
+  <SprkTextInput
+    type="hugeTextInput"
+    label="Text Input Label"
+    name="text-input-label"
+    placeholder="Huge Text Placeholder"
+    valid={false}
+    errorMessage="There is an error on this field."
+  />
+);
+
+legacyInvalidHugeTextInput.story = {
+  name: 'Legacy Huge Invalid (Deprecated)',
+  component: SprkTextInput,
+  parameters: {
+    jest: ['SprkTextInput'],
+  },
+};
+
+export const legacyDisabledHugeTextInput = () => (
   <SprkTextInput
     type="hugeTextInput"
     label="Text Input Label"
@@ -96,8 +164,8 @@ export const disabledHugeTextInput = () => (
   />
 );
 
-disabledHugeTextInput.story = {
-  name: 'Huge Disabled',
+legacyDisabledHugeTextInput.story = {
+  name: 'Legacy Huge Disabled (Deprecated)',
   component: SprkTextInput,
   parameters: {
     jest: ['SprkTextInput'],
