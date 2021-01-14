fafa0c59d762d0b805f13bc65059040d028f3abc
corcorankt1 <guern1kn@gmail.com>
2021-01-14 14:04:03 -0500

    add SprkInput

---
+++ react/src/base/inputs/SprkInput/SprkInput.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+class SprkInput extends Component {
+  constructor(props) {
+    const { value } = props;
+    super(props);
+
+    if (value) {
+      this.state = {
+        hasValue: true,
+      };
+    } else {
+      this.state = {
+        hasValue: false,
+      };
+    }
+  }
+
+  render() {
+    const {
+      variant,
+      additionalClasses,
+      analyticsString,
+      formatter,
+      forwardedRef,
+      idString,
+      isDisabled,
+      isValid,
+      value,
+      ...rest
+    } = this.props;
+    const { hasValue } = this.state;
+
+    // Adds class for IE and Edge
+    const handleOnBlur = (e) => {
+      if (e.target.value.length > 0) {
+        this.setState({ hasValue: true });
+      } else {
+        this.setState({ hasValue: false });
+      }
+    };
+
+    return (
+      <input
+        className={classNames(
+          'sprk-u-Width-100 sprk-b-TextInput',
+          additionalClasses,
+          {
+            'sprk-b-TextInput--error': !isValid,
+            'sprk-b-Input--has-floating-label': hasValue && variant === 'huge',
+          },
+        )}
+        disabled={isDisabled}
+        ref={forwardedRef}
+        data-id={idString}
+        data-analytics={analyticsString}
+        aria-invalid={!isValid}
+        value={isValid && formatter(value) ? formatter(value) : value}
+        onBlur={(e) => handleOnBlur(e)}
+        {...rest}
+      />
+    );
+  }
+}
+
+SprkInput.propTypes = {
+  /**
+   * Determines the style of the input container.
+   * Supplying no value will cause
+   * the default styles to be used.
+   */
+  variant: PropTypes.oneOf(['huge']),
+  /**
+   * A space-separated string of classes
+   * to add to the outermost container of the component.
+   */
+  additionalClasses: PropTypes.string,
+  /**
+   * Assigned to the `data-analytics` attribute serving as
+   * a unique selector for outside libraries to capture data.
+   */
+  analyticsString: PropTypes.string,
+  /**
+   * A function supplied will be passed the value of the
+   * input and then executed, if the valid prop is true. The value
+   * returned will be assigned to the value of the input.
+   */
+  formatter: PropTypes.func,
+  /**
+   * Assigned to the `data-id` attribute serving as a
+   * unique selector for automated tools.
+   */
+  idString: PropTypes.string,
+  /**
+   * Determines whether to render the
+   * component in the valid or the error state.
+   */
+  isValid: PropTypes.bool,
+  /**
+   * A ref passed in will be attached to the
+   * input element of the rendered component.
+   */
+  forwardedRef: PropTypes.shape(),
+  /**
+   * Will render the component in it's disabled state.
+   */
+  isDisabled: PropTypes.bool,
+  /**
+   * 	Assigned to the `value` attribute
+   *  of the rendered input element.
+   */
+  value: PropTypes.string,
+};
+
+SprkInput.defaultProps = {
+  formatter: (value) => value,
+  forwardedRef: React.createRef(),
+  isValid: true,
+  isDisabled: false,
+};
+
+export default SprkInput;
---
+++ react/src/base/inputs/SprkInput/SprkInput.test.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import SprkInput from './SprkInput';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('SprkInput:', () => {
+  it('should render an element with the correct class', () => {
+    const wrapper = mount(<SprkInput />);
+
+    expect(wrapper.find('.sprk-b-TextInput').length).toBe(1);
+    expect(wrapper.find('.sprk-u-Width-100').length).toBe(1);
+    expect(
+      wrapper.find('.sprk-b-TextInput').getDOMNode().classList.length,
+    ).toBe(2);
+    expect(wrapper.state().hasValue).toBe(false);
+  });
+
+  it('should add classes when additionalClasses has a value', () => {
+    const wrapper = mount(<SprkInput additionalClasses="sprk-u-man" />);
+
+    expect(wrapper.find('.sprk-b-TextInput').hasClass('sprk-u-man')).toBe(true);
+    expect(
+      wrapper.find('.sprk-b-TextInput').getDOMNode().classList.length,
+    ).toBe(3);
+  });
+
+  it('should assign data-analytics when analyticsString has a value', () => {
+    const wrapper = mount(<SprkInput analyticsString="321" />);
+    expect(wrapper.find('[data-analytics="321"]').length).toBe(1);
+  });
+
+  it('should assign data-id when idString has a value', () => {
+    const wrapper = mount(<SprkInput idString="321" />);
+    expect(wrapper.find('[data-id="321"]').length).toBe(1);
+  });
+
+  it(`should add floating label class
+  to huge input when a value is present and blurred out`, () => {
+    const wrapper = mount(<SprkInput variant="huge" />);
+    const input = wrapper.find('input');
+    input.value = 'foo';
+    // on blur pass object into blur handler
+    // Enzyme does not update state of the DOM
+    // but we can simulate update to the DOM state by
+    // passing same state into react function
+    input.simulate('blur', { target: { value: 'foo' } });
+
+    expect(
+      wrapper
+        .find('.sprk-b-TextInput')
+        .hasClass('sprk-b-Input--has-floating-label'),
+    ).toBe(true);
+    expect(wrapper.state().hasValue).toBe(true);
+  });
+
+  it(`should remove/not have floating label
+  class to huge input when a value is not present and blurred out`, () => {
+    const wrapper = mount(<SprkInput variant="huge" />);
+    const input = wrapper.find('input');
+
+    input.simulate('blur', { target: { value: '' } });
+
+    expect(
+      wrapper
+        .find('.sprk-b-TextInput')
+        .hasClass('sprk-b-Input--has-floating-label'),
+    ).toBe(false);
+  });
+
+  it('should add floating label class to huge text when there is value', () => {
+    const wrapper = mount(<SprkInput value="value present" variant="huge" />);
+    expect(
+      wrapper
+        .find('.sprk-b-TextInput')
+        .hasClass('sprk-b-Input--has-floating-label'),
+    ).toBe(true);
+  });
+
+  it('should run the supplied formatter', () => {
+    const formatter = jest.fn(() => true);
+    const onChangeMock = jest.fn();
+    mount(
+      <SprkInput type="text" formatter={formatter} onChange={onChangeMock} />,
+    );
+    expect(formatter.mock.calls.length).toBe(2);
+  });
+
+  it('should not run the formatter if the field is invalid', () => {
+    const formatter = jest.fn();
+    mount(<SprkInput type="text" formatter={formatter} isValid={false} />);
+    expect(formatter.mock.calls.length).toBe(0);
+  });
+});
