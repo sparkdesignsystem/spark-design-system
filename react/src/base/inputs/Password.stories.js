4a367bacc0997d4c30fe5fbac8e4c0684f0c2fa5
Febbraro <amberfebbraro@quickenloans.com>
2021-01-19 14:37:14 -0700

    add containers to password

--- react/src/base/inputs/Password.stories.js
+++ react/src/base/inputs/Password.stories.js
@@ -2,6 +2,9 @@ import React from 'react';
 import SprkRevealInput from './SprkRevealInput/SprkRevealInput';
 import SprkInput from './SprkInput/SprkInput';
 import SprkLabel from './SprkLabel/SprkLabel';
+import SprkInputContainer from './SprkInputContainer/SprkInputContainer';
+import SprkErrorContainer from './SprkErrorContainer/SprkErrorContainer';
+
 import { markdownDocumentationLinkBuilder } from '../../../../storybook-utilities/markdownDocumentationLinkBuilder';
 
 export default {
@@ -15,44 +18,55 @@ export default {
 };
 
 export const passwordInput = () => (
-  <>
+  <SprkInputContainer>
     <SprkLabel>Password</SprkLabel>
     <SprkInput />
-  </>
+  </SprkInputContainer>
 );
 
 passwordInput.story = {
   name: 'Default',
   parameters: {
-    jest: ['SprkInput', 'SprkLabel'],
+    jest: ['SprkInput', 'SprkLabel', 'SprkInputContainer'],
   },
 };
 
 export const invalidPasswordInput = () => (
-  <>
+  <SprkInputContainer>
     <SprkLabel>Password</SprkLabel>
     <SprkInput isValid={false} />
-  </>
+    <SprkErrorContainer
+      id="invalid-password"
+      message="Update this story once error container is done"
+    >
+      There is an error on this field.
+    </SprkErrorContainer>
+  </SprkInputContainer>
 );
 
 invalidPasswordInput.story = {
   name: 'Invalid',
   parameters: {
-    jest: ['SprkInput', 'SprkLabel'],
+    jest: [
+      'SprkInput',
+      'SprkLabel',
+      'SprkInputContainer',
+      'SprkErrorContainer',
+    ],
   },
 };
 
 export const disabledPasswordInput = () => (
-  <>
+  <SprkInputContainer>
     <SprkLabel isDisabled>Password</SprkLabel>
     <SprkInput isDisabled />
-  </>
+  </SprkInputContainer>
 );
 
 disabledPasswordInput.story = {
   name: 'Disabled',
   parameters: {
-    jest: ['SprkInput', 'SprkLabel'],
+    jest: ['SprkInput', 'SprkLabel', 'SprkInputContainer'],
   },
 };
 
