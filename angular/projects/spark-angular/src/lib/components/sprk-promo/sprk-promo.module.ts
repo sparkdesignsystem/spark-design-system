fec05d96949b1569f636a1af404813d9f4968412
corcorankt1 <guern1kn@gmail.com>
2021-01-26 14:47:49 -0500

    update routerLink for sprk-promo

--- angular/projects/spark-angular/src/lib/components/sprk-promo/sprk-promo.module.ts
+++ angular/projects/spark-angular/src/lib/components/sprk-promo/sprk-promo.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { SprkIconModule } from '../sprk-icon/sprk-icon.module';
 import { SprkLinkDirectiveModule } from '../../directives/sprk-link/sprk-link.module';
 import { SprkPromoComponent } from './sprk-promo.component';
@@ -7,10 +8,11 @@ import { SprkPromoComponent } from './sprk-promo.component';
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule.forRoot([]),
     SprkIconModule,
-    SprkLinkDirectiveModule
+    SprkLinkDirectiveModule,
   ],
   declarations: [SprkPromoComponent],
-  exports: [SprkPromoComponent]
+  exports: [SprkPromoComponent],
 })
 export class SprkPromoModule {}
