e3af462eaa065eaa3f7f3ed33a38a4087541492d
Febbraro <amberfebbraro@quickenloans.com>
2021-01-26 13:09:22 -0700

    remove debug By from ng

--- angular/projects/spark-angular/src/lib/components/sprk-masthead/sprk-masthead.component.spec.ts
+++ angular/projects/spark-angular/src/lib/components/sprk-masthead/sprk-masthead.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { By } from '@angular/platform-browser';
 import { SprkIconComponent } from '../sprk-icon/sprk-icon.component';
 import { SprkLinkDirective } from '../../directives/sprk-link/sprk-link.directive';
 import { SprkMastheadAccordionComponent } from './sprk-masthead-accordion/sprk-masthead-accordion.component';
