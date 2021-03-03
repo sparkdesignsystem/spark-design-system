d4e018f05b34b5ae3f4d1ce78ccf66d1dbf424ae
corcorankt1 <guern1kn@gmail.com>
2021-03-03 11:22:22 -0500

    add tests for SprkRadioGroup

--- react/src/base/inputs/SprkRadio/SprkRadioGroup/SprkRadioGroup.test.js
+++ react/src/base/inputs/SprkRadio/SprkRadioGroup/SprkRadioGroup.test.js
@@ -4,7 +4,9 @@ import Adapter from 'enzyme-adapter-react-16';
 import SprkRadioGroup from './SprkRadioGroup';
 import SprkRadioItem from '../SprkRadioItem/SprkRadioItem';
 import SprkErrorContainer from '../../SprkErrorContainer/SprkErrorContainer';
-import SprkFieldset from '../../SprkFieldset/SprkFieldset';
+import SprkFieldError from '../../SprkFieldError/SprkFieldError';
+import SprkHelperText from '../../SprkHelperText/SprkHelperText';
+import SprkFieldSet from '../../SprkFieldSet/SprkFieldSet';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -15,6 +17,20 @@ describe('SprkRadioGroup:', () => {
         <SprkRadioItem>Label</SprkRadioItem>
         <SprkRadioItem>Label</SprkRadioItem>
         <SprkRadioItem>Label</SprkRadioItem>
+      </SprkRadioGroup>,
+    );
+    expect(wrapper.find('SprkRadioItem').length).toBe(3);
+  });
+
+  it(`should render SprkRadioItem inside of it when
+  they are grandchildren`, () => {
+    const wrapper = shallow(
+      <SprkRadioGroup>
+        <SprkFieldSet>
+          <SprkRadioItem>Label</SprkRadioItem>
+          <SprkRadioItem>Label</SprkRadioItem>
+          <SprkRadioItem>Label</SprkRadioItem>
+        </SprkFieldSet>
       </SprkRadioGroup>,
     );
     expect(wrapper.find('SprkRadioItem').length).toBe(3);
@@ -60,36 +76,273 @@ describe('SprkRadioGroup:', () => {
     );
   });
 
-  it(`should assign ariaDescribedBy to SprkRadioItem that matches the
-  supplied id on SprkErrorContainer`, () => {
+  it(`should add the helperTextID to the aria-describedby
+  on the container if it isn't present`, () => {
     const wrapper = mount(
       <SprkRadioGroup>
-        <SprkFieldset>
-          <SprkRadioItem name="radio" />
-          <SprkRadioItem name="radio" />
-          <SprkRadioItem name="radio" />
-        </SprkFieldset>
-        <SprkErrorContainer id="error-id" message="Error" />
+        <SprkRadioItem>Radio Item 1</SprkRadioItem>
+        <SprkRadioItem>Radio Item 2</SprkRadioItem>
+        <SprkRadioItem>Radio Item 3</SprkRadioItem>
+        <SprkHelperText id="test-id" />
       </SprkRadioGroup>,
     );
 
-    wrapper.find(SprkRadioItem).forEach((item) => {
-      expect(item.props().ariaDescribedBy).toContain('error-id');
-    });
+    expect(
+      wrapper
+        .find('.sprk-b-InputContainer')
+        .getDOMNode()
+        .getAttribute('aria-describedby'),
+    ).toBe('test-id');
   });
 
-  it(`should not add aria-describedby to RadioItems when there is no
-  ErrorContainer or HelperText`, () => {
+  it(`should add the errorContainerID to the aria-describedby
+  on the container if it isn't present`, () => {
     const wrapper = mount(
       <SprkRadioGroup>
-        <SprkRadioItem name="radio" />
-        <SprkRadioItem name="radio" />
-        <SprkRadioItem name="radio" />
+        <SprkRadioItem>Radio Item 1</SprkRadioItem>
+        <SprkRadioItem>Radio Item 2</SprkRadioItem>
+        <SprkRadioItem>Radio Item 3</SprkRadioItem>
+        <SprkErrorContainer id="test-id" message="test message" />
       </SprkRadioGroup>,
     );
 
-    wrapper.find(SprkRadioItem).forEach((item) => {
-      expect(typeof item.props().ariaDescribedBy === 'undefined').toEqual(true);
-    });
+    expect(
+      wrapper
+        .find('.sprk-b-InputContainer')
+        .getDOMNode()
+        .getAttribute('aria-describedby'),
+    ).toBe('test-id');
+  });
+
+  it(`should add the fieldErrorID to the aria-describedby
+  on the container if it isn't present`, () => {
+    const wrapper = mount(
+      <SprkRadioGroup>
+        <SprkRadioItem>Radio Item 1</SprkRadioItem>
+        <SprkRadioItem>Radio Item 2</SprkRadioItem>
+        <SprkRadioItem>Radio Item 3</SprkRadioItem>
+        <SprkFieldError id="test-id" />
+      </SprkRadioGroup>,
+    );
+
+    expect(
+      wrapper
+        .find('.sprk-b-InputContainer')
+        .getDOMNode()
+        .getAttribute('aria-describedby'),
+    ).toBe('test-id');
+  });
+
+  it(`should allow additional values be passed to the aria-describedby`, () => {
+    const wrapper = mount(
+      <SprkRadioGroup ariaDescribedBy="additional-value">
+        <SprkRadioItem>Radio Item 1</SprkRadioItem>
+        <SprkRadioItem>Radio Item 2</SprkRadioItem>
+        <SprkRadioItem>Radio Item 3</SprkRadioItem>
+        <SprkFieldError id="test-id" />
+      </SprkRadioGroup>,
+    );
+
+    expect(
+      wrapper
+        .find('.sprk-b-InputContainer')
+        .getDOMNode()
+        .getAttribute('aria-describedby'),
+    ).toBe('additional-value test-id');
+  });
+
+  it(`should add helperTextID and fieldErrorID to the
+  aria-describedby if neither are present`, () => {
+    const wrapper = mount(
+      <SprkRadioGroup>
+        <SprkRadioItem>Radio Item 1</SprkRadioItem>
+        <SprkRadioItem>Radio Item 2</SprkRadioItem>
+        <SprkRadioItem>Radio Item 3</SprkRadioItem>
+        <SprkFieldError id="test-error-id" />
+        <SprkHelperText id="test-helper-id" />
+      </SprkRadioGroup>,
+    );
+
+    expect(
+      wrapper
+        .find('.sprk-b-InputContainer')
+        .getDOMNode()
+        .getAttribute('aria-describedby'),
+    ).toBe('test-error-id test-helper-id');
+  });
+
+  it(`should add helperTextID and errorContainerID to the
+  aria-describedby if neither are present`, () => {
+    const wrapper = mount(
+      <SprkRadioGroup>
+        <SprkRadioItem>Radio Item 1</SprkRadioItem>
+        <SprkRadioItem>Radio Item 2</SprkRadioItem>
+        <SprkRadioItem>Radio Item 3</SprkRadioItem>
+        <SprkErrorContainer message="test message" id="test-error-id" />
+        <SprkHelperText id="test-helper-id" />
+      </SprkRadioGroup>,
+    );
+
+    expect(
+      wrapper
+        .find('.sprk-b-InputContainer')
+        .getDOMNode()
+        .getAttribute('aria-describedby'),
+    ).toBe('test-error-id test-helper-id');
+  });
+
+  it(`should add helperTextID and fieldErrorID to the
+  aria-describedby if neither are present and keep additional values`, () => {
+    const wrapper = mount(
+      <SprkRadioGroup ariaDescribedBy="additional-value">
+        <SprkRadioItem>Radio Item 1</SprkRadioItem>
+        <SprkRadioItem>Radio Item 2</SprkRadioItem>
+        <SprkRadioItem>Radio Item 3</SprkRadioItem>
+        <SprkFieldError id="test-error-id" />
+        <SprkHelperText id="test-helper-id" />
+      </SprkRadioGroup>,
+    );
+
+    expect(
+      wrapper
+        .find('.sprk-b-InputContainer')
+        .getDOMNode()
+        .getAttribute('aria-describedby'),
+    ).toBe('additional-value test-error-id test-helper-id');
+  });
+
+  it(`should add helperTextID and errorContainerID to the
+  aria-describedby if neither are present and keep additional values`, () => {
+    const wrapper = mount(
+      <SprkRadioGroup ariaDescribedBy="additional-value">
+        <SprkRadioItem>Radio Item 1</SprkRadioItem>
+        <SprkRadioItem>Radio Item 2</SprkRadioItem>
+        <SprkRadioItem>Radio Item 3</SprkRadioItem>
+        <SprkErrorContainer message="test message" id="test-error-id" />
+        <SprkHelperText id="test-helper-id" />
+      </SprkRadioGroup>,
+    );
+
+    expect(
+      wrapper
+        .find('.sprk-b-InputContainer')
+        .getDOMNode()
+        .getAttribute('aria-describedby'),
+    ).toBe('additional-value test-error-id test-helper-id');
+  });
+
+  it(`should not add helperTextID and errorContainerID to the
+  aria-describedby if both are present and keep additional values`, () => {
+    const wrapper = mount(
+      <SprkRadioGroup ariaDescribedBy="value test-helper-id test-error-id">
+        <SprkRadioItem>Radio Item 1</SprkRadioItem>
+        <SprkRadioItem>Radio Item 2</SprkRadioItem>
+        <SprkRadioItem>Radio Item 3</SprkRadioItem>
+        <SprkErrorContainer message="test message" id="test-error-id" />
+        <SprkHelperText id="test-helper-id" />
+      </SprkRadioGroup>,
+    );
+
+    expect(
+      wrapper
+        .find('.sprk-b-InputContainer')
+        .getDOMNode()
+        .getAttribute('aria-describedby'),
+    ).toBe('value test-helper-id test-error-id');
+  });
+
+  it(`should not add helperTextID and fieldErrorID to the
+  aria-describedby if both are present and keep additional values`, () => {
+    const wrapper = mount(
+      <SprkRadioGroup ariaDescribedBy="value test-helper-id test-error-id">
+        <SprkRadioItem>Radio Item 1</SprkRadioItem>
+        <SprkRadioItem>Radio Item 2</SprkRadioItem>
+        <SprkRadioItem>Radio Item 3</SprkRadioItem>
+        <SprkFieldError id="test-error-id" />
+        <SprkHelperText id="test-helper-id" />
+      </SprkRadioGroup>,
+    );
+
+    expect(
+      wrapper
+        .find('.sprk-b-InputContainer')
+        .getDOMNode()
+        .getAttribute('aria-describedby'),
+    ).toBe('value test-helper-id test-error-id');
+  });
+
+  it(`should not add helperTextID to the
+  aria-describedby if it's present`, () => {
+    const wrapper = mount(
+      <SprkRadioGroup ariaDescribedBy="test-helper-id">
+        <SprkRadioItem>Radio Item 1</SprkRadioItem>
+        <SprkRadioItem>Radio Item 2</SprkRadioItem>
+        <SprkRadioItem>Radio Item 3</SprkRadioItem>
+        <SprkHelperText id="test-helper-id" />
+      </SprkRadioGroup>,
+    );
+
+    expect(
+      wrapper
+        .find('.sprk-b-InputContainer')
+        .getDOMNode()
+        .getAttribute('aria-describedby'),
+    ).toBe('test-helper-id');
+  });
+
+  it(`should not add errorContainerID to the
+  aria-describedby if it's present`, () => {
+    const wrapper = mount(
+      <SprkRadioGroup ariaDescribedBy="test-id">
+        <SprkRadioItem>Radio Item 1</SprkRadioItem>
+        <SprkRadioItem>Radio Item 2</SprkRadioItem>
+        <SprkRadioItem>Radio Item 3</SprkRadioItem>
+        <SprkErrorContainer message="test message" id="test-id" />
+      </SprkRadioGroup>,
+    );
+
+    expect(
+      wrapper
+        .find('.sprk-b-InputContainer')
+        .getDOMNode()
+        .getAttribute('aria-describedby'),
+    ).toBe('test-id');
+  });
+
+  it(`should not add fieldErrorID to the
+  aria-describedby if it's present`, () => {
+    const wrapper = mount(
+      <SprkRadioGroup ariaDescribedBy="test-id">
+        <SprkRadioItem>Radio Item 1</SprkRadioItem>
+        <SprkRadioItem>Radio Item 2</SprkRadioItem>
+        <SprkRadioItem>Radio Item 3</SprkRadioItem>
+        <SprkFieldError id="test-id" />
+      </SprkRadioGroup>,
+    );
+
+    expect(
+      wrapper
+        .find('.sprk-b-InputContainer')
+        .getDOMNode()
+        .getAttribute('aria-describedby'),
+    ).toBe('test-id');
+  });
+
+  it(`should not add aria-describedby to the container
+  if there is no helper or error text`, () => {
+    const wrapper = mount(
+      <SprkRadioGroup>
+        <SprkRadioItem>Radio Item 1</SprkRadioItem>
+        <SprkRadioItem>Radio Item 2</SprkRadioItem>
+        <SprkRadioItem>Radio Item 3</SprkRadioItem>
+      </SprkRadioGroup>,
+    );
+
+    expect(
+      wrapper
+        .find('.sprk-b-InputContainer')
+        .getDOMNode()
+        .getAttribute('aria-describedby'),
+    ).toBe(null);
   });
 });
