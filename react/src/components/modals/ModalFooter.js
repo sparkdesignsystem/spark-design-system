85c6117c00a89fe823d6d3000f12f61ffdd63649
corcorankt1 <guern1kn@gmail.com>
2021-03-25 14:37:49 -0400

    update react modal buttons

--- react/src/components/modals/ModalFooter.js
+++ react/src/components/modals/ModalFooter.js
@@ -11,7 +11,8 @@ class ModalFooter extends Component {
   }
 
   focusConfirm() {
-    // Apply focus to the confirm button. The button should receive focus when the
+    // Apply focus to the confirm button.
+    // The button should receive focus when the
     // modal becomes visible (not necessarily when it renders or updates), and
     // only in the default variant.
 
@@ -29,27 +30,37 @@ class ModalFooter extends Component {
     } = this.props;
 
     return (
-      <footer className="sprk-o-Stack__item">
+      <footer
+        className="
+        sprk-o-Stack__item
+        sprk-c-Modal__footer
+        sprk-o-Stack
+        sprk-o-Stack--split@xxs
+        sprk-o-Stack--end-row"
+      >
         {/* vanilla expects a paragraph here */}
         <p>&nbsp;</p>
         <button
-          className="sprk-c-Button sprk-u-mrm"
-          onClick={confirmClick}
-          type="button"
-          ref={this.confirmButtonRef}
-          data-analytics={confirmAnalyticsString}
-        >
-          {confirmText}
-        </button>
-
-        <button
-          className="sprk-c-Button sprk-c-Button--tertiary"
+          className="
+            sprk-c-Button
+            sprk-c-Button--tertiary
+            sprk-u-mrm
+            sprk-o-Stack__item"
           data-sprk-modal-cancel="exampleChoiceModal"
           onClick={cancelClick}
           type="button"
           data-analytics={cancelAnalyticsString}
         >
           {cancelText}
+        </button>
+        <button
+          className="sprk-c-Button sprk-o-Stack__item"
+          onClick={confirmClick}
+          type="button"
+          ref={this.confirmButtonRef}
+          data-analytics={confirmAnalyticsString}
+        >
+          {confirmText}
         </button>
       </footer>
     );
@@ -59,22 +70,24 @@ class ModalFooter extends Component {
 ModalFooter.propTypes = {
   /**
    * The function that runs when confirm choice is clicked.
-  */
+   */
   confirmClick: PropTypes.func,
   /**
    * The function that runs when cancel choice, the mask, or Esc is pressed.
-  */
+   */
   cancelClick: PropTypes.func,
   /** Text to render on the confirmation button. */
   confirmText: PropTypes.string,
   /** Text to render on the cancel button. */
   cancelText: PropTypes.string,
   /**
-   * Assigned to the `data-analytics` attribute serving as a unique selector for outside libraries to capture data.
+   * Assigned to the `data-analytics` attribute serving
+   * as a unique selector for outside libraries to capture data.
    */
   confirmAnalyticsString: PropTypes.string,
   /**
-   * Assigned to the `data-analytics` attribute serving as a unique selector for outside libraries to capture data.
+   * Assigned to the `data-analytics` attribute serving
+   * as a unique selector for outside libraries to capture data.
    */
   cancelAnalyticsString: PropTypes.string,
 };
