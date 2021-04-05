2068eb5c139cd2b38fa98b8cc5b34896ac596c00
Bob Wise <bobwise@quickenloans.com>
2021-04-05 13:55:31 -0400

    checkbox updates

--- styles/ios/SparkDesignTokens.swift
+++ styles/ios/SparkDesignTokens.swift
@@ -67,10 +67,10 @@ public class SparkDesignTokens {
     public static let sprkCheckboxCustomInputBackgroundColorFocus = UIColor(red: 0.969, green: 0.969, blue: 0.969, alpha:1)
     public static let sprkCheckboxCustomInputBackgroundColorHover = UIColor(red: 0.969, green: 0.969, blue: 0.969, alpha:1)
     public static let sprkCheckboxHugeContainerCheckedBoxShadow = 0 0 0 1px UIColor(red: 0.376, green: 0.227, blue: 0.631, alpha:1)
-    public static let sprkCheckboxHugeContainerCheckedHoverBoxShadow = 0 0 0 1px UIColor(red: 0.110, green: 0.106, blue: 0.102, alpha:1), 0 2px 8px UIColor(red: 0.729, green: 0.729, blue: 0.729, alpha:1)
+    public static let sprkCheckboxHugeContainerCheckedHoverBoxShadow = 0 0 0 2px UIColor(red: 0.110, green: 0.106, blue: 0.102, alpha:1), 0 2px 8px UIColor(red: 0.729, green: 0.729, blue: 0.729, alpha:1)
     public static let sprkCheckboxHugeContainerDisabledBackground = UIColor(red: 0.867, green: 0.867, blue: 0.867, alpha:1)
-    public static let sprkCheckboxHugeContainerFocusCheckedBoxShadow = 0 0 0 2px UIColor(red: 0.110, green: 0.106, blue: 0.102, alpha:1)
-    public static let sprkCheckboxHugeContainerFocusHoverBoxShadow = 0 0 0 1px UIColor(red: 0.110, green: 0.106, blue: 0.102, alpha:1), 0 2px 8px UIColor(red: 0.729, green: 0.729, blue: 0.729, alpha:1)
+    public static let sprkCheckboxHugeContainerFocusCheckedBoxShadow = 0 0 0 2px UIColor(red: 0.376, green: 0.227, blue: 0.631, alpha:1)
+    public static let sprkCheckboxHugeContainerFocusHoverBoxShadow = 0 0 0 2px UIColor(red: 0.110, green: 0.106, blue: 0.102, alpha:1), 0 2px 8px UIColor(red: 0.729, green: 0.729, blue: 0.729, alpha:1)
     public static let sprkCheckboxHugeCustomInputCheckboxElementCheckedBoxShadow = none
     public static let sprkCheckboxHugeCustomInputLeft = CGFloat(16.00)
     public static let sprkCheckboxHugeCustomInputTop = CGFloat(16.00)
