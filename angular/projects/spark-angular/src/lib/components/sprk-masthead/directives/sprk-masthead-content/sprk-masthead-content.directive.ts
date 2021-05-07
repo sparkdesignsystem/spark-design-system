709767812bebf3687d11235b1e75cf6a65db71c8
Febbraro <amberfebbraro@quickenloans.com>
2021-05-07 10:11:47 -0600

    add spacing

--- angular/projects/spark-angular/src/lib/components/sprk-masthead/directives/sprk-masthead-content/sprk-masthead-content.directive.ts
+++ angular/projects/spark-angular/src/lib/components/sprk-masthead/directives/sprk-masthead-content/sprk-masthead-content.directive.ts
@@ -10,8 +10,9 @@ export class SprkMastheadContentDirective {
   constructor(public ref: ElementRef) {}
 
   /**
-   * The value supplied will be assigned to the `data-id` attribute on the
-   * component. This is intended to be used as a selector for automated
+   * The value supplied will be assigned to the
+   * `data-id` attribute on the component.
+   * This is intended to be used as a selector for automated
    * tools. This value should be unique per page.
    */
   @HostBinding('attr.data-id')
