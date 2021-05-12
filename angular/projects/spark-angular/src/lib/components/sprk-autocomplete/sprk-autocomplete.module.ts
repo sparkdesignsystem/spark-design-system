6d57c58158f869a06c64d5a66bb8708cdfc2ef3f
Bob Wise <bobwise@quickenloans.com>
2021-05-12 14:48:57 -0400

    update modules

--- angular/projects/spark-angular/src/lib/components/sprk-autocomplete/sprk-autocomplete.module.ts
+++ angular/projects/spark-angular/src/lib/components/sprk-autocomplete/sprk-autocomplete.module.ts
@@ -5,6 +5,9 @@ import { SprkIconModule } from '../sprk-icon/sprk-icon.module';
 import { SprkAutocompleteComponent } from './sprk-autocomplete.component';
 import { SprkAutocompleteResultsModule } from './sprk-autocomplete-results/sprk-autocomplete-results.module';
 import { SprkAutocompleteResultModule } from './sprk-autocomplete-result/sprk-autocomplete-result.module';
+import { SprkInputContainerModule } from '../inputs/sprk-input-container/sprk-input-container.module';
+import { SprkInputModule } from '../../directives/inputs/sprk-input/sprk-input.module';
+import { SprkFieldErrorModule } from '../../directives/inputs/sprk-field-error/sprk-field-error.module';
 
 @NgModule({
   imports: [
@@ -13,8 +16,21 @@ import { SprkAutocompleteResultModule } from './sprk-autocomplete-result/sprk-au
     SprkIconModule,
     SprkAutocompleteResultsModule,
     SprkAutocompleteResultModule,
-  ], //todo do we need input here? What do we need routermodule for?
+    SprkInputContainerModule,
+    SprkInputModule,
+    SprkFieldErrorModule,
+  ],
   declarations: [SprkAutocompleteComponent],
-  exports: [SprkAutocompleteComponent],
+  exports: [
+    SprkAutocompleteComponent,
+    CommonModule,
+    RouterModule,
+    SprkIconModule,
+    SprkAutocompleteResultsModule,
+    SprkAutocompleteResultModule,
+    SprkInputContainerModule,
+    SprkInputModule,
+    SprkFieldErrorModule,
+  ],
 })
 export class SprkAutocompleteModule {}
