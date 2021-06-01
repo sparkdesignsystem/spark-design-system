b492e1aec6efdde2b0706654ab0ac1d1e6fb2513
Bob Wise <bobwise@quickenloans.com>
2021-06-01 13:31:17 -0400

    oopsie

--- html/spark-exports.js
+++ html/spark-exports.js
@@ -147,6 +147,8 @@ import {
 import { carousel } from './components/carousel';
 import { highlightBoard } from './components/highlight-board';
 import toggleAriaExpandedAttribute from './utilities/toggleAriaExpanded';
+import generateAriaControls from './utilities/generateAriaControls';
+import generateAriaOwns from './utilities/generateAriaOwns';
 import isElementVisible from './utilities/isElementVisible';
 import scrollYDirection from './utilities/scrollYDirection';
 import { checkbox, bindCheckboxUIEvents } from './base/inputs/checkbox';
@@ -266,6 +268,8 @@ export {
   addClassIfNotEmpty,
   toggleClassWithValue,
   toggleAriaExpandedAttribute,
+  generateAriaControls,
+  generateAriaOwns,
   checkbox,
   bindCheckboxUIEvents,
   generateIdForInput,
