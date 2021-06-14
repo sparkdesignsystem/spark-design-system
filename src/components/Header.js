1266f7d42c4ebc71e149643bd33599704539924f
corcorankt1 <guern1kn@gmail.com>
2021-06-14 11:30:36 -0400

    remove width-100 from inputs

--- src/components/Header.js
+++ src/components/Header.js
@@ -154,7 +154,7 @@ const Header = ({ setContext }) => {
       utilityContents={utilityItems}
       navLink={
         <SprkTextInput
-          additionalClasses="docs-header-search sprk-u-Width-100"
+          additionalClasses="docs-header-search"
           leadingIcon="search"
           hiddenLabel
           name="InlineSearch"
