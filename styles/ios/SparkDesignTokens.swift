c49815f7af0f6a5ac4a50ad7cd050b59b3dafd1d
Bob Wise <bobwise@quickenloans.com>
2021-08-26 16:18:04 -0400

    autocomplete z index

--- styles/ios/SparkDesignTokens.swift
+++ styles/ios/SparkDesignTokens.swift
@@ -17,7 +17,6 @@ public class SparkDesignTokens {
     public static let sprkAutocompleteResultFontSize = CGFloat(16.00)
     public static let sprkAutocompleteResultPadding = CGFloat(16.00)
     public static let sprkAutocompleteResultsHiddenDisplay = none
-    public static let sprkAutocompleteResultsZIndex = 0
     public static let sprkBigNavActiveColor = UIColor(red: 0.784, green: 0.063, blue: 0.180, alpha:1)
     public static let sprkBigNavBg = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha:1)
     public static let sprkBigNavItemActiveColor = UIColor(red: 0.784, green: 0.063, blue: 0.180, alpha:1)
