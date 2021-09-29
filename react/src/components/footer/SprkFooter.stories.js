06a6b400a83dd6915e910c35c3eb3092484146e0
RVMendoza <RVxMendoza@gmail.com>
2021-09-29 15:10:23 -0400

    Force deploy

--- react/src/components/footer/SprkFooter.stories.js
+++ react/src/components/footer/SprkFooter.stories.js
@@ -236,7 +236,7 @@ export const defaultStory = () => (
     paragraphs={[
       {
         text:
-          '*Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+          '*Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
       },
       {
         text:
