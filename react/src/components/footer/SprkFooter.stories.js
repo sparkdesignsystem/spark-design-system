7e78fcb1cf066b4601ee7ebe4a950ccdc101d628
RVMendoza <RVxMendoza@gmail.com>
2021-09-29 15:15:03 -0400

    Force deploy pt 2

--- react/src/components/footer/SprkFooter.stories.js
+++ react/src/components/footer/SprkFooter.stories.js
@@ -257,7 +257,7 @@ export const defaultStory = () => (
           additionalClasses="sprk-c-Footer__text"
           element="p"
         >
-          Sed ut perspiciatis unde omnis iste natus error sit <SprkLink href="#nogo" additionalClasses="sprk-b-Link--inline-light"> inline link </SprkLink> accusantium doloremque laudantiu.
+          Sed et ut perspiciatis unde omnis iste natus error sit <SprkLink href="#nogo" additionalClasses="sprk-b-Link--inline-light"> inline link </SprkLink> accusantium doloremque laudantiu.
         </SprkText>
       </SprkStackItem>
     }
